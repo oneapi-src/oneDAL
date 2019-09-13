e4a80968ed784f6dab3264d39dec9514fd58e705
Maksim Derbasov <ntfs.hard@gmail.com>
2019-09-13 14:50:08 +0300

    aos_numeric_table.h check offset and index values
    Checks that offset and index is in range

--- include/data_management/data/aos_numeric_table.h
+++ include/data_management/data/aos_numeric_table.h
@@ -211,6 +211,11 @@ public:
     services::Status setFeature(size_t idx, size_t offset, features::FeatureType featureType = features::DAAL_CONTINUOUS, size_t categoryNumber=0)
     {
         services::Status s;
+        if (offset >= _structSize || idx >= getNumberOfColumns())
+        {
+            return services::Status(services::ErrorIncorrectDataRange);
+        }
+
         if( _ddict.get() == NULL )
         {
             _ddict = NumericTableDictionary::create(&s);
@@ -233,7 +238,14 @@ public:
      */
     void setOffset(size_t idx, size_t offset)
     {
-        _offsets[idx] = offset;
+        if (offset >= _structSize || idx >= getNumberOfColumns())
+        {
+            _status.add(services::throwIfPossible(services::Status(services::ErrorIncorrectDataRange)));
+        }
+        else
+        {
+            _offsets[idx] = offset;
+        }
     }
 
     services::Status getBlockOfRows(size_t vector_idx, size_t vector_num, ReadWriteMode rwflag, BlockDescriptor<double>& block) DAAL_C11_OVERRIDE
