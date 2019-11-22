cfbd0bc09c187cfb2003df122459d5489080c852
agrigori <alexey.grigoriev@intel.com>
2019-11-22 09:51:12 +0300

    Added bf knn batch export for Windows

--- include/algorithms/k_nearest_neighbors/bf_knn_classification_predict.h
+++ include/algorithms/k_nearest_neighbors/bf_knn_classification_predict.h
@@ -85,7 +85,7 @@ public:
  *      - \ref training::interface1::Batch "training::Batch" class
  */
 template<typename algorithmFPType = DAAL_ALGORITHM_FP_TYPE, Method method = defaultDense>
-class Batch : public classifier::prediction::Batch
+class DAAL_EXPORT Batch : public classifier::prediction::Batch
 {
 public:
     typedef classifier::prediction::Batch super;
