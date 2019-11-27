b7ec5b7755a17efdfbd760951f7da476233d4a10
PivovarA <alexander.pivovar@intel.com>
2019-11-27 17:49:01 +0300

    change storage levels and modified SVD

--- samples/scala/spark/sources/SVD.scala
+++ samples/scala/spark/sources/SVD.scala
@@ -23,6 +23,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.api.java._
 import org.apache.spark.api.java.function._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
 
 import scala.Tuple2
 import scala.collection.mutable.ArrayBuffer
@@ -47,31 +48,30 @@ object SVD {
         val dataRDD = getAsPairRDD(matrix)
 
         val resultsFromStep1 = computeStep1Local(dataRDD)
+        dataRDD.unpersist()
 
         val resultsFromStep2 = computeStep2Master(context, resultsFromStep1._1)
 
         val resultsFromStep3 = computeStep3Local(resultsFromStep1._2, resultsFromStep2._2)
 
-        dataRDD.unpersist()
-
         val U = resultsFromStep3.mapPartitions(
-            (it: Iterator[Tuple2[Long, HomogenNumericTable]]) => {
+            (it: Iterator[HomogenNumericTable]) => {
                 val res = new ArrayBuffer[Vector]()
                 while (it.hasNext) {
-                    val tup = it.next
+                    val collection = it.next
                     val contextLocal = new DaalContext()
-                    tup._2.unpack(contextLocal)
-                    val nRows = tup._2.getNumberOfRows().toInt
-                    val nCols = tup._2.getNumberOfColumns().toInt
+                    collection.unpack(contextLocal)
+                    val nRows = collection.getNumberOfRows().toInt
+                    val nCols = collection.getNumberOfColumns().toInt
                     val i: Int = 0
                     for (i <- 0 to (nRows - 1)) {
                         val arr = new Array[Double](nCols)
                         var buffer = DoubleBuffer.allocate(nCols)
-                        buffer = tup._2.getBlockOfRows(i, 1, buffer)
+                        buffer = collection.getBlockOfRows(i, 1, buffer)
                         buffer.get(arr)
                         res += Vectors.dense(arr)
                     }
-                    tup._2.pack()
+                    collection.pack()
                     contextLocal.dispose()
                 }
 
@@ -102,100 +102,78 @@ object SVD {
                                                           Matrices.dense(nValues, nValues, V))
     }
 
-    private def getAsPairRDD(matrix: RowMatrix): RDD[Tuple2[HomogenNumericTable, Long]] = {
-        val nCols = matrix.numCols.toInt
+
+    private def getAsPairRDD(matrix: RowMatrix): RDD[HomogenNumericTable] = {
+
 
         val tablesRDD = matrix.rows.mapPartitions(
             (it: Iterator[Vector]) => {
-                val maxRows: Int = 200000
-                var curRow: Int = 0
+
                 val tables = new ArrayBuffer[HomogenNumericTable]()
                 var arrays = new ArrayBuffer[Array[Double]]()
-                var hasNext = it.hasNext
-                while (hasNext) {
-                    val curVector = it.next
-                    hasNext = it.hasNext
+                it.foreach{curVector =>
                     val rowData = curVector.toArray
                     arrays += rowData
-
-                    curRow += 1
-                    if (curRow == maxRows || !hasNext) {
-                        val arrData = new Array[Double](curRow * nCols)
-                        var i: Int = 0
-                        for (i <- 0 to (curRow - 1)) {
-                            arrays(i).copyToArray(arrData, i * nCols)
-                        }
-                        val context = new DaalContext()
-                        val table = new HomogenNumericTable(context, arrData, nCols, curRow)
-
-                        table.pack()
-                        tables += table
-
-                        context.dispose()
-                        curRow = 0
-                        arrays.clear
-                    }
                 }
+                val numCols: Int = arrays(0).length
+                val numRows: Int = arrays.length
+
+                val arrData = new Array[Double](numRows * numCols)
+                var i: Int = 0
+                for (i <- 0 to (numRows - 1)) {
+                    arrays(i).copyToArray(arrData, i * numCols)
+                }
+                val context = new DaalContext()
+                val table = new HomogenNumericTable(context, arrData, numCols, numRows)
+                table.pack()
+                tables += table
+
+                context.dispose()
+                arrays.clear
                 tables.iterator
             }
-        )
-
-        tablesRDD.zipWithIndex.cache
+        ).persist(StorageLevel.MEMORY_AND_DISK)
+        tablesRDD
     }
 
-    private def computeStep1Local(dataRDD: RDD[Tuple2[HomogenNumericTable, Long]]): Tuple2[RDD[Tuple2[Long, DataCollection]], RDD[Tuple2[Long, DataCollection]]] = {
+    private def computeStep1Local(dataRDD: RDD[HomogenNumericTable]): Tuple2[RDD[Tuple2[Long, DataCollection]], RDD[Tuple2[Long, DataCollection]]] = {
 
         /* Creating RDD containing both partial results for step2 and step3 */
         val dataFromStep1RDD = dataRDD.mapPartitions(
-            (it: Iterator[Tuple2[HomogenNumericTable, Long]]) => {
+            (it: Iterator[HomogenNumericTable]) => {
 
-                val res = new ArrayBuffer[Tuple2[Long, Tuple2[DataCollection, DataCollection]]]()
+                val table = it.next
+                val contextLocal = new DaalContext()
 
-                while(it.hasNext) {
-                    val tup = it.next
+                /* Create algorithm to compute SVD decomposition on local node */
+                val svdStep1Local = new DistributedStep1Local(contextLocal, classOf[java.lang.Double], Method.defaultDense)
+                table.unpack(contextLocal)
+                svdStep1Local.input.set( InputId.data, table )
 
-                    val contextLocal = new DaalContext()
+                /* Compute SVD Step 1  */
+                val pres = svdStep1Local.compute()
+                val dataFromStep1ForStep2 = pres.get( PartialResultId.outputOfStep1ForStep2 )
+                dataFromStep1ForStep2.pack()
+                val dataFromStep1ForStep3 = pres.get( PartialResultId.outputOfStep1ForStep3 )
+                dataFromStep1ForStep3.pack()
 
-                    /* Create algorithm to compute SVD decomposition on local node */
-                    val svdStep1Local = new DistributedStep1Local(contextLocal, classOf[java.lang.Double], Method.defaultDense)
-                    tup._1.unpack(contextLocal)
-                    svdStep1Local.input.set( InputId.data, tup._1 )
+                contextLocal.dispose()
+                Iterator(Tuple2(dataFromStep1ForStep2, dataFromStep1ForStep3))
 
-                    /* Compute SVD Step 1  */
-                    val pres = svdStep1Local.compute()
-                    val dataFromStep1ForStep2 = pres.get( PartialResultId.outputOfStep1ForStep2 )
-                    dataFromStep1ForStep2.pack()
-                    val dataFromStep1ForStep3 = pres.get( PartialResultId.outputOfStep1ForStep3 )
-                    dataFromStep1ForStep3.pack()
-
-                    contextLocal.dispose()
-                    res += new Tuple2(tup._2, new Tuple2(dataFromStep1ForStep2, dataFromStep1ForStep3))
-                }
-                res.iterator
             }).cache()
 
         /* Extracting partial results for step2 */
-        val dataFromStep1ForStep2RDD = dataFromStep1RDD.mapPartitions(
-            (it: Iterator[Tuple2[Long, Tuple2[DataCollection, DataCollection]]]) => {
-                val res = new ArrayBuffer[Tuple2[Long, DataCollection]]()
-                while(it.hasNext)
-                {
-                    val tup = it.next()
-                    res += new Tuple2(tup._1, tup._2._1)
-                }
-                res.iterator
+        val dataFromStep1ForStep2RDD = dataFromStep1RDD.mapPartitionsWithIndex(
+            (index, it: Iterator[Tuple2[DataCollection, DataCollection]]) => {
+                val tup = it.next()
+                Iterator(Tuple2(index.asInstanceOf[Long], tup._1))
             })
 
         /* Extracting partial results for step3 */
-        val dataFromStep1ForStep3RDD = dataFromStep1RDD.mapPartitions(
-            (it: Iterator[Tuple2[Long, Tuple2[DataCollection, DataCollection]]]) => {
-                val res = new ArrayBuffer[Tuple2[Long, DataCollection]]()
-                while(it.hasNext)
-                {
-                    val tup = it.next()
-                    res += new Tuple2(tup._1, tup._2._2)
-                }
-                res.iterator
+        val dataFromStep1ForStep3RDD = dataFromStep1RDD.mapPartitionsWithIndex(
+            (index, it: Iterator[Tuple2[DataCollection, DataCollection]]) => {
+                val tup = it.next()
+                Iterator(Tuple2(index.asInstanceOf[Long], tup._2))
             })
 
         new Tuple2(dataFromStep1ForStep2RDD, dataFromStep1ForStep3RDD)
@@ -241,41 +219,36 @@ object SVD {
     }
 
     private def computeStep3Local(dataFromStep1ForStep3RDD: RDD[Tuple2[Long, DataCollection]], dataFromStep2ForStep3RDD: RDD[Tuple2[Long, DataCollection]])
-        : RDD[Tuple2[Long, HomogenNumericTable]] = {
+        : RDD[HomogenNumericTable] = {
 
         val dataForStep3RDD = dataFromStep1ForStep3RDD.cogroup(dataFromStep2ForStep3RDD)
 
         val ntURDD = dataForStep3RDD.mapPartitions(
             (it: Iterator[Tuple2[Long, Tuple2[Iterable[DataCollection], Iterable[DataCollection]]]]) => {
-                val res = new ArrayBuffer[Tuple2[Long, HomogenNumericTable]]()
-                while(it.hasNext) {
-                    val tup = it.next()
-    
-                    val contextLocal = new DaalContext()
+                val tup = it.next()
 
-                    val ntQPi = tup._2._1.iterator.next()
-                    ntQPi.unpack(contextLocal)
-                    val ntPi  = tup._2._2.iterator.next()
-                    ntPi.unpack(contextLocal)
+                val contextLocal = new DaalContext()
 
-                    /* Create algorithm to compute SVD decomposition on master node */
-                    val svdStep3Local = new DistributedStep3Local(contextLocal, classOf[java.lang.Double], Method.defaultDense)
-                    svdStep3Local.input.set(DistributedStep3LocalInputId.inputOfStep3FromStep1, ntQPi)
-                    svdStep3Local.input.set(DistributedStep3LocalInputId.inputOfStep3FromStep2, ntPi)
+                val ntQPi = tup._2._1.iterator.next()
+                ntQPi.unpack(contextLocal)
+                val ntPi  = tup._2._2.iterator.next()
+                ntPi.unpack(contextLocal)
 
-                    /* Compute SVD Step 3 */
-                    svdStep3Local.compute()
-                    val result = svdStep3Local.finalizeCompute()
+                /* Create algorithm to compute SVD decomposition on master node */
+                val svdStep3Local = new DistributedStep3Local(contextLocal, classOf[java.lang.Double], Method.defaultDense)
+                svdStep3Local.input.set(DistributedStep3LocalInputId.inputOfStep3FromStep1, ntQPi)
+                svdStep3Local.input.set(DistributedStep3LocalInputId.inputOfStep3FromStep2, ntPi)
 
-                    val Ui = result.get(ResultId.leftSingularMatrix).asInstanceOf[HomogenNumericTable]
-                    Ui.pack()
+                /* Compute SVD Step 3 */
+                svdStep3Local.compute()
+                val result = svdStep3Local.finalizeCompute()
 
-                    contextLocal.dispose()
+                val Ui = result.get(ResultId.leftSingularMatrix).asInstanceOf[HomogenNumericTable]
+                Ui.pack()
 
-                    res += new Tuple2[Long, HomogenNumericTable](tup._1, Ui)
-                }
-                
-                res.iterator
+                contextLocal.dispose()
+
+                Iterator(Ui)
             })
 
         ntURDD
