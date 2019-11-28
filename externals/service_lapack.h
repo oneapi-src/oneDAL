55ffa7d22a80ce5df32066129f8e620e479663cf
aprutsko <alexey.prutskov@intel.com>
2019-11-28 20:29:58 +0300

    Added auto dispatcher class for Lapack

--- externals/service_lapack.h
+++ externals/service_lapack.h
@@ -179,6 +179,190 @@ struct Lapack
     }
 };
 
+template<typename fpType>
+struct LapackAutoDispatch
+{
+    typedef typename Lapack<fpType, CpuType::sse2>::SizeType SizeType;
+
+    static void xpotrf(char *uplo, SizeType *p, fpType *ata, SizeType *ldata, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xpotrf, uplo, p, ata, ldata, info);
+    }
+
+    static void xxpotrf(char *uplo, SizeType *p, fpType *ata, SizeType *ldata, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxpotrf, uplo, p, ata, ldata, info);
+    }
+
+    static void xpotrs(char *uplo, SizeType *p, SizeType *ny, fpType *ata, SizeType *ldata, fpType *beta, SizeType *ldaty,
+                SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xpotrs, uplo, p, ny, ata, ldata, beta, ldaty, info);
+    }
+
+    static void xxpotrs(char *uplo, SizeType *p, SizeType *ny, fpType *ata, SizeType *ldata, fpType *beta, SizeType *ldaty,
+                SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxpotrs, uplo, p, ny, ata, ldata, beta, ldaty, info);
+    }
+
+    static void xpotri(char *uplo, SizeType *p, fpType *ata, SizeType *ldata, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xpotri, uplo, p, ata, ldata, info);
+    }
+
+    static void xxpotri(char *uplo, SizeType *p, fpType *ata, SizeType *ldata, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xpotri, uplo, p, ata, ldata, info);
+    }
+
+    static void xgerqf(SizeType *m, SizeType *n, fpType *a, SizeType *lda, fpType *tau, fpType *work, SizeType *lwork,
+                SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xgerqf, m, n, a, lda, tau, work, lwork, info);
+    }
+
+    static void xxgerqf(SizeType *m, SizeType *n, fpType *a, SizeType *lda, fpType *tau, fpType *work, SizeType *lwork,
+                SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxgerqf, m, n, a, lda, tau, work, lwork, info);
+    }
+
+    static void xormrq(char *side, char *trans, SizeType *m, SizeType *n, SizeType *k, fpType *a, SizeType *lda,
+                fpType *tau, fpType *c, SizeType *ldc, fpType *work, SizeType *lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xormrq, side, trans, m, n, k, a, lda, tau, c, ldc, work, lwork, info);
+    }
+
+    static void xxormrq(char *side, char *trans, SizeType *m, SizeType *n, SizeType *k, fpType *a, SizeType *lda,
+                fpType *tau, fpType *c, SizeType *ldc, fpType *work, SizeType *lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxormrq, side, trans, m, n, k, a, lda, tau, c, ldc, work, lwork, info);
+    }
+
+    static void xtrtrs(char *uplo, char *trans, char *diag, SizeType *n, SizeType *nrhs,
+                fpType *a, SizeType *lda, fpType *b, SizeType *ldb, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xtrtrs, uplo, trans, diag, n, nrhs, a, lda, b, ldb, info);
+    }
+
+    static void xxtrtrs(char *uplo, char *trans, char *diag, SizeType *n, SizeType *nrhs,
+                fpType *a, SizeType *lda, fpType *b, SizeType *ldb, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxtrtrs, uplo, trans, diag, n, nrhs, a, lda, b, ldb, info);
+    }
+
+    static void xpptrf(char *uplo, SizeType *n, fpType *ap, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xpptrf, uplo, n, ap, info);
+    }
+
+    static void xxpptrf(char *uplo, SizeType *n, fpType *ap, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxpptrf, uplo, n, ap, info);
+    }
+
+    static void xgeqrf(SizeType m, SizeType n, fpType *a,
+                SizeType lda, fpType *tau, fpType *work, SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xgeqrf, m, n, a, lda, tau, work, lwork, info);
+    }
+
+    static void xxgeqrf(SizeType m, SizeType n, fpType *a,
+                SizeType lda, fpType *tau, fpType *work, SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxgeqrf, m, n, a, lda, tau, work, lwork, info);
+    }
+
+    static void xgeqp3(const SizeType m, const SizeType n, fpType *a,
+                const SizeType lda, SizeType *jpvt, fpType *tau, fpType *work, const SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xgeqp3, m, n, a, lda, jpvt, tau, work, lwork, info);
+    }
+
+    static void xxgeqp3(SizeType m, SizeType n, fpType *a,
+                SizeType lda, SizeType *jpvt, fpType *tau, fpType *work, SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxgeqp3, m, n, a, lda, jpvt, tau, work, lwork, info);
+    }
+
+    static void xorgqr(const SizeType m, const SizeType n, const SizeType k,
+                fpType *a, const SizeType lda, const fpType *tau, fpType *work, const SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xorgqr, m, n, k, a, lda, tau, work, lwork, info);
+    }
+
+    static void xxorgqr(SizeType m, SizeType n, SizeType k,
+                fpType *a, SizeType lda, fpType *tau, fpType *work, SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxorgqr, m, n, k, a, lda, tau, work, lwork, info);
+    }
+
+    static void xgesvd(char jobu, char jobvt, SizeType m, SizeType n,
+                fpType *a, SizeType lda, fpType *s, fpType *u, SizeType ldu, fpType *vt, SizeType ldvt,
+                fpType *work, SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xgesvd, jobu, jobvt, m, n, a, lda, s, u, ldu, vt, ldvt, work, lwork, info);
+    }
+
+    static void xxgesvd(char jobu, char jobvt, SizeType m, SizeType n,
+                fpType *a, SizeType lda, fpType *s, fpType *u, SizeType ldu, fpType *vt, SizeType ldvt,
+                fpType *work, SizeType lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxgesvd, jobu, jobvt, m, n, a, lda, s, u, ldu, vt, ldvt, work, lwork, info);
+    }
+
+    static void xsyevd(char *jobz, char *uplo, SizeType *n, fpType *a, SizeType *lda, fpType *w, fpType *work,
+                SizeType *lwork, SizeType *iwork, SizeType *liwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xsyevd, jobz, uplo, n, a, lda, w, work, lwork, iwork, liwork, info);
+    }
+
+    static void xxsyevd(char *jobz, char *uplo, SizeType *n, fpType *a, SizeType *lda, fpType *w, fpType *work,
+                SizeType *lwork, SizeType *iwork, SizeType *liwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxsyevd, jobz, uplo, n, a, lda, w, work, lwork, iwork, liwork, info);
+    }
+
+    static void xormqr(char *side, char *trans, SizeType *m, SizeType *n, SizeType *k,
+                fpType *a, SizeType *lda, fpType *tau, fpType *c, SizeType *ldc, fpType *work, SizeType *lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xormqr, side, trans, m, n, k, a, lda, tau, c, ldc, work, lwork, info);
+    }
+
+    static void xxormqr(char *side, char *trans, SizeType *m, SizeType *n, SizeType *k,
+                fpType *a, SizeType *lda, fpType *tau, fpType *c, SizeType *ldc, fpType *work, SizeType *lwork, SizeType *info)
+    {
+        const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
+        CALL_CPU_FUNCTION(fpType, cpuId, Lapack, xxormqr, side, trans, m, n, k, a, lda, tau, c, ldc, work, lwork, info);
+    }
+};
+
 } // namespace internal
 } // namespace daal
 
--- service/kernel/oneapi/lapack_gpu.cpp
+++ service/kernel/oneapi/lapack_gpu.cpp
@@ -17,7 +17,6 @@
 
 #include "oneapi/internal/math/reference_lapack.h"
 #include "service_lapack.h"
-#include "daal_kernel_defines.h"
 #include "error_handling.h"
 #include "blas_gpu.h"
 #include "cl_kernels/kernel_blas.cl"
@@ -34,24 +33,11 @@ namespace interface1
 {
 using namespace daal::internal;
 
-#define CALL_CPU_FUNCTION(fptype, cpuId, className, funcName, ...)                                                                                         \
-    switch (cpuId) {                                                                                                                                       \
-    DAAL_KERNEL_SSSE3_ONLY_CODE(case daal::CpuType::ssse3: className<fptype, daal::CpuType::ssse3>::funcName(__VA_ARGS__); break;)                         \
-    DAAL_KERNEL_SSE42_ONLY_CODE(case daal::CpuType::sse42: className<fptype, daal::CpuType::sse42>::funcName(__VA_ARGS__); break;)                         \
-    DAAL_KERNEL_AVX_ONLY_CODE(case daal::CpuType::avx:   className<fptype, daal::CpuType::avx>::funcName(__VA_ARGS__); break;)                             \
-    DAAL_KERNEL_AVX2_ONLY_CODE(case daal::CpuType::avx2:  className<fptype, daal::CpuType::avx2>::funcName(__VA_ARGS__); break;)                           \
-    DAAL_KERNEL_AVX512_ONLY_CODE(case daal::CpuType::avx512:  className<fptype, daal::CpuType::avx512>::funcName(__VA_ARGS__); break;)                     \
-    DAAL_KERNEL_AVX512_MIC_ONLY_CODE(case daal::CpuType::avx512_mic: className<fptype, daal::CpuType::avx512_mic>::funcName(__VA_ARGS__); break;)    \
-    default: className<fptype, daal::CpuType::sse2>::funcName(__VA_ARGS__); break;                                                                         \
-    }
-
-
 template<typename algorithmFPType>
 services::Status ReferencePotrf<algorithmFPType>::operator()(const math::UpLo uplo, const size_t n, services::Buffer<algorithmFPType> & a_buffer,
                                                              const size_t lda)
 {
     services::Status status;
-    const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
 
     char up = uplo == math::UpLo::Upper ? 'U' : 'L';
 
@@ -62,7 +48,7 @@ services::Status ReferencePotrf<algorithmFPType>::operator()(const math::UpLo up
 
     services::SharedPtr<algorithmFPType> aPtr = a_buffer.toHost(data_management::ReadWriteMode::readWrite);
 
-    CALL_CPU_FUNCTION(algorithmFPType, cpuId, Lapack, xpotrf, &up, &nInt, aPtr.get(), &ldaInt, &info);
+    LapackAutoDispatch<algorithmFPType>::xpotrf(&up, &nInt, aPtr.get(), &ldaInt, &info);
 
     DAAL_CHECK(info == 0, services::ErrorID::ErrorNormEqSystemSolutionFailed);
     return status;
@@ -74,7 +60,6 @@ services::Status ReferencePotrs<algorithmFPType>::operator()(const math::UpLo up
                                                              services::Buffer<algorithmFPType> & b_buffer, const size_t ldb)
 {
     services::Status status;
-    const daal::CpuType cpuId = static_cast<daal::CpuType>(services::Environment::getInstance()->getCpuId());
 
     char up = uplo == math::UpLo::Upper ? 'U' : 'L';
 
@@ -88,7 +73,7 @@ services::Status ReferencePotrs<algorithmFPType>::operator()(const math::UpLo up
     services::SharedPtr<algorithmFPType> aPtr = a_buffer.toHost(data_management::ReadWriteMode::readWrite);
     services::SharedPtr<algorithmFPType> bPtr = b_buffer.toHost(data_management::ReadWriteMode::readWrite);
 
-    CALL_CPU_FUNCTION(algorithmFPType, cpuId, Lapack, xpotrs, &up, &nInt, &nyInt, aPtr.get(), &ldaInt, bPtr.get(), &ldbInt, &info);
+    LapackAutoDispatch<algorithmFPType>::xpotrs(&up, &nInt, &nyInt, aPtr.get(), &ldaInt, bPtr.get(), &ldbInt, &info);
 
     DAAL_CHECK(info == 0, services::ErrorID::ErrorNormEqSystemSolutionFailed);
     return status;
