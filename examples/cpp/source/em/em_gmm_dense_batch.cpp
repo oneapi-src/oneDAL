98a7c78c29eba8ae7eed109033e0f83f6f82b0b0
PovelikinRostislav <povelikin.rostislav@gmail.com>
2020-01-23 01:20:26 +0300

    Fix double usage of daal::data_management namespace in examples
    (cherry picked from commit 2334d5854c5037635b2d09c590b5f5efed249bfd)

--- examples/cpp/source/em/em_gmm_dense_batch.cpp
+++ examples/cpp/source/em/em_gmm_dense_batch.cpp
@@ -33,7 +33,6 @@ using namespace std;
 using namespace daal;
 using namespace daal::algorithms;
 using namespace daal::data_management;
-using namespace daal::data_management;
 
 typedef float dataFPType; /* Data floating-point type */
 
--- examples/cpp/source/quality_metrics/svm_two_class_metrics_dense_batch.cpp
+++ examples/cpp/source/quality_metrics/svm_two_class_metrics_dense_batch.cpp
@@ -31,7 +31,6 @@
 
 using namespace std;
 using namespace daal;
-using namespace daal::data_management;
 using namespace daal::algorithms;
 using namespace daal::data_management;
 using namespace daal::algorithms::classifier::quality_metric;
