a299988b7480c6a2e8696005107b2ef5e959fe8a
PovelikinRostislav <povelikin.rostislav@gmail.com>
2020-01-23 21:01:48 +0300

    Apply clang-format to changed files

--- examples/cpp/source/error_handling/error_handling_throw.cpp
+++ examples/cpp/source/error_handling/error_handling_throw.cpp
@@ -41,7 +41,7 @@ int main(int argc, char * argv[])
         FileDataSource<CSVFeatureManager> wrongDataSource(wrongDatasetFileName);
         /* An exception was generated due to absense DAAL_NOTHROW_EXCEPTIONS define by default */
     }
-    catch (daal::services::Exception& e)
+    catch (daal::services::Exception & e)
     {
         /* Retrieve the description of the generated exception. */
         std::cout << "FileDataSource expected error: " << e.what() << std::endl;
