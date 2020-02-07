b64125eb7238e894b1c5c47a48c4dcdad01c9e59
dmitrypa <dmitry.pavlov@intel.com>
2020-02-07 12:52:18 +0300

    Fix SDL gaps in knn

--- algorithms/kernel/k_nearest_neighbors/kdtree_knn_classification_model_impl.h
+++ algorithms/kernel/k_nearest_neighbors/kdtree_knn_classification_model_impl.h
@@ -150,6 +150,7 @@ public:
         {
             data_management::SOANumericTablePtr tbl(
                 new data_management::SOANumericTable(value->getNumberOfColumns(), value->getNumberOfRows(), data_management::DictionaryIface::equal));
+            DAAL_CHECK_MALLOC(tbl.get())
             tbl->getDictionary()->setAllFeatures<algorithmFPType>(); // Just to set type of all features. Also, no way to use featuresEqual flag.
             tbl->resize(value->getNumberOfRows());
 
@@ -195,6 +196,7 @@ public:
         else
         {
             data_management::SOANumericTablePtr tbl(new data_management::SOANumericTable(value->getNumberOfColumns(), value->getNumberOfRows()));
+            DAAL_CHECK_MALLOC(tbl.get())
             tbl->setArray(static_cast<algorithmFPType *>(0), 0);                    // Just to create the dictionary.
             tbl->getDictionary()->setNumberOfFeatures(value->getNumberOfColumns()); // Sadly, setArray() hides number of features from the dictionary.
             data_management::NumericTableFeature temp;
