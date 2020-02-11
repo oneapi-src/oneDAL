1e2d9e81049639e791f2fd8e440ccfc6f098031d
Derbasov, Maksim <maksim.derbasov@intel.com>
2020-02-11 16:16:42 +0300

    fix build on visual studio compiler

--- include/algorithms/algorithm_container_base_batch.h
+++ include/algorithms/algorithm_container_base_batch.h
@@ -180,6 +180,7 @@ public:
 protected:
     AlgorithmContainerImpl<batch> * _cntr;
 
+private:
     AlgorithmDispatchContainer(const AlgorithmDispatchContainer&);
     AlgorithmDispatchContainer& operator=(const AlgorithmDispatchContainer&);
 };
