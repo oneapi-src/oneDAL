2175b2cf2d16e92bfb873a84083be08f7d2ef9b9
Derbasov, Maksim <maksim.derbasov@intel.com>
2020-02-11 17:16:43 +0300

    revert to build fix

--- include/algorithms/algorithm_container_base_batch.h
+++ include/algorithms/algorithm_container_base_batch.h
@@ -179,10 +179,6 @@ public:
 
 protected:
     AlgorithmContainerImpl<batch> * _cntr;
-
-private:
-    AlgorithmDispatchContainer(const AlgorithmDispatchContainer&);
-    AlgorithmDispatchContainer& operator=(const AlgorithmDispatchContainer&);
 };
 
 /** @} */
