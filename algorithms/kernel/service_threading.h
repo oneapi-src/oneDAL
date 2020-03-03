7e53cf111fe60cfd000b10a05d298ee89fe2480f
ntimakin <nikita.timakin@intel.com>
2020-03-03 17:08:25 +0300

    parallel_deterministic_sum added

--- algorithms/kernel/service_threading.h
+++ algorithms/kernel/service_threading.h
@@ -129,6 +129,32 @@ public:
     }
 };
 
+template <typename algorithmFPType, CpuType cpu, typename F>
+algorithmFPType * parallel_deterministic_sum(size_t n, size_t grain_size, size_t len, const F & lambda) // lambda must have the prototype: void(void *, size_t, size_t)
+{
+    return static_cast<algorithmFPType *>(daal::parallel_deterministic_reduce(n, grain_size,
+        [&] (void ** value)
+        {
+            *value = static_cast<void *>(services::internal::service_scalable_calloc<algorithmFPType, cpu>(len));
+        }, [&] (void ** value)
+        {
+            services::internal::service_scalable_free<algorithmFPType, cpu>(static_cast<algorithmFPType *>(*value));
+        }, lambda,
+        [&] (void * lhs_, void * rhs_)
+        {
+            algorithmFPType * lhs = static_cast<algorithmFPType *>(lhs_);
+            algorithmFPType * rhs = static_cast<algorithmFPType *>(rhs_);
+
+            PRAGMA_IVDEP
+            PRAGMA_VECTOR_ALWAYS
+            for (size_t i = 0; i < len; ++i)
+            {
+                lhs[i] += rhs[i];
+            }
+        }
+    ));
+}
+
 } // namespace daal
 
 #endif
