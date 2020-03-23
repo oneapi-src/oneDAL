54bb3fa0a787820cd187caea6a9046fc106b0479
Kirill <Petrovich_1811@mail.ru>
2020-03-24 02:06:04 +0300

    add example

---
+++ examples/cpp_sycl/source/svm/svm_init.cpp
@@ -0,0 +1,64 @@
+
+#include "daal_sycl.h"
+#include "service.h"
+#include "service_sycl.h"
+
+using namespace std;
+using namespace daal;
+using namespace daal::algorithms;
+using namespace daal::data_management;
+
+string trainDatasetFileName = "../data/batch/svm_two_class_train_dense.csv";
+
+const size_t nFeatures = 20;
+
+kernel_function::KernelIfacePtr kernel(new kernel_function::linear::Batch<>());
+
+int main(int argc, char * argv[])
+{
+    checkArguments(argc, argv, 2, &trainDatasetFileName, &testDatasetFileName);
+
+    for (const auto & deviceSelector : getListOfDevices())
+    {
+        const auto & nameDevice = deviceSelector.first;
+        const auto & device     = deviceSelector.second;
+        if (!device.is_gpu()) continue;
+        cl::sycl::queue queue(device);
+        std::cout << "Running on " << nameDevice << "\n\n";
+
+        daal::services::SyclExecutionContext ctx(queue);
+        services::Environment::getInstance()->setDefaultExecutionContext(ctx);
+
+        trainModel(trainingResult);
+    }
+
+    return 0;
+}
+
+void trainModel()
+{
+    FileDataSource<CSVFeatureManager> trainDataSource(trainDatasetFileName, DataSource::notAllocateNumericTable, DataSource::doDictionaryFromContext);
+
+    auto trainData        = SyclHomogenNumericTable<>::create(nFeatures, 0, NumericTable::doNotAllocate);
+    auto trainGroundTruth = SyclHomogenNumericTable<>::create(1, 0, NumericTable::doNotAllocate);
+
+    auto mergedData(new MergedNumericTable(trainData, trainGroundTruth));
+
+    trainDataSource.loadDataBlock(mergedData.get());
+
+    svm::training::Batch<> algorithm;
+
+    algorithm.parameter.kernel        = kernel;
+    algorithm.parameter.cacheSize     = 40000000;
+    algorithm.parameter.C             = 1e-2;
+    algorithm.parameter.maxIterations = 10;
+
+    algorithm.input.set(classifier::training::data, trainData);
+    algorithm.input.set(classifier::training::labels, trainGroundTruth);
+
+    /* Build the SVM model */
+    algorithm.compute();
+
+    /* Retrieve the algorithm results */
+    trainingResult = algorithm.getResult();
+}
