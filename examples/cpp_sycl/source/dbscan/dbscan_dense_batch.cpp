d4ae2b88a22cc7d094724c276b23074f06770a2a
agrigori <alexey.grigoriev@intel.com>
2020-03-30 17:13:57 +0300

    Fixed dbcan example

--- examples/cpp_sycl/source/dbscan/dbscan_dense_batch.cpp
+++ examples/cpp_sycl/source/dbscan/dbscan_dense_batch.cpp
@@ -64,7 +64,7 @@ int main(int argc, char * argv[])
         dbscan::Batch<> algorithm(epsilon, minObservations);
 
         algorithm.input.set(dbscan::data, dataSource.getNumericTable());
-
+        algorithm.parameter().memorySavingMode = true;
         algorithm.compute();
 
         /* Print the clusterization results */
