8ce95ae34c558537e2ea93542f68de7c3f3f1cd5
agrigori <alexey.grigoriev@intel.com>
2020-05-27 13:34:17 +0300

    Fix for daal4py warnings

--- include/algorithms/kmeans/kmeans_multinode_batch.h
+++ include/algorithms/kmeans/kmeans_multinode_batch.h
@@ -182,6 +182,9 @@ using interface1::MultiNodeBatchContainer;
 using interface1::MultiNodeBatch;
 using interface1::Method;
 using interface1::defaultDense;
+using Input     = algorithms::kmeans::Input;
+using Parameter = algorithms::kmeans::Parameter;
+using Result    = algorithms::kmeans::Result;
 
 } // namespace kmeans
 } // namespace preview
