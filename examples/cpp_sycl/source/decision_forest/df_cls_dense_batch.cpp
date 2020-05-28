fd7dd8d21098d4283a0ff3d65288039191fce306
amaklaev <artem.maklaev@intel.com>
2020-05-28 23:25:38 +0300

    fixed format

--- examples/cpp_sycl/source/decision_forest/df_cls_dense_batch.cpp
+++ examples/cpp_sycl/source/decision_forest/df_cls_dense_batch.cpp
@@ -39,8 +39,8 @@ using namespace daal::data_management;
 using namespace daal::algorithms::decision_forest::classification;
 
 /* Input data set parameters */
-const string trainDatasetFileName = "../data/batch/df_classification_train.csv";
-const string testDatasetFileName  = "../data/batch/df_classification_test.csv";
+const string trainDatasetFileName         = "../data/batch/df_classification_train.csv";
+const string testDatasetFileName          = "../data/batch/df_classification_test.csv";
 const size_t categoricalFeaturesIndices[] = {};
 const size_t nFeatures                    = 3; /* Number of features in training and testing data sets */
 
