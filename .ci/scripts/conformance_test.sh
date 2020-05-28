ae43885bed3d1249badad5fe354d613fac910425
pahandrovich <p_yak_11@mail.ru>
2020-05-28 14:15:00 +0300

    fix daal env

--- .ci/scripts/conformance_test.sh
+++ .ci/scripts/conformance_test.sh
@@ -20,20 +20,25 @@ while [[ $# -gt 0 ]]; do
 done
 
 export PATH=${CONDA_DIR}/bin:$PATH
-conda-env list
 conda create -y -n conf python=3.7
-echo PATH
-echo $PATH
-conda-env list
 source activate base
 conda activate conf
 conda install -y -c intel mpich tbb-devel numpy pytest scikit-learn
 conda remove -y daal4py --force
 conda remove -y daal --force
 export DAALROOT=${BUILD_DIR}/daal/latest
+echo DAALROOT
+echo $DAALROOT
+echo ${BUILD_DIR}/daal/latest
+ls -l ${BUILD_DIR}/daal/latest
 conda install $HOME/miniconda/envs/CB/conda-bld/linux-64/daal4py*.tar.bz2
 conda list
 
+export DAALROOT=${BUILD_DIR}/daal/latest
+echo DAALROOT
+echo $DAALROOT
+echo ${BUILD_DIR}/daal/latest
+ls -l ${BUILD_DIR}/daal/latest
 cd .ci/scripts/conformance-scripts/
 python run_tests.py 0.22.1
 cd ../../..
