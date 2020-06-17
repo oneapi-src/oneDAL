c99aee2a63946efefce767ea32de885eb516eb84
Kirill Petrov <kirill.petrov@intel.com>
2020-06-17 14:21:27 +0300

    soa experiment

--- service/kernel/data_management/soa_numeric_table.cpp
+++ service/kernel/data_management/soa_numeric_table.cpp
@@ -37,6 +37,101 @@ SOANumericTable::SOANumericTable(size_t nColumns, size_t nRows, DictionaryIface:
     }
 }
 
+services::Status SOANumericTable::searchMinPointer()
+{
+    size_t ncols = getNumberOfColumns();
+
+    if (_arrOffsets) daal::services::daal_free(_arrOffsets);
+
+    _arrOffsets = (DAAL_INT64 *)daal::services::daal_malloc(ncols * sizeof(DAAL_INT64));
+    DAAL_CHECK_MALLOC(_arrOffsets)
+    _index        = 0;
+    char * ptrMin = (char *)_arrays[0].get();
+
+    /* search index for min pointer */
+    for (size_t i = 1; i < ncols; ++i)
+    {
+        if ((char *)_arrays[i].get() < ptrMin)
+        {
+            _index = i;
+            ptrMin = (char *)_arrays[i].get();
+        }
+    }
+
+    /* compute offsets */
+    for (size_t i = 0; i < ncols; ++i)
+    {
+        char * pv      = (char *)(_arrays[i].get());
+        _arrOffsets[i] = (DAAL_INT64)(pv - ptrMin);
+        DAAL_ASSERT(_arrOffsets[i] >= 0)
+    }
+
+    return services::Status();
+}
+
+bool SOANumericTable::resizePointersArray(size_t nColumns)
+{
+    if (_arrays.size() >= nColumns)
+    {
+        size_t counter = 0;
+        for (size_t i = 0; i < nColumns; i++)
+        {
+            counter += (_arrays[i] != 0);
+        }
+        _arraysInitialized = counter;
+
+        if (_arraysInitialized == nColumns)
+        {
+            _memStatus = _partialMemStatus;
+        }
+        else
+        {
+            _memStatus = notAllocated;
+        }
+
+        return true;
+    }
+    _arrays.resize(nColumns);
+    _memStatus = notAllocated;
+
+    bool is_resized = _arrays.resize(nColumns);
+    if (is_resized)
+    {
+        _memStatus = notAllocated;
+    }
+
+    if (_arrOffsets)
+    {
+        daal::services::daal_free(_arrOffsets);
+        _arrOffsets = NULL;
+        _index      = 0;
+    }
+
+    return is_resized;
+}
+
+services::Status SOANumericTable::setNumberOfColumnsImpl(size_t ncol)
+{
+    services::Status s;
+    DAAL_CHECK_STATUS(s, NumericTable::setNumberOfColumnsImpl(ncol));
+
+    if (!resizePointersArray(ncol))
+    {
+        return services::Status(services::ErrorMemoryAllocationFailed);
+    }
+    return s;
+}
+
+void SOANumericTable::freeDataMemoryImpl()
+{
+    _arrays.clear();
+    _arrays.resize(_ddict->getNumberOfFeatures());
+    _arraysInitialized = 0;
+
+    _partialMemStatus = notAllocated;
+    _memStatus        = notAllocated;
+}
+
 services::SharedPtr<SOANumericTable> SOANumericTable::create(size_t nColumns, size_t nRows, DictionaryIface::FeaturesEqual featuresEqual,
                                                              services::Status * stat)
 {
