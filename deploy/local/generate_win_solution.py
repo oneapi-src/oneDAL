3f71e2295faa2112aa9e8cf81f70043a9c12b4f8
Ruslan Israfilov <ruslan.israfilov@intel.com>
2020-06-22 20:12:15 +0300

    Add license header

--- deploy/local/generate_win_solution.py
+++ deploy/local/generate_win_solution.py
@@ -1,3 +1,23 @@
+#===============================================================================
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#===============================================================================
+
+##  Content:
+##     Visual Studio Projects generator for examples
+##******************************************************************************
+
 import os
 import glob
 import uuid
@@ -110,9 +130,6 @@ def generate_proj(config, relative_example_path):
         example_guid = guid,
         example_name = example_name,
         example_relative_path = normalized_example_path,
-        dynamic_link_libs = 'onedal_dll.lib;daal_core_dll.lib',
-        static_link_libs_thread = 'onedal.lib;daal_core.lib;daal_thread.lib',
-        static_link_libs_seq = 'onedal.lib;daal_core.lib;daal_sequential.lib',
     )
     proj_filters = get_template(config, 'daal_win.vcxproj.filters').format(
         example_relative_path = normalized_example_path,
@@ -130,7 +147,7 @@ def find_all_examples(examples_dir):
 
 def generate(config):
     examples_info = []
-    for relative_path in find_all_examples(config.examples_dir): 
+    for relative_path in find_all_examples(config.examples_dir):
         guid, name = generate_proj(config, relative_path)
         examples_info.append((guid, name))
     generate_sln(config, examples_info)
