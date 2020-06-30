94ef7661d44581602c7c976492d21a54f5f3ab9e
Kirill <kirill.petrov@intel.com>
2020-06-30 18:07:03 +0300

    Implemented training and prediction of SVM for new interfaces (#674)

---
+++ cpp/oneapi/dal/algo/svm/backend/gpu/train_kernel_thunder.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+* Copyright 2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "oneapi/dal/algo/svm/backend/gpu/train_kernel.hpp"
+
+namespace oneapi::dal::svm::backend {
+
+template <typename Float>
+struct train_kernel_gpu<Float, task::classification, method::thunder> {
+    train_result operator()(const dal::backend::context_gpu& ctx,
+                            const descriptor_base& params,
+                            const train_input& input) const {
+        return train_result();
+    }
+};
+
+template struct train_kernel_gpu<float, task::classification, method::thunder>;
+template struct train_kernel_gpu<double, task::classification, method::thunder>;
+
+} // namespace oneapi::dal::svm::backend
---
+++ examples/oneapi/cpp/source/svm/svm_two_class_thunder_dense_batch.cpp
@@ -0,0 +1,89 @@
+/*******************************************************************************
+* Copyright 2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "oneapi/dal/data/accessor.hpp"
+#include "oneapi/dal/data/table.hpp"
+#include "oneapi/dal/algo/svm.hpp"
+
+#include "example_util/utils.hpp"
+
+using namespace oneapi;
+
+int main(int argc, char const *argv[]) {
+
+  constexpr std::int64_t row_count_train = 6;
+  constexpr std::int64_t column_count = 2;
+  const float x_train[] = {
+      -2.f, -1.f,
+      -1.f, -1.f,
+      -1.f, -2.f,
+      +1.f, +1.f,
+      +1.f, +2.f,
+      +2.f, +1.f,
+  };
+  const float y_train[] = {
+      -1.f,
+      -1.f,
+      -1.f,
+      +1.f,
+      +1.f,
+      +1.f,
+  };
+
+  const auto x_train_table =
+      dal::homogen_table{row_count_train, column_count, x_train};
+  const auto y_train_table = dal::homogen_table{row_count_train, 1, y_train};
+
+  const auto svm_desc = dal::svm::descriptor{}
+                            .set_c(1.0)
+                            .set_accuracy_threshold(0.01)
+                            .set_max_iteration_count(1000)
+                            .set_cache_size(200.0)
+                            .set_tau(1e-6);
+
+  const auto result_train = dal::train(svm_desc, x_train_table, y_train_table);
+
+  std::cout << "Bias:" << std::endl << result_train.get_bias() << std::endl;
+  std::cout << "Support indices:" << std::endl
+            << result_train.get_support_indices() << std::endl;
+
+  constexpr std::int64_t row_count_test = 3;
+  const float x_test[] = {
+      -1.f, -1.f,
+      +2.f, +2.f,
+      +3.f, +2.f,
+  };
+  const float y_true[] = {
+      -1.f,
+      +1.f,
+      +1.f,
+  };
+
+  const auto x_test_table =
+      dal::homogen_table{row_count_test, column_count, x_test};
+  const auto y_true_table = dal::homogen_table{row_count_test, 1, y_true};
+
+  const auto result_test =
+      dal::infer(svm_desc, result_train.get_model(), x_test_table);
+
+  std::cout << "Decision function result:" << std::endl
+            << result_test.get_decision_function() << std::endl;
+  std::cout << "Labels result:" << std::endl
+            << result_test.get_labels() << std::endl;
+  std::cout << "Labels true:" << std::endl << y_true_table << std::endl;
+
+  return 0;
+}
