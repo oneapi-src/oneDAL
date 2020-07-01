d3ca0f3b18460fb49a2eb2abb08abeac63eee8f4
amaklaev <artem.maklaev@intel.com>
2020-07-01 13:49:15 +0300

    build fixes

--- cpp/oneapi/dal/algo/decision_forest/backend/cpu/train_kernel_default_dense.cpp
+++ cpp/oneapi/dal/algo/decision_forest/backend/cpu/train_kernel_default_dense.cpp
@@ -20,8 +20,10 @@
 
 #include <daal/include/algorithms/decision_forest/decision_forest_classification_training_batch.h>
 #include <daal/include/algorithms/decision_forest/decision_forest_classification_training_types.h>
-#include <daal/src/algorithms/dtrees/forest/classification/df_classification_train_dense_default_kernel.h>
+
 #include <daal/src/algorithms/dtrees/forest/classification/df_classification_train_kernel.h>
+//to prevent reordering by clang-format
+#include <daal/src/algorithms/dtrees/forest/classification/df_classification_train_dense_default_kernel.h>
 
 #include "oneapi/dal/algo/decision_forest/backend/cpu/train_kernel.hpp"
 #include "oneapi/dal/backend/interop/common.hpp"
