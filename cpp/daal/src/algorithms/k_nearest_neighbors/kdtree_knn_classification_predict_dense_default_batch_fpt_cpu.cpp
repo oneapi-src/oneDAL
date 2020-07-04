03051a107d9f949c1410affd957dbd5a797c5110
agrigori <alexey.grigoriev@intel.com>
2020-07-04 17:25:31 +0300

    Fixed uresolved externals in win build

--- cpp/daal/src/algorithms/k_nearest_neighbors/kdtree_knn_classification_predict_dense_default_batch_fpt_cpu.cpp
+++ cpp/daal/src/algorithms/k_nearest_neighbors/kdtree_knn_classification_predict_dense_default_batch_fpt_cpu.cpp
@@ -40,7 +40,7 @@ template class BatchContainer<DAAL_FPTYPE, defaultDense, DAAL_CPU>;
 
 namespace internal
 {
-template class KNNClassificationPredictKernel<DAAL_FPTYPE, defaultDense, DAAL_CPU>;
+template class DAAL_EXPORT KNNClassificationPredictKernel<DAAL_FPTYPE, defaultDense, DAAL_CPU>;
 
 } // namespace internal
 } // namespace prediction
