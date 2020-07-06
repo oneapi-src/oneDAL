af18fe387b9a65a3ee6befe566089feed71a27bb
Kirill Petrov <kirill.petrov@intel.com>
2020-07-06 16:29:59 +0300

    fix checks

--- examples/oneapi/dpc/source/linear_kernel/linear_kernel_dense_batch.cpp
+++ examples/oneapi/dpc/source/linear_kernel/linear_kernel_dense_batch.cpp
@@ -1,18 +1,18 @@
 /*******************************************************************************
- * Copyright 2020 Intel Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+* Copyright 2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
 
 #include <CL/sycl.hpp>
 
@@ -50,8 +50,8 @@ void run(sycl::queue &queue) {
     auto y = sycl::malloc_shared<float>(row_count_y * column_count, queue);
     queue.memcpy(y, y_host, sizeof(float) * row_count_y * column_count).wait();
 
-    const auto x_table = dal::homogen_table{queue, row_count_x, column_count, x};
-    const auto y_table = dal::homogen_table{queue, row_count_y, column_count, y};
+    const auto x_table = dal::homogen_table{ queue, row_count_x, column_count, x };
+    const auto y_table = dal::homogen_table{ queue, row_count_y, column_count, y };
     const auto kernel_desc = dal::linear_kernel::descriptor{}
         .set_k(2.0)
         .set_b(1.0);
