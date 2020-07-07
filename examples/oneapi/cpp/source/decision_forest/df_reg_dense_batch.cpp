5752b859e021b5ce8df86eabaaea7893bf566d72
amaklaev <artem.maklaev@intel.com>
2020-07-07 11:36:30 +0300

    updated example for regression

--- examples/oneapi/cpp/source/decision_forest/df_reg_dense_batch.cpp
+++ examples/oneapi/cpp/source/decision_forest/df_reg_dense_batch.cpp
@@ -21,57 +21,63 @@ using namespace oneapi;
 namespace df = oneapi::dal::decision_forest;
 
 int main(int argc, char const *argv[]) {
-    constexpr std::int64_t row_count_train = 6;
-    constexpr std::int64_t row_count_test  = 3;
-    constexpr std::int64_t column_count    = 2;
+  constexpr std::int64_t row_count_train = 10;
+  constexpr std::int64_t row_count_test = 5;
+  constexpr std::int64_t column_count = 2;
 
-    const float x_train[] = {
-        -2.f, -1.f, -1.f, -1.f, -1.f, -2.f, +1.f, +1.f, +1.f, +2.f, +2.f, +1.f,
-    };
-    const float y_train[] = {
-        0.f, 0.f, 0.f, 1.f, 1.f, 1.f,
-    };
+  const float x_train[] = {
+      0.1f,  0.25f, 0.15f, 0.35f, 0.25f, 0.55f, 0.3f, 0.65f, 0.4f, 0.85f,
+      0.45f, 0.95f, 0.55f, 1.15f, 0.6f,  1.25f, 0.7f, 1.45f, 0.8f, 1.65f,
+  };
 
-    const float x_test[] = {
-        -1.f, -1.f, 2.f, 2.f, 3.f, 2.f,
-    };
-    const float y_test[] = {
-        0.f,
-        1.f,
-        1.f,
-    };
+  const float y_train[] = {
+      0.0079f, 0.0160f, 0.0407f, 0.0573f, 0.0989f,
+      0.1240f, 0.1827f, 0.2163f, 0.2919f, 0.3789f,
+  };
 
-    const auto x_train_table = dal::homogen_table{ row_count_train, column_count, x_train };
-    const auto y_train_table = dal::homogen_table{ row_count_train, 1, y_train };
+  const float x_test[] = {
+      0.2f, 0.45f, 0.35f, 0.75f, 0.5f, 1.05f, 0.65f, 1.35f, 0.75f, 1.55f,
+  };
 
-    const auto x_test_table = dal::homogen_table{ row_count_test, column_count, x_test };
-    const auto y_test_table = dal::homogen_table{ row_count_test, 1, y_test };
+  const float y_test[] = {
+      0.0269f, 0.0767f, 0.1519f, 0.2527f, 0.3340f,
+  };
+  const auto x_train_table =
+      dal::homogen_table{row_count_train, column_count, x_train};
+  const auto y_train_table = dal::homogen_table{row_count_train, 1, y_train};
 
-    const auto df_desc =
-        df::descriptor<float, df::task::regression, df::method::dense>{}
-            .set_tree_count(10)
-            .set_features_per_node(1)
-            .set_min_observations_in_leaf_node(2)
-            .set_variable_importance_mode(df::variable_importance_mode::mdi)
-            .set_train_results_to_compute(
-                (std::uint64_t)df::train_result_to_compute::compute_out_of_bag_error)
-            .set_infer_results_to_compute(
-                (std::uint64_t)df::infer_result_to_compute::compute_class_labels);
+  const auto x_test_table =
+      dal::homogen_table{row_count_test, column_count, x_test};
+  const auto y_test_table = dal::homogen_table{row_count_test, 1, y_test};
 
-    const auto result_train = dal::train(df_desc, x_train_table, y_train_table);
+  const auto df_desc =
+      df::descriptor<float, df::task::regression, df::method::dense>{}
+          .set_tree_count(10)
+          .set_features_per_node(1)
+          .set_min_observations_in_leaf_node(1)
+          .set_variable_importance_mode(df::variable_importance_mode::mdi)
+          .set_train_results_to_compute(
+              (std::uint64_t)
+                  df::train_result_to_compute::compute_out_of_bag_error)
+          .set_infer_results_to_compute(
+              (std::uint64_t)df::infer_result_to_compute::compute_class_labels);
 
-    std::cout << "Variable importance results: " << std::endl
-              << result_train.get_var_importance() << std::endl;
+  const auto result_train = dal::train(df_desc, x_train_table, y_train_table);
 
-    std::cout << "OOB error: " << result_train.get_oob_err() << std::endl;
+  std::cout << "Variable importance results: " << std::endl
+            << result_train.get_var_importance() << std::endl;
 
-    /* infer part in progress */
+  std::cout << "OOB error: " << result_train.get_oob_err() << std::endl;
 
-    const auto result_infer = dal::infer(df_desc, result_train.get_model(), x_test_table);
+  /* infer part in progress */
 
-    std::cout << "Prediction results: " << std::endl << result_infer.get_labels() << std::endl;
+  const auto result_infer =
+      dal::infer(df_desc, result_train.get_model(), x_test_table);
 
-    std::cout << "Ground truth: " << std::endl << y_test_table << std::endl;
+  std::cout << "Prediction results: " << std::endl
+            << result_infer.get_labels() << std::endl;
 
-    return 0;
+  std::cout << "Ground truth: " << std::endl << y_test_table << std::endl;
+
+  return 0;
 }
