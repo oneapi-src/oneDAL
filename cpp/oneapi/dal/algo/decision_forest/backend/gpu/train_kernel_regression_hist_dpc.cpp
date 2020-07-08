d7237cf087eaff9d3e76e108615e266253d74c83
amaklaev <artem.maklaev@intel.com>
2020-07-08 18:58:10 +0300

    added correct header for exceptions

--- cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_kernel_regression_hist_dpc.cpp
+++ cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_kernel_regression_hist_dpc.cpp
@@ -30,7 +30,7 @@
 #include "oneapi/dal/algo/decision_forest/backend/gpu/train_kernel.hpp"
 #include "oneapi/dal/algo/decision_forest/backend/interop_helpers.hpp"
 #include "oneapi/dal/backend/interop/common_dpc.hpp"
-#include "oneapi/dal/backend/interop/error_converter.hpp"
+#include "oneapi/dal/exceptions.hpp"
 #include "oneapi/dal/backend/interop/table_conversion.hpp"
 #include "oneapi/dal/detail/common.hpp"
 
