5ae9546a06832bc6ac9c9a69266e9622941d22e7
Petrov, Nikolay A <nikolay.a.petrov@intel.com>
2020-08-07 08:10:52 +0300

    Fix for java examples

--- examples/daal/java/launcher_lnx.sh
+++ examples/daal/java/launcher_lnx.sh
@@ -91,7 +91,7 @@ for example in "${Java_example_list[@]}"; do
     fi
 # Running examples
     if [ "${rmode}" != "build" ]; then
-        arr=("${example//// }")
+        arr=(${example//// })
         if [ -z "${arr[2]}" ]; then
             example_dir=${arr[0]}
             example_name=${arr[1]}
@@ -107,7 +107,8 @@ for example in "${Java_example_list[@]}"; do
             example_path=com.intel.daal.examples.${example_dir}.${example_name}
             res_path=${result_folder}/${example_dir}/${example_name}.res
 
-            java -Xmx${memory} -Djava.library.path=${Djava_library_path} "${example_path}" >"${res_path}" 2>&1
+            java_call="java -Xmx${memory} -Djava.library.path=${Djava_library_path} ${example_path}"
+            ${java_call} >> "${res_path}" 2>&1
             errcode=$?
             if [ "${errcode}" == "0" ]; then
                  echo -e "$(date +'%H:%M:%S') PASSED\t\t${example_name}"
--- examples/daal/java/launcher_mac.sh
+++ examples/daal/java/launcher_mac.sh
@@ -78,7 +78,7 @@ for example in "${Java_example_list[@]}"; do
     fi
 # Running examples
     if [ "${rmode}" != "build" ]; then
-        arr=("${example//// }")
+        arr=(${example//// })
         if [ -z "${arr[2]}" ]; then
             example_dir=${arr[0]}
             example_name=${arr[1]}
@@ -93,7 +93,8 @@ for example in "${Java_example_list[@]}"; do
             example_path=com.intel.daal.examples.${example_dir}.${example_name}
             res_path=${result_folder}/${example_dir}/${example_name}.res
 
-            java -Djava.library.path=${Djava_library_path} "${example_path}" >"${res_path}" 2>&1
+            java_call="java -Xmx${memory} -Djava.library.path=${Djava_library_path} ${example_path}"
+            ${java_call} > "${res_path}" 2>&1
             errcode=$?
             if [ "${errcode}" == "0" ]; then
                  echo -e "$(date +'%H:%M:%S') PASSED\t\t${example_name}"
