06f885268dad37ee934dcad6538bc70bb32e2bf6
teabagk7 <konstantin.nektyagaev@intel.com>
2020-08-11 14:56:27 +0300

    sourcing tbb from oneapi

--- .ci/scripts/daal4py_test.sh
+++ .ci/scripts/daal4py_test.sh
@@ -31,8 +31,8 @@ export PATH=${CONDA_DIR}/bin:$PATH
 source activate conf
 conda list
 source ${ONEAPI_DIR}/compiler/latest/env/vars.sh
+source ${ONEAPI_DIR}/tbb/latest/env/varsh.sh
 source ${BUILD_DIR}/daal/latest/env/vars.sh intel64
-export TBBROOT=${BUILD_DIR}/tbb/latest
 cd ${DAAL4PY_DIR}
 
 # testing
