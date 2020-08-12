6235372f1b16ea828b8fb7a1d502f711cd055ca2
agrigori <alexey.grigoriev@intel.com>
2020-08-12 16:11:19 +0300

    Cut off distributed kmeans example for debugging

--- examples/daal/cpp_sycl/source/kmeans/kmeans_dense_distr.cpp
+++ examples/daal/cpp_sycl/source/kmeans/kmeans_dense_distr.cpp
@@ -58,13 +58,10 @@ int main(int argc, char * argv[])
         daal::services::SyclExecutionContext ctx(queue);
         services::Environment::getInstance()->setDefaultExecutionContext(ctx);
 
-        kmeans::Distributed<step2Master> masterAlgorithm(nClusters);
 
         NumericTablePtr data[nBlocks];
 
         NumericTablePtr centroids;
-        NumericTablePtr assignments[nBlocks];
-        NumericTablePtr objectiveFunction;
 
         kmeans::init::Distributed<step2Master, algorithmFPType, kmeans::init::randomDense> masterInit(nClusters);
         for (size_t i = 0; i < nBlocks; i++)
@@ -89,49 +86,6 @@ int main(int argc, char * argv[])
         masterInit.finalizeCompute();
         centroids = masterInit.getResult()->get(kmeans::init::centroids);
 
-        /* Calculate centroids */
-        for (size_t it = 0; it < nIterations; it++)
-        {
-            for (size_t i = 0; i < nBlocks; i++)
-            {
-                /* Create an algorithm object for the K-Means algorithm */
-                kmeans::Distributed<step1Local> localAlgorithm(nClusters, false);
-
-                /* Set the input data to the algorithm */
-                localAlgorithm.input.set(kmeans::data, data[i]);
-                localAlgorithm.input.set(kmeans::inputCentroids, centroids);
-
-                localAlgorithm.compute();
-
-                masterAlgorithm.input.add(kmeans::partialResults, localAlgorithm.getPartialResult());
-            }
-
-            masterAlgorithm.compute();
-            masterAlgorithm.finalizeCompute();
-
-            centroids         = masterAlgorithm.getResult()->get(kmeans::centroids);
-            objectiveFunction = masterAlgorithm.getResult()->get(kmeans::objectiveFunction);
-        }
-
-        /* Calculate assignments */
-        for (size_t i = 0; i < nBlocks; i++)
-        {
-            /* Create an algorithm object for the K-Means algorithm */
-            kmeans::Batch<> localAlgorithm(nClusters, 0);
-
-            /* Set the input data to the algorithm */
-            localAlgorithm.input.set(kmeans::data, data[i]);
-            localAlgorithm.input.set(kmeans::inputCentroids, centroids);
-
-            localAlgorithm.compute();
-
-            assignments[i] = localAlgorithm.getResult()->get(kmeans::assignments);
-        }
-
-        /* Print the clusterization results */
-        printNumericTable(assignments[0], "First 10 cluster assignments from 1st node:", 10);
-        printNumericTable(centroids, "First 10 dimensions of centroids:", 20, 10);
-        printNumericTable(objectiveFunction, "Objective function value:");
     }
 
     return 0;
