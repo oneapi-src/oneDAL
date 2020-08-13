592138346cdc3ed8adbbabb010c370bb8630f1d5
KulikovNikita <nikita.kulikov@intel.com>
2020-08-13 17:55:17 +0300

    Some utilities

--- cpp/oneapi/dal/backend/primitives/common.hpp
+++ cpp/oneapi/dal/backend/primitives/common.hpp
@@ -16,8 +16,65 @@
 
 #pragma once
 
+#include <cstdint>
+
 namespace oneapi::dal::backend::primitives {
 
 enum class layout : int { row_major = 0, col_major = 1 };
+
+template <int size>
+struct unsigned_type {};
+
+template <>
+struct unsigned_type<1> {
+    typedef std::uint8_t type;
+    constexpr inline static type ones    = 0xff;
+    constexpr inline static type zeros   = 0x00;
+    constexpr inline static type vals[2] = { zeros, ones };
+    constexpr inline static type val(bool condition) {
+        return vals[int(condition)];
+    }
+};
+
+template <>
+struct unsigned_type<2> {
+    typedef std::uint16_t type;
+    constexpr inline static type ones    = 0xffff;
+    constexpr inline static type zeros   = 0x0000;
+    constexpr inline static type vals[2] = { zeros, ones };
+    constexpr inline static type val(bool condition) {
+        return vals[int(condition)];
+    }
+};
+
+template <>
+struct unsigned_type<4> {
+    typedef std::uint32_t type;
+    constexpr inline static type ones    = 0xffffffff;
+    constexpr inline static type zeros   = 0x00000000;
+    constexpr inline static type vals[2] = { zeros, ones };
+    constexpr inline static type val(bool condition) {
+        return vals[int(condition)];
+    }
+};
+
+template <>
+struct unsigned_type<8> {
+    typedef std::uint64_t type;
+    constexpr inline static type ones    = 0xffffffffffffffff;
+    constexpr inline static type zeros   = 0x0000000000000000;
+    constexpr inline static type vals[2] = { zeros, ones };
+    constexpr inline static type val(bool condition) {
+        return vals[int(condition)];
+    }
+};
+
+template <typename T>
+inline void conditional_swap(T& a, T& b, bool swap = true) {
+    constexpr unsigned_type<sizeof(T)> ut;
+    reinterpret_cast<ut::type&>(a) ^= reinterpret_cast<ut::type&>(b);
+    reinterpret_cast<ut::type&>(b) ^= ut.val(swap) & reinterpret_cast<ut::type&>(a);
+    reinterpret_cast<ut::type&>(a) ^= reinterpret_cast<ut::type&>(b);
+}
 
 } // namespace oneapi::dal::backend::primitives
