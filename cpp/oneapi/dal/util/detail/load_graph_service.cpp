5343f118e1785ff3ab3350d2301e2d3bdc3f7602
Olga Razvenskaya <olga.razvenskaya@intel.com>
2020-08-16 12:58:08 +0300

    threading fixes

--- cpp/oneapi/dal/util/detail/load_graph_service.cpp
+++ cpp/oneapi/dal/util/detail/load_graph_service.cpp
@@ -15,9 +15,12 @@
 *******************************************************************************/
 
 #include "oneapi/dal/util/detail/load_graph_service.hpp"
+#include <iostream>
 #include "oneapi/dal/detail/common.hpp"
 #include "src/externals/service_service.h"
+#include "src/threading/threading.h"
 
+using namespace std;
 namespace oneapi::dal::preview::load_graph::detail {
 
 ONEAPI_DAL_EXPORT int daal_string_to_int(const char* nptr, char** endptr) {
@@ -25,42 +28,9 @@ ONEAPI_DAL_EXPORT int daal_string_to_int(const char* nptr, char** endptr) {
 }
 } // namespace oneapi::dal::preview::load_graph::detail
 
-#if defined(__DO_TBB_LAYER__)
-    #define TBB_PREVIEW_GLOBAL_CONTROL 1
-    #define TBB_PREVIEW_TASK_ARENA     1
-
-    #include <stdlib.h> // malloc and free
-    #include <tbb/global_control.h>
-    #include <tbb/scalable_allocator.h>
-    #include <tbb/spin_mutex.h>
-    #include <tbb/task_arena.h>
-    #include <tbb/tbb.h>
-    #include "services/daal_atomic_int.h"
-
-    #if defined(TBB_INTERFACE_VERSION) && TBB_INTERFACE_VERSION >= 12002
-        #include <tbb/task.h>
-    #endif
-
-using namespace daal::services;
-#else
-    #include "src/externals/service_service.h"
-#endif
-
 ONEAPI_DAL_EXPORT void _daal_threader_for_oneapi(int n,
                                                  int threads_request,
                                                  const void* a,
                                                  oneapi::dal::preview::functype func) {
-#if defined(__DO_TBB_LAYER__)
-    tbb::parallel_for(tbb::blocked_range<int>(0, n, 1), [&](tbb::blocked_range<int> r) {
-        int i;
-        for (i = r.begin(); i < r.end(); i++) {
-            func(i, a);
-        }
-    });
-#elif defined(__DO_SEQ_LAYER__)
-    int i;
-    for (i = 0; i < n; i++) {
-        func(i, a);
-    }
-#endif
+    _daal_threader_for(n, threads_request, a, static_cast<daal::functype>(func));
 }
