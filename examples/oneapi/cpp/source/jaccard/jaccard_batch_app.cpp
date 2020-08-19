7478f6ba1be47918713a97d50e19be2facbd1fb6
aleksey.kumin <akumin@nnldaal051.inn.intel.com>
2020-08-19 04:39:15 +0300

    true threading app vers

--- examples/oneapi/cpp/source/jaccard/jaccard_batch_app.cpp
+++ examples/oneapi/cpp/source/jaccard/jaccard_batch_app.cpp
@@ -14,31 +14,30 @@
  * limitations under the License.
  *******************************************************************************/
 
+#include <chrono>
 #include <iostream>
-#include <chrono> 
 
 #include "example_util/utils.hpp"
 #include "oneapi/dal/algo/jaccard.hpp"
+#include "oneapi/dal/data/graph_service_functions.hpp"
 #include "oneapi/dal/data/table.hpp"
 #include "oneapi/dal/data/undirected_adjacency_array_graph.hpp"
 #include "oneapi/dal/util/csv_data_source.hpp"
 #include "oneapi/dal/util/load_graph.hpp"
-#include "oneapi/dal/data/graph_service_functions.hpp"
 
-#include "tbb/parallel_for.h"
 #include "tbb/global_control.h"
+#include "tbb/parallel_for.h"
 
 using namespace oneapi::dal;
 using namespace oneapi::dal::preview;
 using namespace std;
 using namespace std::chrono;
 
-
-// input parameters - processed graph, size of block by row, size of block by column
-// algorithm process only upper triangular matrix
-template<class Graph> 
-void vertex_similarity_block_processing(const Graph &g, const int32_t row_size, const int32_t column_size) 
-{
+// input parameters - processed graph, size of block by row, size of block by
+// column algorithm process only upper triangular matrix
+template <class Graph>
+void vertex_similarity_block_processing(const Graph &g, const int32_t row_size,
+                                        const int32_t column_size) {
   // comupte the number of the vertex pairs in the block of the graph
   auto vertex_pairs_count = row_size * column_size;
   auto vertex_pair_element_count = 2;   // 2 elements in the vertex pair
@@ -50,10 +49,13 @@ void vertex_similarity_block_processing(const Graph &g, const int32_t row_size, 
       jaccard_coeff_element_count * sizeof(float); // size in bytes
   // compute the maximal required memory for the result of the block processing
   // in bytes
-  auto block_result_size =
-      (vertex_pair_element_count * vertex_pair_size + jaccard_coeff_element_count * jaccard_coeff_size) * vertex_pairs_count;
-  std::vector< std::vector<byte_t>> processing_blocks(tbb::this_task_arena::max_concurrency(), std::vector<byte_t> (block_result_size)); // memory for all threads
-  //start block precessing
+  auto block_result_size = (vertex_pair_element_count * vertex_pair_size +
+                            jaccard_coeff_element_count * jaccard_coeff_size) *
+                           vertex_pairs_count;
+  std::vector<std::vector<byte_t>> processing_blocks(
+      tbb::this_task_arena::max_concurrency(),
+      std::vector<byte_t>(block_result_size)); // memory for all threads
+  // start block precessing
   int32_t vertex_count = get_vertex_count(g);
   int32_t rows_count = vertex_count / row_size; // the number rows
   if (vertex_count % row_size) {
@@ -66,8 +68,10 @@ void vertex_similarity_block_processing(const Graph &g, const int32_t row_size, 
           // compute range for row
           int32_t block_begin_row = i * row_size;
           int32_t block_end_row = (i + 1) * row_size;
-          int32_t begin_column = 1 + block_begin_row; // start column ranges from diagonal
-          int32_t columns_count = (vertex_count - begin_column) / column_size; // the number columns
+          int32_t begin_column =
+              1 + block_begin_row; // start column ranges from diagonal
+          int32_t columns_count =
+              (vertex_count - begin_column) / column_size; // the number columns
           if ((vertex_count - begin_column) % column_size) {
             columns_count++;
           }
@@ -77,58 +81,47 @@ void vertex_similarity_block_processing(const Graph &g, const int32_t row_size, 
                 for (int j = inner_r.begin(); j != inner_r.end(); ++j) {
                   // compute range for column
                   int32_t block_begin_column = begin_column + j * column_size;
-                  int32_t block_end_column = begin_column + (j + 1) * column_size;
-                  //set block ranges
-                  const auto jaccard_desc_default = jaccard::descriptor<>().set_block(
-                  {block_begin_row, std::min(block_end_row, vertex_count)},
-                  {block_begin_column, std::min(block_end_column, vertex_count)});
-                  //compute coeffs
-                  vertex_similarity(jaccard_desc_default, g,
-                        static_cast<void *>((processing_blocks[tbb::this_task_arena::current_thread_index()]).data()));
-
-                }},tbb::simple_partitioner{});
-        }}, tbb::simple_partitioner{});
+                  int32_t block_end_column =
+                      begin_column + (j + 1) * column_size;
+                  // set block ranges
+                  const auto jaccard_desc_default =
+                      jaccard::descriptor<>().set_block(
+                          {block_begin_row,
+                           std::min(block_end_row, vertex_count)},
+                          {block_begin_column,
+                           std::min(block_end_column, vertex_count)});
+                  // compute coeffs
+                  vertex_similarity(
+                      jaccard_desc_default, g,
+                      static_cast<void *>(
+                          (processing_blocks
+                               [tbb::this_task_arena::current_thread_index()])
+                              .data()));
+                }
+              },
+              tbb::simple_partitioner{});
+        }
+      },
+      tbb::simple_partitioner{});
 }
 
 int main(int argc, char **argv) {
   // read the graph
-    if (argc < 2) return 0;
-    std::string filename = argv[1];
-    csv_data_source ds(filename);
+  if (argc < 2)
+    return 0;
+  std::string filename = argv[1];
+  csv_data_source ds(filename);
   load_graph::descriptor<> d;
   auto my_graph = load_graph::load(d, ds);
 
+  int32_t row_size = 1024;
+  int32_t column_size = 1024;
+  int32_t tbb_threads_number = 1;
 
+  tbb::global_control c(tbb::global_control::max_allowed_parallelism,
+                        tbb_threads_number);
 
-    int num_trials_custom = 1;
-    int num_trials_lib = 1;
+  vertex_similarity_block_processing(my_graph, row_size, column_size);
 
-    int32_t row_size = 1;
-    int32_t column_size = 1024;
-
-    int32_t tbb_threads_number = 1;
-
-    if (argc > 6) {
-        row_size = stoi(argv[2]);
-        column_size = stoi(argv[3]);
-        num_trials_custom = stoi(argv[4]);
-        tbb_threads_number = stoi(argv[5]);
-    }
-      tbb::global_control c(tbb::global_control::max_allowed_parallelism, tbb_threads_number);
-    //cout << "jaccard_non_existent with custom block_size = " << row_size <<"i" <<column_size << endl;
-    vector<double> time;
-    double median;
-
-    for(int i = 0; i < num_trials_custom; i++) {
-        auto start = high_resolution_clock::now();
-        vertex_similarity_block_processing(my_graph, row_size, column_size);
-        auto stop = high_resolution_clock::now();
-
-        time.push_back(std::chrono::duration_cast<std::chrono::duration<double>>(stop - start).count());
-        cout << i << " iter: " << time.back() << endl;
-    }
-return 0;
+  return 0;
 }
-
-
-
