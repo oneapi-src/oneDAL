ee640e8340cc2a6489f1739d47fcb28f61509161
Kirill Petrov <kirill.petrov@intel.com>
2020-08-20 19:27:32 +0300

    fix

--- cpp/oneapi/dal/algo/svm/backend/utils.hpp
+++ cpp/oneapi/dal/algo/svm/backend/utils.hpp
@@ -29,20 +29,21 @@ static array<Float> convert_labels(const array<Float>& arr_label,
                                    const binary_label_t<Float>& in_binary_labels,
                                    binary_label_t<Float>& out_binary_labels) {
     const std::int64_t count = arr_label.get_count();
-    auto arr_new_label       = array<Float>::empty(count);
+    auto new_label_arr       = array<Float>::empty(count);
+    auto new_label_data      = new_label_arr.get_mutable_data();
 
     Float value_first_class_label  = arr_label[0];
     Float value_second_class_label = arr_label[1];
 
-    arr_new_label[0] = in_binary_labels.first;
-    std::int64_t i   = 1;
+    new_label_data[0] = in_binary_labels.first;
+    std::int64_t i    = 1;
     for (; i < count; ++i) {
         if (arr_label[i] == value_first_class_label) {
-            arr_new_label[i] = in_binary_labels.first;
+            new_label_data[i] = in_binary_labels.first;
         }
         else {
             value_second_class_label = arr_label[i];
-            arr_new_label[i]         = in_binary_labels.second;
+            new_label_data[i]        = in_binary_labels.second;
             break;
         }
     }
@@ -52,10 +53,10 @@ static array<Float> convert_labels(const array<Float>& arr_label,
 
     for (; i < count; ++i) {
         if (arr_label[i] == value_first_class_label) {
-            arr_new_label[i] = in_binary_labels.first;
+            new_label_data[i] = in_binary_labels.first;
         }
         else if (arr_label[i] == value_second_class_label) {
-            arr_new_label[i] = in_binary_labels.second;
+            new_label_data[i] = in_binary_labels.second;
         }
         else {
             throw invalid_argument("Input label data should have only two unique labels");
@@ -64,7 +65,7 @@ static array<Float> convert_labels(const array<Float>& arr_label,
 
     out_binary_labels.first  = value_first_class_label;
     out_binary_labels.second = value_second_class_label;
-    return arr_new_label;
+    return new_label_data;
 }
 
 #ifdef ONEAPI_DAL_DATA_PARALLEL
@@ -75,20 +76,22 @@ static array<Float> convert_labels(const sycl::queue& queue,
                                    const binary_label_t<Float>& in_binary_labels,
                                    binary_label_t<Float>& out_binary_labels) {
     // TODO: make for dpcpp kernel
-    const std::int64_t count       = arr_label.get_count();
-    auto arr_new_label             = array<Float>::empty(queue, count);
+    const std::int64_t count = arr_label.get_count();
+    auto new_label_arr       = array<Float>::empty(queue, count);
+    auto new_label_data      = new_label_arr.get_mutable_data();
+
     Float value_first_class_label  = arr_label[0];
     Float value_second_class_label = arr_label[1];
 
-    arr_new_label[0] = in_binary_labels.first;
-    std::int64_t i   = 1;
+    new_label_data[0] = in_binary_labels.first;
+    std::int64_t i    = 1;
     for (; i < count; ++i) {
         if (arr_label[i] == value_first_class_label) {
-            arr_new_label[i] = in_binary_labels.first;
+            new_label_data[i] = in_binary_labels.first;
         }
         else {
             value_second_class_label = arr_label[i];
-            arr_new_label[i]         = in_binary_labels.second;
+            new_label_data[i]        = in_binary_labels.second;
             break;
         }
     }
@@ -98,10 +101,10 @@ static array<Float> convert_labels(const sycl::queue& queue,
 
     for (; i < count; ++i) {
         if (arr_label[i] == value_first_class_label) {
-            arr_new_label[i] = in_binary_labels.first;
+            new_label_data[i] = in_binary_labels.first;
         }
         else if (arr_label[i] == value_second_class_label) {
-            arr_new_label[i] = in_binary_labels.second;
+            new_label_data[i] = in_binary_labels.second;
         }
         else {
             throw invalid_argument("Input label data should have only two unique labels");
@@ -111,7 +114,7 @@ static array<Float> convert_labels(const sycl::queue& queue,
     out_binary_labels.first  = value_first_class_label;
     out_binary_labels.second = value_second_class_label;
 
-    return arr_new_label;
+    return new_label_data;
 }
 
 #endif
