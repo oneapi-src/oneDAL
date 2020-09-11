bc8f75f02c9343fa90174ad3d3745de0881dc586
Kirill Petrov <kirill.petrov@intel.com>
2020-09-11 12:26:11 +0300

    fix prints in examples

--- examples/java/com/intel/daal/examples/svm/SVMMultiClassBoserCSRBatch.java
+++ examples/java/com/intel/daal/examples/svm/SVMMultiClassBoserCSRBatch.java
@@ -148,8 +148,8 @@ class SVMMultiClassBoserCSRBatch {
         NumericTable predictionResults = predictionResult.get(PredictionResultId.prediction);
         Service.printClassificationResult(testGroundTruth, predictionResults, "Ground truth", "Classification results",
                 "Multi-class SVM classification sample program results (first 20 observations):", 20);
+        System.out.println("");
         Service.printNumericTable("Multi-class SVM classification decision function results (first 20 observations):",
                  predictionResult.get(PredictionResultId.decisionFunction) , 20);
-        System.out.println("");
     }
 }
