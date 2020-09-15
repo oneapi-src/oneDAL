7d3d877640cc89ed51edfccab3c28d4b65fbb11a
Pavel Yakovlev <35104773+Pahandrovich@users.noreply.github.com>
2020-09-15 23:34:22 +0300

    [2020-mnt] Add conformance tests for knn and forest (#938)
    Add conformance tests for knn and forest

--- .ci/scripts/conformance-scripts/download_tests.sh
+++ .ci/scripts/conformance-scripts/download_tests.sh
@@ -44,6 +44,15 @@ case ${ALG_NAME} in
     "svm")
         wget -O test_svm.py ${SKLEARN_URL_ROOT}svm/tests/test_svm.py
     ;;
+    "svm_sparse")
+        wget -O test_svm_sparse.py ${SKLEARN_URL_ROOT}svm/tests/test_sparse.py
+    ;;
+    "forest")
+        wget -O test_forest.py ${SKLEARN_URL_ROOT}ensemble/tests/test_forest.py
+    ;;
+    "knn")
+        wget -O test_knn.py ${SKLEARN_URL_ROOT}neighbors/tests/test_neighbors.py
+    ;;
     *)
         echo "Unknown algorithm: ${ALG_NAME}"
         exit 1
