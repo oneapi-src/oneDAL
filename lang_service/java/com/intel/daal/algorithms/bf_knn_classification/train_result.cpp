9d10366b7629b2de4ef379299f62e6ce6aeb5c57
ntimakin <nikita.timakin@intel.com>
2020-09-15 08:23:40 +0300

    Include paths changed

--- lang_service/java/com/intel/daal/algorithms/bf_knn_classification/train_result.cpp
+++ lang_service/java/com/intel/daal/algorithms/bf_knn_classification/train_result.cpp
@@ -19,7 +19,7 @@
 
 #include "com_intel_daal_algorithms_bf_knn_classification_training_TrainingResult.h"
 
-#include "com/intel/daal/common_helpers.h"
+#include "lang_service/java/com/intel/daal/include/common_helpers.h"
 
 #include "com_intel_daal_algorithms_classifier_training_TrainingResultId.h"
 #define ModelId com_intel_daal_algorithms_classifier_training_TrainingResultId_Model
