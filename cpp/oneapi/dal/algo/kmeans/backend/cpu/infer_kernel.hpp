ae8a1ac05da399ef0bbf7680acaa90366bc87ef5
Mikhail Chernov <55237517+mchernov-intel@users.noreply.github.com>
2020-09-22 12:21:01 +0300

    Conformance between spec and API: kmeans (#909)
    Conformance between spec and API: kmeans
    
    (cherry picked from commit 412adbdc376a9a8cce9c72117bf69afe4ce27c64)

--- cpp/oneapi/dal/algo/kmeans/backend/cpu/infer_kernel.hpp
+++ cpp/oneapi/dal/algo/kmeans/backend/cpu/infer_kernel.hpp
@@ -21,11 +21,11 @@
 
 namespace oneapi::dal::kmeans::backend {
 
-template <typename Float, typename Method>
+template <typename Float, typename Method, typename Task>
 struct infer_kernel_cpu {
-    infer_result operator()(const dal::backend::context_cpu& ctx,
-                            const descriptor_base& params,
-                            const infer_input& input) const;
+    infer_result<Task> operator()(const dal::backend::context_cpu& ctx,
+                                  const descriptor_base<Task>& params,
+                                  const infer_input<Task>& input) const;
 };
 
 } // namespace oneapi::dal::kmeans::backend
