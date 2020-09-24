e92154f5988445ab7bab8edbe3da4ad8f8f0fedc
Artem Aroslankin <artem.aroslankin@intel.com>
2020-09-24 16:01:33 +0300

    Aligning graph examples for using const + auto (#986)
    * Aligning examples for using const + auto
    
    * used alias for related to graph vertices variables in example
    
    * removed using  preview + std

--- examples/oneapi/cpp/source/jaccard/jaccard_batch.cpp
+++ examples/oneapi/cpp/source/jaccard/jaccard_batch.cpp
@@ -24,33 +24,34 @@
 #include "oneapi/dal/table/common.hpp"
 
 using namespace oneapi::dal;
-using namespace oneapi::dal::preview;
 
 int main(int argc, char **argv) {
   const std::string filename = get_data_path("graph.csv");
   // read the graph
-  graph_csv_data_source ds(filename);
-  load_graph::descriptor<> d;
-  auto my_graph = load_graph::load(d, ds);
+  const preview::graph_csv_data_source ds(filename);
+  const preview::load_graph::descriptor<> d;
+  const auto my_graph = preview::load_graph::load(d, ds);
 
   // set blocks ranges
-  auto row_range_begin = 0; auto row_range_end = 2;
-  auto column_range_begin = 0; auto column_range_end = 3;
+  const std::int64_t row_range_begin = 0;
+  const std::int64_t row_range_end = 2;
+  const std::int64_t column_range_begin = 0;
+  const std::int64_t column_range_end = 3;
 
   // set algorithm parameters
-  const auto jaccard_desc = jaccard::descriptor<>().set_block(
+  const auto jaccard_desc = preview::jaccard::descriptor<>().set_block(
       {row_range_begin, row_range_end}, {column_range_begin, column_range_end});
 
   // create caching builder for jaccard result
-  jaccard::caching_builder builder;
+  preview::jaccard::caching_builder builder;
 
   // compute Jaccard similarity coefficients
-  auto result_vertex_similarity = vertex_similarity(jaccard_desc, my_graph, builder);
+  const auto result_vertex_similarity = preview::vertex_similarity(jaccard_desc, my_graph, builder);
 
   // extract the result
-  auto jaccard_coeffs = result_vertex_similarity.get_coeffs();
-  auto vertex_pairs = result_vertex_similarity.get_vertex_pairs();
-  auto nonzero_coeff_count = result_vertex_similarity.get_nonzero_coeff_count();
+  const auto jaccard_coeffs = result_vertex_similarity.get_coeffs();
+  const auto vertex_pairs = result_vertex_similarity.get_vertex_pairs();
+  const std::int64_t nonzero_coeff_count = result_vertex_similarity.get_nonzero_coeff_count();
 
   std::cout << "The number of nonzero Jaccard coeffs in the block: "
             << nonzero_coeff_count << std::endl;
