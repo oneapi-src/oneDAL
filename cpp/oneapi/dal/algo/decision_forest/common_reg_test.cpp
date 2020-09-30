553a6b5092dd0c9f2c64d9cc6bdd8a5bbdc7fe3c
Artem Maklaev <55365370+makart19@users.noreply.github.com>
2020-09-30 20:10:45 +0300

    added call of gpu predictor in new interfaces (#1015)
    (cherry picked from commit c79a77ef5b1da765eeadbce76fedf36e8328b54a)

--- cpp/oneapi/dal/algo/decision_forest/common_reg_test.cpp
+++ cpp/oneapi/dal/algo/decision_forest/common_reg_test.cpp
@@ -140,8 +140,7 @@ TEST(df_bad_arg_tests, set_train_data) {
     const float y_train[] = { 0.f, 0.f, 0.f, 1.f, 1.f, 1.f };
 
     dal::homogen_table x_train_table;
-    const auto y_train_table =
-        dal::homogen_table{ y_train, row_count_train, 1, dal::empty_delete<const float>() };
+    const auto y_train_table = dal::homogen_table::wrap(y_train, row_count_train, 1);
 
     const auto df_desc = df::descriptor<float, df::task::regression, df::method::dense>{};
 
@@ -156,10 +155,7 @@ TEST(df_bad_arg_tests, set_train_labels) {
         -2.f, -1.f, -1.f, -1.f, -1.f, -2.f, +1.f, +1.f, +1.f, +2.f, +2.f, +1.f
     };
 
-    const auto x_train_table = dal::homogen_table{ x_train,
-                                                   row_count_train,
-                                                   column_count,
-                                                   dal::empty_delete<const float>() };
+    const auto x_train_table = dal::homogen_table::wrap(x_train, row_count_train, column_count);
     dal::homogen_table y_train_table;
 
     const auto df_desc = df::descriptor<float, df::task::regression, df::method::dense>{};
@@ -176,12 +172,8 @@ TEST(df_bad_arg_tests, set_bootstrap) {
     };
     const float y_train[] = { 0.f, 0.f, 0.f, 1.f, 1.f, 1.f };
 
-    const auto x_train_table = dal::homogen_table{ x_train,
-                                                   row_count_train,
-                                                   column_count,
-                                                   dal::empty_delete<const float>() };
-    const auto y_train_table =
-        dal::homogen_table{ y_train, row_count_train, 1, dal::empty_delete<const float>() };
+    const auto x_train_table = dal::homogen_table::wrap(x_train, row_count_train, column_count);
+    const auto y_train_table = dal::homogen_table::wrap(y_train, row_count_train, 1);
 
     // check bootstrap(false) and mda_raw
     {
@@ -232,12 +224,8 @@ TEST(df_bad_arg_tests, data_rows_matches_labels_rows) {
     };
     const float y_train[] = { 0.f, 0.f, 0.f, 1.f, 1.f, 1.f };
 
-    const auto x_train_table = dal::homogen_table{ x_train,
-                                                   row_count_train,
-                                                   column_count,
-                                                   dal::empty_delete<const float>() };
-    const auto y_train_table =
-        dal::homogen_table{ y_train, row_count_train_invalid, 1, dal::empty_delete<const float>() };
+    const auto x_train_table = dal::homogen_table::wrap(x_train, row_count_train, column_count);
+    const auto y_train_table = dal::homogen_table::wrap(y_train, row_count_train_invalid, 1);
 
     const auto df_desc = df::descriptor<float, df::task::regression, df::method::dense>{};
     ASSERT_THROW(dal::train(df_desc, x_train_table, y_train_table), dal::invalid_argument);
@@ -252,12 +240,8 @@ TEST(df_bad_arg_tests, set_infer_data) {
     };
     const float y_train[] = { 0.f, 0.f, 0.f, 1.f, 1.f, 1.f };
 
-    const auto x_train_table = dal::homogen_table{ x_train,
-                                                   row_count_train,
-                                                   column_count,
-                                                   dal::empty_delete<const float>() };
-    const auto y_train_table =
-        dal::homogen_table{ y_train, row_count_train, 1, dal::empty_delete<const float>() };
+    const auto x_train_table = dal::homogen_table::wrap(x_train, row_count_train, column_count);
+    const auto y_train_table = dal::homogen_table::wrap(y_train, row_count_train, 1);
     dal::homogen_table x_test_table;
 
     const auto df_desc = df::descriptor<float, df::task::regression, df::method::dense>{};
