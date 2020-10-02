5a73c3ab6cb0cc58ddafd6570b7efae66bbcde7e
rlnx <ruslan.is@outlook.com>
2020-10-02 14:59:13 -0700

    Remove Catch2 and Fmt deps

--- cpp/oneapi/dal/test/common.hpp
+++ cpp/oneapi/dal/test/common.hpp
@@ -21,9 +21,6 @@
 #include <iostream>
 #include <type_traits>
 
-#include <fmt/core.h>
-#include <catch2/catch.hpp>
-
 #include "oneapi/dal/train.hpp"
 #include "oneapi/dal/infer.hpp"
 #include "oneapi/dal/compute.hpp"
@@ -32,64 +29,6 @@
 
 // Disable clang-format as it dramatically
 // affects redability of macro definitions
-
-// clang-format off
-
-// Workaround DPC++ Compiler's warning on unused
-// variable declared by Catch2's TEST_CASE macro
-#ifdef __clang__
-#define _TS_DISABLE_UNUSED_VARIABLE  \
-    _Pragma("clang diagnostic push") \
-    _Pragma("clang diagnostic ignored \"-Wunused-variable\"")
-
-#define _TS_ENABLE_UNUSED_VARIABLE _Pragma("clang diagnostic pop")
-
-#undef TEST_CASE
-#define TEST_CASE(...)                   \
-    _TS_DISABLE_UNUSED_VARIABLE          \
-    INTERNAL_CATCH_TESTCASE(__VA_ARGS__) \
-    _TS_ENABLE_UNUSED_VARIABLE
-
-#undef TEMPLATE_TEST_CASE
-#define TEMPLATE_TEST_CASE(...)                    \
-    _TS_DISABLE_UNUSED_VARIABLE                    \
-    INTERNAL_CATCH_TEMPLATE_TEST_CASE(__VA_ARGS__) \
-    _TS_ENABLE_UNUSED_VARIABLE
-#endif
-
-#define _TS_STRINGIFY_ALGO_TYPES(Float, Method) \
-    " - (" _TS_STRINGIFY(Float) ", " _TS_STRINGIFY(Method) ")"
-
-#define _TS_STRINGIFY_ALGO_TAGS(Float, Method) _TS_STRINGIFY([Float][Method])
-
-#define _TS_HANDLE_ALGO_TYPES(ctx, x) \
-    _TS_HANDLE_ALGO_TYPES_IMPL(_TS_GET_0(ctx), _TS_GET_1(ctx), _TS_GET_0(x), _TS_GET_1(x))
-
-#define _TS_HANDLE_ALGO_TYPES_IMPL(test_case, function, Float, Method) \
-    TEST_CASE(test_case _TS_STRINGIFY_ALGO_TYPES(Float, Method),  \
-                        _TS_STRINGIFY_ALGO_TAGS(Float, Method)) { \
-        function<Float, Method>();                                \
-    }
-
-#define _TS_ALGO_TEST_CASE_FUNC_NAME(id) \
-    _TS_CONCAT_2(test_algo_, id)
-
-#define _TS_ALGO_TEST_CASE_FUNC_DECL(id)       \
-    template <typename Float, typename Method> \
-    void _TS_ALGO_TEST_CASE_FUNC_NAME(id)()
-
-#define REGISTER_ALGO_TEST_CASE(test_case, function, float_list, method_list) \
-    _TS_FOR_EACH((test_case, function), _TS_HANDLE_ALGO_TYPES, _TS_COMB(float_list, method_list))
-
-#define ALGO_TEST_CASE(test_case, float_list, method_list)          \
-    _TS_ALGO_TEST_CASE_FUNC_DECL(__LINE__);                         \
-    REGISTER_ALGO_TEST_CASE(test_case,                              \
-                            _TS_ALGO_TEST_CASE_FUNC_NAME(__LINE__), \
-                            float_list,                             \
-                            method_list)                            \
-    _TS_ALGO_TEST_CASE_FUNC_DECL(__LINE__)
-
-// clang-format on
 
 #ifdef ONEAPI_DAL_DATA_PARALLEL
 #define DECLARE_TEST_POLICY(policy_name) oneapi::dal::test::device_test_policy policy_name
