b09a32b61559a24056caf8c8386ef15f4529a996
amgrigoriev <55235634+amgrigoriev@users.noreply.github.com>
2020-10-13 17:45:39 +0300

    Modified DBSCAN algorithm for GPU (#1035)
    (cherry picked from commit d6c7598616e09da1733e676a3cb0afc49ecbcb3d)
    
    # Conflicts:
    #	cpp/daal/src/algorithms/dbscan/oneapi/dbscan_dense_default_batch_ucapi_impl.i

---
+++ cpp/daal/include/services/internal/sycl/daal_defines_sycl.h
@@ -0,0 +1,35 @@
+/* file: daal_defines_sycl.h */
+/*******************************************************************************
+* Copyright 2014-2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/*
+//++
+//  Common definitions.
+//--
+*/
+
+#ifndef __DAAL_DEFINES_SYCL_H__
+#define __DAAL_DEFINES_SYCL_H__
+
+/** \file daal_defines_sycl.h */
+
+#include "services/daal_defines.h"
+
+#define DAAL_ASSERT_UNIVERSAL_BUFFER(buffer, bufferType, bufferSize) \
+    DAAL_ASSERT(buffer.type() == TypeIds<bufferType>::id());         \
+    DAAL_ASSERT(buffer.template get<bufferType>().size() == bufferSize);
+
+#endif
