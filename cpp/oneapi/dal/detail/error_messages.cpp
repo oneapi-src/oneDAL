e81c0b6f4b0599dcccf8a44a395d274d4a0e4be9
Ruslan Israfilov <ruslan.israfilov@intel.com>
2020-10-26 20:53:39 +0300

    Messages placeholder

---
+++ cpp/oneapi/dal/detail/error_messages.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+* Copyright 2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "oneapi/dal/detail/error_messages.hpp"
+
+#define DEFINE_MESSAGE(id, text)                \
+    const char* error_messages::id() noexcept { \
+        return text;                            \
+    }
+
+namespace oneapi::dal::detail {
+
+} // namespace oneapi::dal::detail
