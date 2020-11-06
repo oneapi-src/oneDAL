0b7bbe42e917cb0ffde7c5c95e8d7001dd6780c7
orrrrtem <artem.aroslankin@intel.com>
2020-11-06 09:12:26 +0300

    remove useless using in kernel

--- cpp/oneapi/dal/algo/jaccard/backend/cpu/vertex_similarity_default_kernel_avx2.hpp
+++ cpp/oneapi/dal/algo/jaccard/backend/cpu/vertex_similarity_default_kernel_avx2.hpp
@@ -265,7 +265,6 @@ vertex_similarity_result call_jaccard_default_kernel_avx2(
     const descriptor_base &desc,
     vertex_similarity_input<undirected_adjacency_array_graph<>> &input) {
     const auto &my_graph = input.get_graph();
-    using graph_type = undirected_adjacency_array_graph<>;
     const auto &g = oneapi::dal::detail::get_impl(my_graph);
     auto g_edge_offsets = g._edge_offsets.data();
     auto g_vertex_neighbors = g._vertex_neighbors.data();
