1e1610f14de0a8c8f4c42cdecd8103ebe78d76d8
Kirill Petrov <kirill.petrov@intel.com>
2020-11-11 01:30:37 +0300

    uncomment

--- cpp/daal/src/algorithms/svm/oneapi/svm_train_cache_oneapi.h
+++ cpp/daal/src/algorithms/svm/oneapi/svm_train_cache_oneapi.h
@@ -142,14 +142,10 @@ public:
         services::internal::Buffer<uint32_t> wsIndicesReal = wsIndices;
         if (_ifComputeSubKernel)
         {
-            // SubBuffer in not working on L0 (Date: 20200423).
-            // So workaround is set offset _nSelectRows in kernel copyDataByIndices
-            // Uncomment this code after fix
-
-            // blockSize = _blockSize / 2;
-            // wsIndicesReal = wsIndices.getSubBuffer(_nSelectRows, blockSize, &status);
-            // DAAL_CHECK_STATUS_VAR(status);
-            // DAAL_CHECK_STATUS(status, initSubKernel(blockSize, xTable));
+            blockSize     = _blockSize / 2;
+            wsIndicesReal = wsIndices.getSubBuffer(_nSelectRows, blockSize, &status);
+            DAAL_CHECK_STATUS_VAR(status);
+            DAAL_CHECK_STATUS(status, initSubKernel(blockSize, xTable));
         }
 
         DAAL_CHECK_STATUS(status, Helper::copyDataByIndices(xBuff, wsIndicesReal, _xBlockBuff, blockSize, p));
