ce7afc96e69e651a05c102e98ccca832490e108a
Olga Razvenskaya <olga.razvenskaya@intel.com>
2020-11-11 14:22:25 +0300

    miss print fixed

--- cpp/oneapi/dal/detail/threading.hpp
+++ cpp/oneapi/dal/detail/threading.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2020 std::int32_tel Corporation
+* Copyright 2020 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
