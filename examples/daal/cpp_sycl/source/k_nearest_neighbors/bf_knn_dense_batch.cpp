ec4f5a29590f26d51825d445766ed37daa1a561a
KulikovNikita <nikita.kulikov@intel.com>
2020-11-30 15:13:29 +0300

    Clang format

--- examples/daal/cpp_sycl/source/k_nearest_neighbors/bf_knn_dense_batch.cpp
+++ examples/daal/cpp_sycl/source/k_nearest_neighbors/bf_knn_dense_batch.cpp
@@ -38,113 +38,100 @@ using daal::data_management::internal::SyclHomogenNumericTable;
 using daal::services::internal::SyclExecutionContext;
 
 /* Input data set parameters */
-const string trainDatasetFileName =
-    "../data/batch/k_nearest_neighbors_train.csv";
-const string testDatasetFileName = "../data/batch/k_nearest_neighbors_test.csv";
+const string trainDatasetFileName = "../data/batch/k_nearest_neighbors_train.csv";
+const string testDatasetFileName  = "../data/batch/k_nearest_neighbors_test.csv";
 
 const size_t nFeatures = 5;
-const size_t nClasses = 5;
+const size_t nClasses  = 5;
 
-void trainModel(bf_knn_classification::training::ResultPtr &trainingResult);
-void testModel(bf_knn_classification::training::ResultPtr &trainingResult,
-               classifier::prediction::ResultPtr &predictionResult,
-               NumericTablePtr &testGroundTruth);
-void printResults(NumericTablePtr &testGroundTruth,
-                  classifier::prediction::ResultPtr &predictionResult);
+void trainModel(bf_knn_classification::training::ResultPtr & trainingResult);
+void testModel(bf_knn_classification::training::ResultPtr & trainingResult, classifier::prediction::ResultPtr & predictionResult,
+               NumericTablePtr & testGroundTruth);
+void printResults(NumericTablePtr & testGroundTruth, classifier::prediction::ResultPtr & predictionResult);
 
-int main(int argc, char *argv[]) {
-  checkArguments(argc, argv, 2, &trainDatasetFileName, &testDatasetFileName);
+int main(int argc, char * argv[])
+{
+    checkArguments(argc, argv, 2, &trainDatasetFileName, &testDatasetFileName);
 
-  for (const auto &deviceSelector : getListOfDevices()) {
-    const auto &nameDevice = deviceSelector.first;
-    const auto &device = deviceSelector.second;
-    cl::sycl::queue queue(device);
-    std::cout << "Running on " << nameDevice << "\n\n";
+    for (const auto & deviceSelector : getListOfDevices())
+    {
+        const auto & nameDevice = deviceSelector.first;
+        const auto & device     = deviceSelector.second;
+        cl::sycl::queue queue(device);
+        std::cout << "Running on " << nameDevice << "\n\n";
 
-    SyclExecutionContext ctx(queue);
-    services::Environment::getInstance()->setDefaultExecutionContext(ctx);
+        SyclExecutionContext ctx(queue);
+        services::Environment::getInstance()->setDefaultExecutionContext(ctx);
 
-    bf_knn_classification::training::ResultPtr trainingResult;
-    classifier::prediction::ResultPtr predictionResult;
-    NumericTablePtr testGroundTruth;
+        bf_knn_classification::training::ResultPtr trainingResult;
+        classifier::prediction::ResultPtr predictionResult;
+        NumericTablePtr testGroundTruth;
 
-    trainModel(trainingResult);
-    testModel(trainingResult, predictionResult, testGroundTruth);
-    printResults(testGroundTruth, predictionResult);
-  }
-  return 0;
+        trainModel(trainingResult);
+        testModel(trainingResult, predictionResult, testGroundTruth);
+        printResults(testGroundTruth, predictionResult);
+    }
+    return 0;
 }
 
-void trainModel(bf_knn_classification::training::ResultPtr &trainingResult) {
-  /* Initialize FileDataSource<CSVFeatureManager> to retrieve the input data
+void trainModel(bf_knn_classification::training::ResultPtr & trainingResult)
+{
+    /* Initialize FileDataSource<CSVFeatureManager> to retrieve the input data
    * from a .csv file */
-  FileDataSource<CSVFeatureManager> trainDataSource(
-      trainDatasetFileName, DataSource::notAllocateNumericTable,
-      DataSource::doDictionaryFromContext);
+    FileDataSource<CSVFeatureManager> trainDataSource(trainDatasetFileName, DataSource::notAllocateNumericTable, DataSource::doDictionaryFromContext);
 
-  /* Create Numeric Tables for training data and labels */
-  NumericTablePtr trainData = SyclHomogenNumericTable<>::create(
-      nFeatures, 0, NumericTable::notAllocate);
-  NumericTablePtr trainGroundTruth =
-      SyclHomogenNumericTable<>::create(1, 0, NumericTable::doNotAllocate);
-  NumericTablePtr mergedData(
-      new MergedNumericTable(trainData, trainGroundTruth));
-  /* Retrieve the data from the input file */
-  trainDataSource.loadDataBlock(mergedData.get());
+    /* Create Numeric Tables for training data and labels */
+    NumericTablePtr trainData        = SyclHomogenNumericTable<>::create(nFeatures, 0, NumericTable::notAllocate);
+    NumericTablePtr trainGroundTruth = SyclHomogenNumericTable<>::create(1, 0, NumericTable::doNotAllocate);
+    NumericTablePtr mergedData(new MergedNumericTable(trainData, trainGroundTruth));
+    /* Retrieve the data from the input file */
+    trainDataSource.loadDataBlock(mergedData.get());
 
-  /* Create an algorithm object to train the BF kNN model */
-  bf_knn_classification::training::Batch<> algorithm;
+    /* Create an algorithm object to train the BF kNN model */
+    bf_knn_classification::training::Batch<> algorithm;
 
-  /* Pass the training data set and dependent values to the algorithm */
-  algorithm.input.set(classifier::training::data, trainData);
-  algorithm.input.set(classifier::training::labels, trainGroundTruth);
-  algorithm.parameter().nClasses = nClasses;
-  /* Train the BF kNN model */
-  algorithm.compute();
-  /* Retrieve the results of the training algorithm  */
-  trainingResult = algorithm.getResult();
+    /* Pass the training data set and dependent values to the algorithm */
+    algorithm.input.set(classifier::training::data, trainData);
+    algorithm.input.set(classifier::training::labels, trainGroundTruth);
+    algorithm.parameter().nClasses = nClasses;
+    /* Train the BF kNN model */
+    algorithm.compute();
+    /* Retrieve the results of the training algorithm  */
+    trainingResult = algorithm.getResult();
 }
 
-void testModel(bf_knn_classification::training::ResultPtr &trainingResult,
-               classifier::prediction::ResultPtr &predictionResult,
-               NumericTablePtr &testGroundTruth) {
-  /* Initialize FileDataSource<CSVFeatureManager> to retrieve the test data from
+void testModel(bf_knn_classification::training::ResultPtr & trainingResult, classifier::prediction::ResultPtr & predictionResult,
+               NumericTablePtr & testGroundTruth)
+{
+    /* Initialize FileDataSource<CSVFeatureManager> to retrieve the test data from
    * a .csv file */
-  FileDataSource<CSVFeatureManager> testDataSource(
-      testDatasetFileName, DataSource::notAllocateNumericTable,
-      DataSource::doDictionaryFromContext);
+    FileDataSource<CSVFeatureManager> testDataSource(testDatasetFileName, DataSource::notAllocateNumericTable, DataSource::doDictionaryFromContext);
 
-  /* Create Numeric Tables for testing data and labels */
-  NumericTablePtr testData = SyclHomogenNumericTable<>::create(
-      nFeatures, 0, NumericTable::doNotAllocate);
-  testGroundTruth =
-      SyclHomogenNumericTable<>::create(1, 0, NumericTable::doNotAllocate);
-  NumericTablePtr mergedData(new MergedNumericTable(testData, testGroundTruth));
+    /* Create Numeric Tables for testing data and labels */
+    NumericTablePtr testData = SyclHomogenNumericTable<>::create(nFeatures, 0, NumericTable::doNotAllocate);
+    testGroundTruth          = SyclHomogenNumericTable<>::create(1, 0, NumericTable::doNotAllocate);
+    NumericTablePtr mergedData(new MergedNumericTable(testData, testGroundTruth));
 
-  /* Retrieve the data from input file */
-  testDataSource.loadDataBlock(mergedData.get());
+    /* Retrieve the data from input file */
+    testDataSource.loadDataBlock(mergedData.get());
 
-  /* Create algorithm objects for BF kNN prediction with the default method */
-  bf_knn_classification::prediction::Batch<> algorithm;
+    /* Create algorithm objects for BF kNN prediction with the default method */
+    bf_knn_classification::prediction::Batch<> algorithm;
 
-  /* Pass the testing data set and trained model to the algorithm */
-  algorithm.input.set(classifier::prediction::data, testData);
-  algorithm.input.set(classifier::prediction::model,
-                      trainingResult->get(classifier::training::model));
-  algorithm.parameter().nClasses = nClasses;
+    /* Pass the testing data set and trained model to the algorithm */
+    algorithm.input.set(classifier::prediction::data, testData);
+    algorithm.input.set(classifier::prediction::model, trainingResult->get(classifier::training::model));
+    algorithm.parameter().nClasses = nClasses;
 
-  /* Compute prediction results */
-  algorithm.compute();
+    /* Compute prediction results */
+    algorithm.compute();
 
-  /* Retrieve algorithm results */
-  predictionResult = algorithm.getResult();
+    /* Retrieve algorithm results */
+    predictionResult = algorithm.getResult();
 }
 
-void printResults(NumericTablePtr &testGroundTruth,
-                  classifier::prediction::ResultPtr &predictionResult) {
-  printNumericTables<int, int>(
-      testGroundTruth,
-      predictionResult->get(classifier::prediction::prediction), "Ground truth",
-      "Classification results",
-      "BF kNN classification results (first 20 observations):", 20);
+void printResults(NumericTablePtr & testGroundTruth, classifier::prediction::ResultPtr & predictionResult)
+{
+    printNumericTables<int, int>(testGroundTruth, predictionResult->get(classifier::prediction::prediction), "Ground truth", "Classification results",
+                                 "BF kNN classification results (first 20 observations):", 20);
 }
