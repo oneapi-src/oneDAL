58db8048da66b75568fd6a4177a40ffae48dbc14
Nikolay Petrov <nikolay.a.petrov@intel.com>
2020-12-02 12:03:57 +0300

    Enable DPC++ support in daal4py (#1036)
    * Enable DPC++ support in daal4py

--- .ci/env/dpcpp.sh
+++ .ci/env/dpcpp.sh
@@ -6,12 +6,13 @@ rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo add-apt-repository -y "deb https://apt.repos.intel.com/oneapi all main"
 sudo apt-get update
-sudo apt-get install              \
-    intel-oneapi-common-vars      \
-    intel-oneapi-common-licensing \
-    intel-oneapi-tbb-devel        \
-    intel-oneapi-dpcpp-compiler   \
-    intel-oneapi-dev-utilities    \
-    intel-oneapi-libdpstd-devel   \
+sudo apt-get install                    \
+    intel-oneapi-common-vars            \
+    intel-oneapi-common-licensing       \
+    intel-oneapi-tbb-devel              \
+    intel-oneapi-dpcpp-cpp-compiler     \
+    intel-oneapi-dev-utilities          \
+    intel-oneapi-libdpstd-devel         \
     cmake
 sudo bash -c 'echo libintelocl.so > /etc/OpenCL/vendors/intel-cpu.icd'
+sudo mv -f /opt/intel/oneapi/compiler/latest/linux/lib/oclfpga /opt/intel/oneapi/compiler/latest/linux/lib/oclfpga_
