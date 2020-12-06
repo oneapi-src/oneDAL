1a0836fc6695a65f3b518a8d5fdad212e3fcd647
Kirill Petrov <kirill.petrov@intel.com>
2020-12-06 22:57:15 +0300

    fix checks

--- cpp/daal/include/data_management/data/csr_numeric_table.h
+++ cpp/daal/include/data_management/data/csr_numeric_table.h
@@ -1214,7 +1214,10 @@ protected:
             }
 
             size_t * row_offsets = block.getBlockRowIndicesSharedPtr().get();
-
+            if (!row_offsets)
+            {
+                return services::Status(services::ErrorMemoryAllocationFailed);
+            }
             for (size_t i = 0; i < nrows + 1; i++)
             {
                 row_offsets[i] = rowOffsets[idx + i] - rowOffsets[idx] + 1;
