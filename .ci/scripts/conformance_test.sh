58c5ac5a4654e9da2dd044691212f26168e64229
Kirill <Petrovich_1811@mail.ru>
2020-12-17 16:49:18 +0300

    Update conformance_test.sh

--- .ci/scripts/conformance_test.sh
+++ .ci/scripts/conformance_test.sh
@@ -22,33 +22,6 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# set enviroment
-PYTHON_VERSION="3.7"
-source ${CONDA_DIR}/etc/profile.d/conda.sh
-export PATH=${CONDA_DIR}/bin:$PATH
-conda create -y -n conf python=${PYTHON_VERSION}
-source activate conf
-conda install -y -c intel mpich numpy pytest pandas
-conda install -y -c conda-forge scikit-learn
-conda install $HOME/miniconda/envs/CB/conda-bld/linux-64/daal4py*.tar.bz2
-conda list
-compiler_vars=${ONEAPI_DIR}/compiler/latest/env/vars.sh
-if [ ! -e "${compiler_vars}" ]
-then
-    echo "Can't find compiler vars ${compiler_vars}"
-    exit 1
-fi
-source "${compiler_vars}"
-dal_vars=${BUILD_DIR}/daal/latest/env/vars.sh
-if [ ! -e "${dal_vars}" ]
-then
-    echo "Can't find oneDAL vars ${dal_vars}"
-    exit 1
-fi
-source ${dal_vars} intel64
-export TBBROOT=${BUILD_DIR}/tbb/latest/lib/intel64
-export LD_LIBRARY_PATH=${BUILD_DIR}/tbb/latest/lib/intel64:$LD_LIBRARY_PATH
-
 # testing
 cd .ci/scripts/conformance-scripts/ || exit 1
 export IDP_SKLEARN_VERBOSE=INFO
