bd31c142853b8a1b556ff5d3fc1cd4de11184a65
orrrrtem <artem.aroslankin@intel.com>
2021-01-29 11:56:50 +0300

    fixed include of daal in load graph

--- cpp/oneapi/dal/io/detail/load_graph.cpp
+++ cpp/oneapi/dal/io/detail/load_graph.cpp
@@ -14,8 +14,10 @@
 * limitations under the License.
 *******************************************************************************/
 
-#include "oneapi/dal/io/detail/load_graph.hpp"
+#include <daal/include/services/daal_atomic_int.h>
+#include <daal/include/services/daal_memory.h>
 
+#include "oneapi/dal/io/detail/load_graph.hpp"
 #include "oneapi/dal/detail/common.hpp"
 #include "oneapi/dal/exceptions.hpp"
 #include "oneapi/dal/graph/detail/container.hpp"
@@ -27,8 +29,6 @@
 #include "oneapi/dal/detail/threading.hpp"
 #include "oneapi/dal/io/graph_csv_data_source.hpp"
 #include "oneapi/dal/io/load_graph_descriptor.hpp"
-#include "services/daal_atomic_int.h"
-#include "services/daal_memory.h"
 
 namespace oneapi::dal::preview::load_graph::detail {
 
