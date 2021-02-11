1d7e5611f775b533600a718a36240a5b97e821cf
Mikhail Chernov <mikhail.chernov@intel.com>
2021-02-11 22:03:42 +0300

    Normal generator

--- cpp/oneapi/dal/algo/pca/test/batch.cpp
+++ cpp/oneapi/dal/algo/pca/test/batch.cpp
@@ -200,7 +200,8 @@ TEMPLATE_LIST_TEST_M(pca_batch_test,
 
     const te::dataframe data =
         GENERATE_DATAFRAME(te::dataframe_builder{ 100, 10 }.fill_uniform(0.2, 0.5),
-                           te::dataframe_builder{ 100000, 10 }.fill_uniform(-0.2, 1.5));
+                           te::dataframe_builder{ 100000, 10 }.fill_uniform(-0.2, 1.5),
+                           te::dataframe_builder{ 100000, 10 }.fill_normal(5.0, 2.0));
 
     // Homogen floating point type is the same as algorithm's floating point type
     const auto data_table_id = this->get_homogen_table_id();
