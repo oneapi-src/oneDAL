ca792c69db0cb99a0e182625c2e1d5137126c0e2
polina.plastova <pplastov@nntvml218.inn.intel.com>
2021-02-24 11:30:03 +0300

    Edit jaccard tests

--- cpp/oneapi/dal/algo/jaccard/test/badarg.cpp
+++ cpp/oneapi/dal/algo/jaccard/test/badarg.cpp
@@ -1,99 +1,77 @@
-#include <fstream>
+#include <initializer_list>
 
 #include "oneapi/dal/algo/jaccard/vertex_similarity.hpp"
-#include "oneapi/dal/io/load_graph.hpp"
 
 #include "oneapi/dal/test/engine/common.hpp"
 
 namespace oneapi::dal::algo::jaccard::test {
 
-class graph_base_data {
-public:
-    graph_base_data() = default;
-
-    void write_test_data() {
-        std::ofstream outf(filename);
-        if (outf.is_open()) {
-            outf << file_content;
-        }
-        else {
-            throw invalid_argument(dal::detail::error_messages::file_not_found());
-        }
-        outf.close();
-    }
-
-    void delete_test_data() {
-        std::remove(filename.c_str());
-    }
-
-    const std::string get_filename() const {
-        return filename;
-    }
-
-private:
-    const std::string filename = "test_graph.csv";
-    const std::string file_content = "3 5 0 1 2 3 3 7 0 4 2 6 0 2";
-    ;
-};
-
 class jaccard_badarg_test {
 public:
-    using my_graph_type = dal::preview::undirected_adjacency_vector_graph<>;
-
-    template <typename GraphType>
     auto create_graph() {
-        GraphType graph_data;
-        graph_data.write_test_data();
-        const dal::preview::graph_csv_data_source ds(graph_data.get_filename());
-        const dal::preview::load_graph::descriptor<dal::preview::edge_list<int32_t>, my_graph_type>
-            desc;
-        auto my_graph = dal::preview::load_graph::load(desc, ds);
-        graph_data.delete_test_data();
+        dal::preview::undirected_adjacency_vector_graph<> my_graph;
+        auto &graph_impl = oneapi::dal::detail::get_impl(my_graph);
+
+        const std::int64_t vertex_count = 7;
+        const std::int64_t edge_count = 8;
+        const std::int64_t rows_count = vertex_count + 1;
+        const std::int64_t cols_count = edge_count * 2;
+
+        std::int32_t *degrees = new std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
+        std::int32_t *cols =
+            new std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
+        std::int32_t *rows = new std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+
+        graph_impl.set_topology(vertex_count, edge_count, rows, cols, degrees);
         return my_graph;
     }
 
-    auto get_jaccard_descriptor(const std::int64_t row_range_begin,
-                                const std::int64_t row_range_end,
-                                const std::int64_t column_range_begin,
-                                const std::int64_t column_range_end) {
+    void check_vertex_similarity(const std::int64_t row_range_begin,
+                                 const std::int64_t row_range_end,
+                                 const std::int64_t column_range_begin,
+                                 const std::int64_t column_range_end) {
         const auto jaccard_desc = dal::preview::jaccard::descriptor<>().set_block(
             { row_range_begin, row_range_end },
             { column_range_begin, column_range_end });
-        return jaccard_desc;
+        const auto my_graph = create_graph();
+        dal::preview::jaccard::caching_builder builder;
+        const auto result_vertex_similarity =
+            oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder);
     }
 };
 
-TEST_M(jaccard_badarg_test, "jaccard badarg test") {
-    graph_base_data graph;
-    const auto my_graph = this->create_graph<graph_base_data>();
-    dal::preview::jaccard::caching_builder builder;
+#define JACCARD_BADARG_TEST(name) TEST_M(jaccard_badarg_test, name)
 
-    auto jaccard_desc = this->get_jaccard_descriptor(-2, 0, 0, 3);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Negative interval");
-
-    jaccard_desc = this->get_jaccard_descriptor(0, 2, -3, 0);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Negative interval");
-
-    jaccard_desc = this->get_jaccard_descriptor(2, 0, 0, 3);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Row begin is greater than row end");
-
-    jaccard_desc = this->get_jaccard_descriptor(0, 2, 3, 0);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Column begin is greater than column end");
-
-    jaccard_desc = this->get_jaccard_descriptor(0, 0, 0, 3);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Row count is lower than or equal to zero");
-
-    jaccard_desc = this->get_jaccard_descriptor(0, 2, 0, 0);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Row count is lower than or equal to zero"); // Column expected
-
-    jaccard_desc = this->get_jaccard_descriptor(0, 9, 0, 9);
-    REQUIRE_THROWS_WITH(oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder),
-                        "Interval is greater than vertex count");
+JACCARD_BADARG_TEST("accepts non-negative block ranges") {
+    REQUIRE_NOTHROW(this->check_vertex_similarity(0, 2, 0, 3));
 }
+
+JACCARD_BADARG_TEST("throws if row_range_begin is negative") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(-2, 0, 0, 3), invalid_argument);
+}
+
+JACCARD_BADARG_TEST("throws if column_range_begin is negative") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(0, 2, -3, 0), invalid_argument);
+}
+
+JACCARD_BADARG_TEST("throws if row_range_begin is greater than row_range_end") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(2, 0, 0, 3), invalid_argument);
+}
+
+JACCARD_BADARG_TEST("throws if column_range_begin is greater than column_range_end") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(0, 2, 3, 0), invalid_argument);
+}
+
+JACCARD_BADARG_TEST("throws if row count is equal to zero") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(0, 0, 0, 3), domain_error);
+}
+
+JACCARD_BADARG_TEST("throws if column count is equal to zero") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(0, 2, 0, 0), domain_error);
+}
+
+JACCARD_BADARG_TEST("throws if block ranges is greater than vertex count") {
+    REQUIRE_THROWS_AS(this->check_vertex_similarity(0, 8, 0, 8), out_of_range);
+}
+
 } // namespace oneapi::dal::algo::jaccard::test
--- cpp/oneapi/dal/algo/jaccard/test/batch.cpp
+++ cpp/oneapi/dal/algo/jaccard/test/batch.cpp
@@ -1,75 +1,30 @@
-
-#include "oneapi/dal/test/engine/common.hpp"
-
-#include <fstream>
+#include <array>
 
 #include "oneapi/dal/algo/jaccard/vertex_similarity.hpp"
-#include "oneapi/dal/io/load_graph.hpp"
 
+#include "oneapi/dal/test/engine/common.hpp"
 namespace oneapi::dal::algo::jaccard::test {
-
-class graph_base_data {
-public:
-    graph_base_data() = default;
-
-    void write_test_data() {
-        std::ofstream outf(filename);
-        if (outf.is_open()) {
-            outf << file_content;
-        }
-        else {
-            throw invalid_argument(dal::detail::error_messages::file_not_found());
-        }
-        outf.close();
-    }
-
-    void delete_test_data() {
-        std::remove(filename.c_str());
-    }
-
-    const std::string get_filename() const {
-        return filename;
-    }
-
-    std::int64_t get_nonzero_coeff_count() const {
-        return nonzero_coeff_count;
-    }
-
-    std::int64_t get_rows_count() const {
-        return rows_count;
-    }
-
-    const std::array<std::int64_t, 18> &get_vertex_pairs_data() const {
-        return vertex_pairs_data;
-    }
-
-    const std::array<float, 6> &get_jaccard_coeffs_data() const {
-        return jaccard_coeffs_data;
-    }
-
-private:
-    const std::string filename = "test_graph.csv";
-    const std::string file_content = "3 5 0 1 2 3 3 7 0 4 2 6 0 2";
-    const std::array<std::int64_t, 18> vertex_pairs_data = { 0, 0, 1, 1, 2, 2, 0, 0, 0,
-                                                             0, 0, 0, 0, 3, 1, 2, 1, 2 };
-    const std::array<float, 6> jaccard_coeffs_data = { 1.0, 0.2, 1.0, 0.33333, 0.33333, 1.0 };
-    const std::int64_t nonzero_coeff_count = 6;
-    const std::int64_t rows_count = 12;
-};
 
 class jaccard_batch_test {
 public:
-    using my_graph_type = dal::preview::undirected_adjacency_vector_graph<>;
+    static constexpr std::int64_t nonzero_coeff_count = 8;
+    static constexpr std::int64_t rows_count = 12;
 
-    template <typename GraphType>
     auto create_graph() {
-        GraphType graph_data;
-        graph_data.write_test_data();
-        const dal::preview::graph_csv_data_source ds(graph_data.get_filename());
-        const dal::preview::load_graph::descriptor<dal::preview::edge_list<int32_t>, my_graph_type>
-            desc;
-        auto my_graph = dal::preview::load_graph::load(desc, ds);
-        graph_data.delete_test_data();
+        dal::preview::undirected_adjacency_vector_graph<> my_graph;
+        auto &graph_impl = oneapi::dal::detail::get_impl(my_graph);
+
+        const std::int64_t vertex_count = 7;
+        const std::int64_t edge_count = 8;
+        const std::int64_t rows_count = vertex_count + 1;
+        const std::int64_t cols_count = edge_count * 2;
+
+        std::int32_t *degrees = new std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
+        std::int32_t *cols =
+            new std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
+        std::int32_t *rows = new std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+
+        graph_impl.set_topology(vertex_count, edge_count, rows, cols, degrees);
         return my_graph;
     }
 
@@ -83,67 +38,60 @@ public:
         return jaccard_desc;
     }
 
-    template <typename VertexPairsDataType, std::size_t SIZE>
-    void check_vertex_pairs_data(const VertexPairsDataType vertex_pairs_data,
-                                 const std::array<std::int64_t, SIZE> &correct_vertex_pairs_data,
-                                 std::int64_t element_count,
-                                 std::int64_t pair_count) {
+    template <typename VertexPairsDataType>
+    void check_vertex_pairs_data(const VertexPairsDataType &vertex_pairs_data) {
         int correct_pair_num = 0;
-        for (std::int64_t i = 0; i < pair_count; i++) {
-            if (vertex_pairs_data[i] == correct_vertex_pairs_data[i] &&
-                vertex_pairs_data[i + element_count] ==
-                    correct_vertex_pairs_data[i + element_count])
+        for (std::int64_t i = 0; i < nonzero_coeff_count; i++) {
+            if (vertex_pairs_data[i] == vertex_pairs[i] &&
+                vertex_pairs_data[i + rows_count] == vertex_pairs[i + rows_count])
                 correct_pair_num++;
         }
-        REQUIRE(correct_pair_num == pair_count);
+        REQUIRE(correct_pair_num == nonzero_coeff_count);
     }
 
-    template <typename JaccardCoeffsDataType, std::size_t SIZE>
-    void check_jaccard_coeffs_data(const JaccardCoeffsDataType jaccard_coeffs_data,
-                                   const std::array<float, SIZE> &correct_jaccard_coeffs_data,
-                                   std::int64_t element_count) {
+    template <typename JaccardCoeffsDataType>
+
+    void check_jaccard_coeffs_data(const JaccardCoeffsDataType &jaccard_coeffs_data) {
         int correct_element_num = 0;
-        for (std::int64_t i = 0; i < element_count; i++) {
-            if (Approx(jaccard_coeffs_data[i]) == correct_jaccard_coeffs_data[i])
+        for (std::int64_t i = 0; i < nonzero_coeff_count; i++) {
+            if (Approx(jaccard_coeffs_data[i]) == jaccard_coeffs[i])
                 correct_element_num++;
         }
-        REQUIRE(correct_element_num == element_count);
+        REQUIRE(correct_element_num == nonzero_coeff_count);
     }
+
+private:
+    static constexpr std::array<std::int64_t, 20> vertex_pairs = { 0, 0, 1, 1, 1, 2, 2, 2, 0, 0,
+                                                                   0, 0, 0, 2, 1, 2, 3, 0, 1, 2 };
+    static constexpr std::array<float, 8> jaccard_coeffs = { 1.0,  0.25, 1.0,      0.166667,
+                                                             0.25, 0.25, 0.166667, 1.0 };
 };
 
 TEST_M(jaccard_batch_test, "jaccard batch test") {
-    auto jaccard_desc = this->get_jaccard_descriptor(0, 3, 0, 4);
-    graph_base_data graph;
-    const auto my_graph = this->create_graph<graph_base_data>();
+    const auto jaccard_desc = this->get_jaccard_descriptor(0, 3, 0, 4);
+    const auto my_graph = this->create_graph();
     dal::preview::jaccard::caching_builder builder;
-
     const auto result_vertex_similarity =
         dal::preview::vertex_similarity(jaccard_desc, my_graph, builder);
 
     INFO("check nonzero_coeff_count")
     const std::int64_t nonzero_coeff_count = result_vertex_similarity.get_nonzero_coeff_count();
-    REQUIRE(nonzero_coeff_count == graph.get_nonzero_coeff_count());
+    REQUIRE(nonzero_coeff_count == this->nonzero_coeff_count);
 
     INFO("check rows_count")
     const auto rows_count = result_vertex_similarity.get_vertex_pairs().get_row_count();
-    REQUIRE(rows_count == graph.get_rows_count());
+    REQUIRE(rows_count == this->rows_count);
 
     INFO("check vertex_pairs_data")
     auto vertex_pairs_table = result_vertex_similarity.get_vertex_pairs();
     homogen_table &vertex_pairs = static_cast<homogen_table &>(vertex_pairs_table);
     const auto vertex_pairs_data = vertex_pairs.get_data<int>();
-    this->check_vertex_pairs_data(vertex_pairs_data,
-                                  graph.get_vertex_pairs_data(),
-                                  rows_count,
-                                  nonzero_coeff_count);
+    this->check_vertex_pairs_data(vertex_pairs_data);
 
     INFO("check jaccard_coeffs_data")
     auto coeffs_table = result_vertex_similarity.get_coeffs();
     homogen_table &coeffs = static_cast<homogen_table &>(coeffs_table);
     const auto jaccard_coeffs_data = coeffs.get_data<float>();
-
-    this->check_jaccard_coeffs_data(jaccard_coeffs_data,
-                                    graph.get_jaccard_coeffs_data(),
-                                    nonzero_coeff_count);
+    this->check_jaccard_coeffs_data(jaccard_coeffs_data);
 }
 } // namespace oneapi::dal::algo::jaccard::test
