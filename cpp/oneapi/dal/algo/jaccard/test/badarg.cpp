89ae26a2a6d73d525ba6afd0d92cce268b0346ca
polina.plastova <pplastov@nntvml218.inn.intel.com>
2021-03-05 00:36:41 +0300

    Update jaccard tests

--- cpp/oneapi/dal/algo/jaccard/test/badarg.cpp
+++ cpp/oneapi/dal/algo/jaccard/test/badarg.cpp
@@ -1,3 +1,19 @@
+/*******************************************************************************
+* Copyright 2021 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
 #include <initializer_list>
 
 #include "oneapi/dal/algo/jaccard/vertex_similarity.hpp"
@@ -9,20 +25,44 @@ namespace oneapi::dal::algo::jaccard::test {
 class jaccard_badarg_test {
 public:
     auto create_graph() {
-        dal::preview::undirected_adjacency_vector_graph<> my_graph;
+        oneapi::dal::preview::undirected_adjacency_vector_graph<> my_graph;
         auto &graph_impl = oneapi::dal::detail::get_impl(my_graph);
+        auto &vertex_allocator = graph_impl._vertex_allocator;
+        auto &edge_allocator = graph_impl._edge_allocator;
 
         const std::int64_t vertex_count = 7;
         const std::int64_t edge_count = 8;
-        const std::int64_t rows_count = vertex_count + 1;
         const std::int64_t cols_count = edge_count * 2;
+        const std::int64_t rows_count = vertex_count + 1;
 
-        std::int32_t *degrees = new std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
+        std::int32_t *degrees_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>::allocate(
+                vertex_allocator,
+                vertex_count);
+        std::int32_t *cols_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>::allocate(
+                vertex_allocator,
+                cols_count);
+        std::int64_t *rows_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int64_t>::allocate(
+                edge_allocator,
+                rows_count);
+        std::int32_t *rows_vertex_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>::allocate(
+                vertex_allocator,
+                rows_count);
+
+        std::int32_t *degrees = new (degrees_) std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
         std::int32_t *cols =
-            new std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
-        std::int32_t *rows = new std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+            new (cols_) std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
+        std::int64_t *rows = new (rows_) std::int64_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+        std::int32_t *rows_vertex =
+            new (rows_vertex_) std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
 
         graph_impl.set_topology(vertex_count, edge_count, rows, cols, degrees);
+        graph_impl.get_topology()._rows_vertex =
+            oneapi::dal::preview::detail::container<std::int32_t>::wrap(rows_vertex, rows_count);
+
         return my_graph;
     }
 
@@ -34,13 +74,15 @@ public:
             { row_range_begin, row_range_end },
             { column_range_begin, column_range_end });
         const auto my_graph = create_graph();
+
         dal::preview::jaccard::caching_builder builder;
+
         const auto result_vertex_similarity =
             oneapi::dal::preview::vertex_similarity(jaccard_desc, my_graph, builder);
     }
 };
 
-#define JACCARD_BADARG_TEST(name) TEST_M(jaccard_badarg_test, name)
+#define JACCARD_BADARG_TEST(name) TEST_M(jaccard_badarg_test, name, "[jaccard][badarg]")
 
 JACCARD_BADARG_TEST("accepts non-negative block ranges") {
     REQUIRE_NOTHROW(this->check_vertex_similarity(0, 2, 0, 3));
--- cpp/oneapi/dal/algo/jaccard/test/batch.cpp
+++ cpp/oneapi/dal/algo/jaccard/test/batch.cpp
@@ -1,4 +1,21 @@
+/*******************************************************************************
+* Copyright 2021 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
 #include <array>
+#include <initializer_list>
 
 #include "oneapi/dal/algo/jaccard/vertex_similarity.hpp"
 
@@ -11,20 +28,44 @@ public:
     static constexpr std::int64_t rows_count = 12;
 
     auto create_graph() {
-        dal::preview::undirected_adjacency_vector_graph<> my_graph;
+        oneapi::dal::preview::undirected_adjacency_vector_graph<> my_graph;
         auto &graph_impl = oneapi::dal::detail::get_impl(my_graph);
+        auto &vertex_allocator = graph_impl._vertex_allocator;
+        auto &edge_allocator = graph_impl._edge_allocator;
 
         const std::int64_t vertex_count = 7;
         const std::int64_t edge_count = 8;
-        const std::int64_t rows_count = vertex_count + 1;
         const std::int64_t cols_count = edge_count * 2;
+        const std::int64_t rows_count = vertex_count + 1;
 
-        std::int32_t *degrees = new std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
+        std::int32_t *degrees_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>::allocate(
+                vertex_allocator,
+                vertex_count);
+        std::int32_t *cols_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>::allocate(
+                vertex_allocator,
+                cols_count);
+        std::int64_t *rows_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int64_t>::allocate(
+                edge_allocator,
+                rows_count);
+        std::int32_t *rows_vertex_ =
+            std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>::allocate(
+                vertex_allocator,
+                rows_count);
+
+        std::int32_t *degrees = new (degrees_) std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
         std::int32_t *cols =
-            new std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
-        std::int32_t *rows = new std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+            new (cols_) std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
+        std::int64_t *rows = new (rows_) std::int64_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+        std::int32_t *rows_vertex =
+            new (rows_vertex_) std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
 
         graph_impl.set_topology(vertex_count, edge_count, rows, cols, degrees);
+        graph_impl.get_topology()._rows_vertex =
+            oneapi::dal::preview::detail::container<std::int32_t>::wrap(rows_vertex, rows_count);
+
         return my_graph;
     }
 
@@ -40,24 +81,24 @@ public:
 
     template <typename VertexPairsDataType>
     void check_vertex_pairs_data(const VertexPairsDataType &vertex_pairs_data) {
-        int correct_pair_num = 0;
+        int correct_pair_count = 0;
         for (std::int64_t i = 0; i < nonzero_coeff_count; i++) {
             if (vertex_pairs_data[i] == vertex_pairs[i] &&
                 vertex_pairs_data[i + rows_count] == vertex_pairs[i + rows_count])
-                correct_pair_num++;
+                correct_pair_count++;
         }
-        REQUIRE(correct_pair_num == nonzero_coeff_count);
+        REQUIRE(correct_pair_count == nonzero_coeff_count);
     }
 
     template <typename JaccardCoeffsDataType>
 
     void check_jaccard_coeffs_data(const JaccardCoeffsDataType &jaccard_coeffs_data) {
-        int correct_element_num = 0;
+        int correct_element_count = 0;
         for (std::int64_t i = 0; i < nonzero_coeff_count; i++) {
             if (Approx(jaccard_coeffs_data[i]) == jaccard_coeffs[i])
-                correct_element_num++;
+                correct_element_count++;
         }
-        REQUIRE(correct_element_num == nonzero_coeff_count);
+        REQUIRE(correct_element_count == nonzero_coeff_count);
     }
 
 private:
@@ -94,4 +135,5 @@ TEST_M(jaccard_batch_test, "jaccard batch test") {
     const auto jaccard_coeffs_data = coeffs.get_data<float>();
     this->check_jaccard_coeffs_data(jaccard_coeffs_data);
 }
+
 } // namespace oneapi::dal::algo::jaccard::test
