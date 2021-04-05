a2a7ac275b5963be828433bd3687d7d570ae5377
Ruslan Israfilov <ruslan.israfilov@intel.com>
2021-04-05 11:16:45 +0300

    Fix variadic macro on Windows

--- cpp/oneapi/dal/backend/micromkl/macro.hpp
+++ cpp/oneapi/dal/backend/micromkl/macro.hpp
@@ -20,6 +20,7 @@
 #error "This header cannot be included outside of micromkl module"
 #endif
 
+#define EXPAND(...)                      __VA_ARGS__
 #define FUNC_NAME(prefix, name)          prefix##_##name
 #define FUNC_NAME_CPU(cpu, prefix, name) prefix##_##cpu##_##name
 #define DISPATCH_ID_NAME(cpu)            oneapi::dal::backend::cpu_dispatch_##cpu
@@ -41,17 +42,17 @@
     FUNC_CPU_DECL(nominal_cpu, prefix, name, argdecl)                     \
     DISPATCH_FUNC_CPU(nominal_cpu, actual_cpu, prefix, name, argdecl, argcall)
 
-#define FUNC_AVX512(...) FUNC_CPU(avx512, avx512, __VA_ARGS__)
-#define FUNC_AVX2(...)   FUNC_CPU(avx2, avx2, __VA_ARGS__)
-#define FUNC_AVX(...)    FUNC_CPU(avx, avx, __VA_ARGS__)
-#define FUNC_SSE42(...)  FUNC_CPU(sse42, sse42, __VA_ARGS__)
+#define FUNC_AVX512(...) EXPAND(FUNC_CPU(avx512, avx512, __VA_ARGS__))
+#define FUNC_AVX2(...)   EXPAND(FUNC_CPU(avx2, avx2, __VA_ARGS__))
+#define FUNC_AVX(...)    EXPAND(FUNC_CPU(avx, avx, __VA_ARGS__))
+#define FUNC_SSE42(...)  EXPAND(FUNC_CPU(sse42, sse42, __VA_ARGS__))
 
 #ifdef __APPLE__
-#define FUNC_SSSE3(...) FUNC_CPU(ssse3, sse42, __VA_ARGS__)
-#define FUNC_SSE2(...)  FUNC_CPU(sse2, sse42, __VA_ARGS__)
+#define FUNC_SSSE3(...) EXPAND(FUNC_CPU(ssse3, sse42, __VA_ARGS__))
+#define FUNC_SSE2(...)  EXPAND(FUNC_CPU(sse2, sse42, __VA_ARGS__))
 #else
-#define FUNC_SSSE3(...) FUNC_CPU(ssse3, ssse3, __VA_ARGS__)
-#define FUNC_SSE2(...)  FUNC_CPU(sse2, sse2, __VA_ARGS__)
+#define FUNC_SSSE3(...) EXPAND(FUNC_CPU(ssse3, ssse3, __VA_ARGS__))
+#define FUNC_SSE2(...)  EXPAND(FUNC_CPU(sse2, sse2, __VA_ARGS__))
 #endif
 
 #define FUNC(prefix, name, argdecl, argcall)    \
@@ -67,36 +68,36 @@
     template void name<DISPATCH_ID_NAME(cpu), Float> argdecl(Float);
 
 #ifdef ONEDAL_CPU_DISPATCH_AVX512
-#define INSTANTIATE_AVX512(...) INSTANTIATE_CPU(avx512, __VA_ARGS__)
+#define INSTANTIATE_AVX512(...) EXPAND(INSTANTIATE_CPU(avx512, __VA_ARGS__))
 #else
 #define INSTANTIATE_AVX512(...)
 #endif
 
 #ifdef ONEDAL_CPU_DISPATCH_AVX2
-#define INSTANTIATE_AVX2(...) INSTANTIATE_CPU(avx2, __VA_ARGS__)
+#define INSTANTIATE_AVX2(...) EXPAND(INSTANTIATE_CPU(avx2, __VA_ARGS__))
 #else
 #define INSTANTIATE_AVX2(...)
 #endif
 
 #ifdef ONEDAL_CPU_DISPATCH_AVX
-#define INSTANTIATE_AVX(...) INSTANTIATE_CPU(avx, __VA_ARGS__)
+#define INSTANTIATE_AVX(...) EXPAND(INSTANTIATE_CPU(avx, __VA_ARGS__))
 #else
 #define INSTANTIATE_AVX(...)
 #endif
 
 #ifdef ONEDAL_CPU_DISPATCH_SSE42
-#define INSTANTIATE_SSE42(...) INSTANTIATE_CPU(sse42, __VA_ARGS__)
+#define INSTANTIATE_SSE42(...) EXPAND(INSTANTIATE_CPU(sse42, __VA_ARGS__))
 #else
 #define INSTANTIATE_SSE42(...)
 #endif
 
 #ifdef ONEDAL_CPU_DISPATCH_SSSE3
-#define INSTANTIATE_SSSE3(...) INSTANTIATE_CPU(ssse3, __VA_ARGS__)
+#define INSTANTIATE_SSSE3(...) EXPAND(INSTANTIATE_CPU(ssse3, __VA_ARGS__))
 #else
 #define INSTANTIATE_SSSE3(...)
 #endif
 
-#define INSTANTIATE_SSE2(...) INSTANTIATE_CPU(sse2, __VA_ARGS__)
+#define INSTANTIATE_SSE2(...) EXPAND(INSTANTIATE_CPU(sse2, __VA_ARGS__))
 
 #define INSTANTIATE_FLOAT(name, Float, argdecl) \
     INSTANTIATE_AVX512(name, Float, argdecl)    \
