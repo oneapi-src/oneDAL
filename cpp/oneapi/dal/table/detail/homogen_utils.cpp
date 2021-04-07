c43025fc052471fabf61de598cad99d6dd286f03
Michael Smirnov <michael.smirnov@intel.com>
2021-04-07 12:15:49 +0300

    get_original_data implemented

---
+++ cpp/oneapi/dal/table/detail/homogen_utils.cpp
@@ -0,0 +1,37 @@
+/*******************************************************************************
+* Copyright 2021 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "oneapi/dal/table/detail/homogen_utils.hpp"
+#include "oneapi/dal/table/backend/homogen_table_impl.hpp"
+
+namespace oneapi::dal::detail::v1 {
+
+array<byte_t> get_original_data(const homogen_table& t) {
+    using impl_t = backend::homogen_table_impl;
+    using wrapper_t = homogen_table_impl_wrapper<impl_t>;
+
+    const auto& pimpl = pimpl_accessor{}.get_pimpl(t);
+    auto* impl_raw_ptr = dynamic_cast<wrapper_t*>(pimpl.get());
+    if (impl_raw_ptr != nullptr) {
+        auto& homogen_impl = impl_raw_ptr->get();
+        return homogen_impl.get_data_array();
+    }
+    else {
+        return array<byte_t>{};
+    }
+}
+
+} // namespace oneapi::dal::detail::v1
