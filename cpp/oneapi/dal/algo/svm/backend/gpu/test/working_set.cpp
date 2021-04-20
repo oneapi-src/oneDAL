806428afc140438b76f7f5a900bbdf74ce57dece
Vladislav Nazarov <vladislav.nazarov@intel.com>
2021-04-20 20:51:43 +0300

    Add common flow test

--- cpp/oneapi/dal/algo/svm/backend/gpu/test/working_set.cpp
+++ cpp/oneapi/dal/algo/svm/backend/gpu/test/working_set.cpp
@@ -30,63 +30,69 @@ namespace la = te::linalg;
 namespace de = dal::detail;
 
 template <typename TestType>
-class train_working_set_test : public te::policy_fixture {
+class working_set_test : public te::policy_fixture {
 public:
-    using Float = std::tuple_element_t<0, TestType>;
-    using Index = std::tuple_element_t<1, TestType>;
+    using Float = TestType;
 
-    // bool not_available_on_device() {
-    //         constexpr bool is_smo = std::is_same_v<Method, svm::method::smo>;
-    //         return this->get_policy().is_gpu() && is_smo;
-    //     }
+    void test_working_set(const std::vector<float_t>& f,
+                          const std::vector<float_t>& y,
+                          const std::vector<float_t>& alpha,
+                          const float_t C,
+                          const std::int64_t n_vectors,
+                          const std::int64_t expected_n_ws,
+                          const std::vector<std::uint32_t>& expected_ws_indices) {
+        auto& q = this->get_queue();
+        INFO("Allocate ndarray");
+        auto f_ndarray = pr::ndarray<float_t, 1>::empty(q, { n_vectors });
+        auto y_ndarray = pr::ndarray<float_t, 1>::empty(q, { n_vectors });
+        auto alpha_ndarray = pr::ndarray<float_t, 1>::empty(q, { n_vectors });
+        dal::backend::copy<float_t>(q, f_ndarray.get_mutable_data(), f.data(), n_vectors)
+            .wait_and_throw();
+        dal::backend::copy<float_t>(q, y_ndarray.get_mutable_data(), y.data(), n_vectors)
+            .wait_and_throw();
+        dal::backend::copy<float_t>(q, alpha_ndarray.get_mutable_data(), alpha.data(), n_vectors)
+            .wait_and_throw();
+
+        INFO("Init working set");
+        auto ws = working_set<float_t>(q);
+        ws.init(n_vectors);
+
+        INFO("Check n_ws");
+        REQUIRE(ws.get_size() == expected_n_ws);
+
+        INFO("Run select_ws");
+        ws.select_ws(y_ndarray, alpha_ndarray, f_ndarray, C).wait_and_throw();
+
+        INFO("Check ws_indices");
+        const auto indices = ws.get_ws_indices();
+        const auto indices_arr = indices.flatten(q);
+
+        const auto indices_mat_host = la::matrix<std::uint32_t>::wrap(indices_arr).to_host();
+        const auto indices_arr_host = indices_mat_host.get_array();
+        for (std::int64_t i = 0; i < indices.get_dimension(0); i++)
+            REQUIRE(indices_arr_host[i] == expected_ws_indices[i]);
+    }
 };
 
-using train_working_set_types = COMBINE_TYPES((float, double), (std::uint32_t));
+// using working_set_types = std::tuple<float, double>;
+using working_set_types = std::tuple<float>;
 
-TEMPLATE_LIST_TEST_M(train_working_set_test,
-                     "basic run",
-                     "[working_set]",
-                     train_working_set_types) {
-    using float_t = std::tuple_element_t<0, TestType>;
-    // using integer_t = std::tuple_element_t<1, TestType>;
-    auto& q = this->get_queue();
-    // auto q = sycl::queue{sycl::device{sycl::gpu_selector{}}};
+TEMPLATE_LIST_TEST_M(working_set_test, "select ws common flow", "[svm][working_set]", working_set_types) {
+    using float_t = TestType;
 
-    constexpr std::int64_t n_vectors = 10;
+    constexpr std::int64_t n_vectors = 9;
 
-    std::vector<float_t> f = { 1, 3, 10, 4, 2, 8, 6, 5, 9, 7 };
-    std::vector<float_t> y = { -1, -1, -1, -1, -1, 1, 1, 1, 1, 1 };
-    std::vector<float_t> alpha = { 0, 0, 0.1, 0.2, 1.5, 0, 0.2, 0.4, 1.5, 1.5 };
+    const std::vector<float_t> f = { 2, 7, 3, 9, 5, 1, 8, 4, 6 };
+    const std::vector<float_t> y = { -1, -1, 1, 1, -1, -1, 1, -1, 1 };
+    const std::vector<float_t> alpha = { 1.5, 0, 5.0, 1.2, 0.2, 0, 5, 4.3, 0 };
 
-    float_t* f_ptr = sycl::malloc_device<float_t>(n_vectors, q);
-    float_t* y_ptr = sycl::malloc_device<float_t>(n_vectors, q);
-    float_t* alpha_ptr = sycl::malloc_device<float_t>(n_vectors, q);
+    constexpr float_t C = 5.0;
 
-    q.memcpy(f_ptr, f.data(), n_vectors * sizeof(float_t));
-    q.memcpy(y_ptr, y.data(), n_vectors * sizeof(float_t));
-    q.memcpy(alpha_ptr, alpha.data(), n_vectors * sizeof(float_t));
+    constexpr std::int64_t expected_n_ws = 8;
 
-    const pr::ndarray<float_t, 1> f_ndarray = pr::ndarray<float_t, 1>::wrap(f_ptr, n_vectors);
-    const pr::ndarray<float_t, 1> y_ndarray = pr::ndarray<float_t, 1>::wrap(y_ptr, n_vectors);
-    const pr::ndarray<float_t, 1> alpha_ndarray =
-        pr::ndarray<float_t, 1>::wrap(alpha_ptr, n_vectors);
+    const std::vector<std::uint32_t> expected_ws_indices = { 0, 7, 4, 8, 2, 1, 6, 3 };
 
-    float_t C = 1.5;
-
-    auto ws = working_set<float_t>(q);
-    ws.init(n_vectors);
-    // auto ws_copy_event = ws.copy_last_to_first();
-    ws.select_ws(y_ndarray, alpha_ndarray, f_ndarray, C).wait_and_throw();
-    // REQUIRE(ws.get_size() == 4);
-    const auto indices = ws.get_ws_indices();
-    const auto raw_arr = indices.flatten(q);
-
-    const auto raw_mat_host = la::matrix<std::uint32_t>::wrap(raw_arr).to_host();
-    const auto raw_arr_host = raw_mat_host.get_array();
-
-    std::cout << "indices\n";
-    for (std::int64_t i = 0; i < indices.get_dimension(0); i++)
-        std::cout << raw_arr_host[i] << " ";
+    this->test_working_set(f, y, alpha, C, n_vectors, expected_n_ws, expected_ws_indices);
 }
 
 } // namespace oneapi::dal::svm::backend::test
