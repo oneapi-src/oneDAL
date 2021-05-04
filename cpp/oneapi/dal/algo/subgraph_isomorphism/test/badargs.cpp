8659d2de275617ea508e9ab80579a8a7e32cca14
YakhtinLeonid <leonid.yakhtin@intel.com>
2021-05-04 16:01:44 +0300

    Refactor badargs + add empty graph cases

--- cpp/oneapi/dal/algo/subgraph_isomorphism/test/badargs.cpp
+++ cpp/oneapi/dal/algo/subgraph_isomorphism/test/badargs.cpp
@@ -26,47 +26,114 @@ namespace oneapi::dal::algo::subgraph_isomorphism::test {
 
 typedef dal::preview::subgraph_isomorphism::kind isomorphism_kind;
 
+class graph_base_data {
+public:
+    graph_base_data() = default;
+
+    std::int64_t get_vertex_count() const {
+        return vertex_count;
+    }
+
+    std::int64_t get_edge_count() const {
+        return edge_count;
+    }
+
+    std::int64_t get_cols_count() const {
+        return cols_count;
+    }
+
+    std::int64_t get_rows_count() const {
+        return rows_count;
+    }
+
+protected:
+    std::int64_t vertex_count;
+    std::int64_t edge_count;
+    std::int64_t cols_count;
+    std::int64_t rows_count;
+};
+
+class double_triangle_target_type : public graph_base_data {
+public:
+    double_triangle_target_type() {
+        vertex_count = 9;
+        edge_count = 16;
+        cols_count = 32;
+        rows_count = 10;
+    }
+    std::array<std::int32_t, 9> degrees = { 2, 4, 3, 4, 4, 5, 3, 5, 2 };
+    std::array<std::int32_t, 32> cols = { 1, 3, 0, 2, 5, 3, 4, 1, 3, 7, 0, 2, 1, 2, 6, 5,
+                                          7, 1, 8, 7, 4, 6, 4, 7, 5, 3, 5, 6, 4, 8, 5, 7 };
+    std::array<std::int64_t, 10> rows = { 0, 2, 6, 9, 13, 17, 22, 25, 30, 32 };
+    std::array<std::int32_t, 9> labels = { 1, 0, 1, 0, 0, 1, 0, 1, 0 };
+};
+
+class empty_graph_type : public graph_base_data {
+public:
+    empty_graph_type() {
+        vertex_count = 0;
+        edge_count = 0;
+        cols_count = 0;
+        rows_count = 1;
+    }
+    std::array<std::int32_t, 0> degrees = {};
+    std::array<std::int32_t, 0> cols = {};
+    std::array<std::int64_t, 1> rows = { 0 };
+    std::array<std::int32_t, 0> labels = {};
+};
+
 class subgraph_isomorphism_badarg_test {
 public:
+    using my_graph_type = dal::preview::undirected_adjacency_vector_graph<std::int32_t>;
+
+    template <typename GraphType>
     auto create_graph() {
-        oneapi::dal::preview::undirected_adjacency_vector_graph<std::int32_t> my_graph;
+        GraphType graph_data;
+        my_graph_type my_graph;
+
         auto &graph_impl = oneapi::dal::detail::get_impl(my_graph);
         auto &vertex_allocator = graph_impl._vertex_allocator;
         auto &edge_allocator = graph_impl._edge_allocator;
 
-        const std::int64_t vertex_count = 7;
-        const std::int64_t edge_count = 8;
-        const std::int64_t cols_count = edge_count * 2;
-        const std::int64_t rows_count = vertex_count + 1;
+        const std::int64_t vertex_count = graph_data.get_vertex_count();
+        const std::int64_t edge_count = graph_data.get_edge_count();
+        const std::int64_t cols_count = graph_data.get_cols_count();
+        const std::int64_t rows_count = graph_data.get_rows_count();
 
         typedef std::allocator_traits<std::allocator<char>>::rebind_traits<std::int32_t>
             int32_traits_t;
         typedef std::allocator_traits<std::allocator<char>>::rebind_traits<std::int64_t>
             int64_traits_t;
-        std::int32_t *degrees_ = int32_traits_t::allocate(vertex_allocator, vertex_count);
-        std::int32_t *cols_ = int32_traits_t::allocate(vertex_allocator, cols_count);
-        std::int64_t *rows_ = int64_traits_t::allocate(edge_allocator, rows_count);
-        std::int32_t *rows_vertex_ = int32_traits_t::allocate(vertex_allocator, rows_count);
+        std::int32_t *degrees = int32_traits_t::allocate(vertex_allocator, vertex_count);
+        std::int32_t *cols = int32_traits_t::allocate(vertex_allocator, cols_count);
+        std::int64_t *rows = int64_traits_t::allocate(edge_allocator, rows_count);
+        std::int32_t *rows_vertex = int32_traits_t::allocate(vertex_allocator, rows_count);
 
-        std::int32_t *degrees = new (degrees_) std::int32_t[vertex_count]{ 1, 3, 4, 2, 3, 1, 2 };
-        std::int32_t *cols =
-            new (cols_) std::int32_t[cols_count]{ 1, 0, 2, 4, 1, 3, 4, 5, 2, 6, 1, 2, 6, 2, 3, 4 };
-        std::int64_t *rows = new (rows_) std::int64_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
-        std::int32_t *rows_vertex =
-            new (rows_vertex_) std::int32_t[rows_count]{ 0, 1, 4, 8, 10, 13, 14, 16 };
+        for (int i = 0; i < vertex_count; i++) {
+            degrees[i] = graph_data.degrees[i];
+        }
 
+        for (int i = 0; i < cols_count; i++) {
+            cols[i] = graph_data.cols[i];
+        }
+        for (int i = 0; i < rows_count; i++) {
+            rows[i] = graph_data.rows[i];
+            rows_vertex[i] = graph_data.rows[i];
+        }
         graph_impl.set_topology(vertex_count, edge_count, rows, cols, degrees);
         graph_impl.get_topology()._rows_vertex =
             oneapi::dal::preview::detail::container<std::int32_t>::wrap(rows_vertex, rows_count);
-
         return my_graph;
     }
 
+    template <typename TargetGraphType, typename PatternGraphType>
     void check_subgraph_isomorphism(bool semantic_match,
-                                    std::int64_t max_match_count,
-                                    isomorphism_kind kind) {
-        const auto target_graph = create_graph();
-        const auto pattern_graph = create_graph();
+                                    isomorphism_kind kind,
+                                    std::int64_t max_match_count) {
+        TargetGraphType target_graph_data;
+        PatternGraphType pattern_graph_data;
+        const auto target_graph = create_graph<TargetGraphType>();
+        const auto pattern_graph = create_graph<PatternGraphType>();
 
         std::allocator<char> alloc;
         const auto subgraph_isomorphism_desc =
@@ -83,15 +150,35 @@ public:
 #define SUBGRAPH_ISOMORPHISM_BADARG_TEST(name) \
     TEST_M(subgraph_isomorphism_badarg_test, name, "[subgraph_isomorphism][badarg]")
 
-SUBGRAPH_ISOMORPHISM_BADARG_TEST("positive check") {
-    REQUIRE_NOTHROW(this->check_subgraph_isomorphism(false, 100, isomorphism_kind::induced));
+SUBGRAPH_ISOMORPHISM_BADARG_TEST("Positive check") {
+    REQUIRE_NOTHROW(
+        this->check_subgraph_isomorphism<double_triangle_target_type, double_triangle_target_type>(
+            false,
+            isomorphism_kind::induced,
+            100));
 }
 
-SUBGRAPH_ISOMORPHISM_BADARG_TEST("throws if match count is negative") {
-    REQUIRE_THROWS_AS(this->check_subgraph_isomorphism(false, -1, isomorphism_kind::induced),
-                      invalid_argument);
+SUBGRAPH_ISOMORPHISM_BADARG_TEST("Empty target graph") {
+    REQUIRE_NOTHROW(this->check_subgraph_isomorphism<empty_graph_type, double_triangle_target_type>(
+        false,
+        isomorphism_kind::induced,
+        100));
 }
 
-// TODO: Add empty graph case after implementation
+SUBGRAPH_ISOMORPHISM_BADARG_TEST("Empty pattern graph") {
+    REQUIRE_NOTHROW(this->check_subgraph_isomorphism<double_triangle_target_type, empty_graph_type>(
+        false,
+        isomorphism_kind::induced,
+        100));
+}
+
+SUBGRAPH_ISOMORPHISM_BADARG_TEST("Throws if match count is negative") {
+    REQUIRE_THROWS_AS(
+        (this->check_subgraph_isomorphism<double_triangle_target_type, double_triangle_target_type>(
+            false,
+            isomorphism_kind::induced,
+            -1)),
+        invalid_argument);
+}
 
 } // namespace oneapi::dal::algo::subgraph_isomorphism::test
