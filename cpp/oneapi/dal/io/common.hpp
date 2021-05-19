b9821d6bb3b733e9138aa8d4bdbe67d2021d55ba
orrrrtem <artem.aroslankin@intel.com>
2021-05-19 18:53:11 +0300

    removed include vector

--- cpp/oneapi/dal/io/common.hpp
+++ cpp/oneapi/dal/io/common.hpp
@@ -23,7 +23,6 @@
 #include "oneapi/dal/common.hpp"
 #include "oneapi/dal/detail/common.hpp"
 #include "oneapi/dal/io/detail/common.hpp"
-#include <vector>
 
 namespace oneapi::dal::preview {
 /// Type of graph representation as an edge list
