7a67070bd2135126ed88c9f7f7099a5ac6e98700
Yakovlev <pavel.yakovlev@intel.com>
2021-05-27 19:34:04 +0300

    add conditions to make for md and mdd

--- dev/make/cmplr.dpcpp.mk
+++ dev/make/cmplr.dpcpp.mk
@@ -33,7 +33,7 @@ COMPILER.lnx.dpcpp = dpcpp $(if $(IA_is_ia32),-m32,-m64) -stdlib=libstdc++ -fgnu
 COMPILER.win.dpcpp = dpcpp -MD -nologo -WX -Wno-deprecated-declarations
 
 link.dynamic.lnx.dpcpp = dpcpp $(if $(IA_is_ia32),-m32,-m64)
-link.dynamic.win.dpcpp = dpcpp $(if $(IA_is_ia32),-m32,-m64)
+link.dynamic.win.dpcpp = dpcpp $(if $(CONFIG_is_release),-MD, -MDd /debug:none) $(if $(IA_is_ia32),-m32,-m64)
 
 pedantic.opts.lnx.dpcpp = -pedantic \
                           -Wall \
