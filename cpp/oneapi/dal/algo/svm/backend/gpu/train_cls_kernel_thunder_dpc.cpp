a342d3002c7b2c515d9e2d3bf2a85e9344b57f75
Kirill Petrov <kirill.petrov@intel.com>
2021-05-28 12:02:41 +0300

    fix build

--- cpp/oneapi/dal/algo/svm/backend/gpu/train_cls_kernel_thunder_dpc.cpp
+++ cpp/oneapi/dal/algo/svm/backend/gpu/train_cls_kernel_thunder_dpc.cpp
@@ -95,7 +95,7 @@ static result_t call_daal_kernel(const context_gpu& ctx,
                                                                             daal_svm_parameter));
     const std::int64_t n_sv = daal_model->getSupportIndices()->getNumberOfRows();
     if (n_sv == 0) {
-        return train_result<Task>{};
+        return result_t{};
     }
     auto table_support_indices =
         interop::convert_from_daal_homogen_table<Float>(daal_model->getSupportIndices());
