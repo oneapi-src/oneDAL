b198edb03f9f0a9e363b7848c9ac56d240142c8b
Michael Averbukh <mikhail.averbukh@intel.com>
2021-06-02 17:15:36 +0300

    Experiment #4 to address infinite loop

--- cpp/oneapi/dal/algo/subgraph_isomorphism/backend/cpu/matching.hpp
+++ cpp/oneapi/dal/algo/subgraph_isomorphism/backend/cpu/matching.hpp
@@ -629,7 +629,6 @@ solution<Cpu> engine_bundle<Cpu>::run() {
     std::uint64_t max_threads_count = dal::detail::threader_get_max_threads();
     std::uint64_t possible_first_states_count_per_thread = first_states_count / max_threads_count;
     if (possible_first_states_count_per_thread < 1) {
-        // max_threads_count = first_states_count;
         possible_first_states_count_per_thread = 1;
     }
     else {
@@ -638,7 +637,7 @@ solution<Cpu> engine_bundle<Cpu>::run() {
     }
 
     std::cout << "max_threads_count = " << max_threads_count << std::endl;
-    const std::uint64_t array_size = max_threads_count * 2 / 10;
+    const std::uint64_t array_size = max_threads_count * 5 / 10;
     std::cout << "array_size = " << array_size << std::endl;
     auto engine_array_ptr = allocator_.make_shared_memory<matching_engine<Cpu>>(array_size);
     matching_engine<Cpu>* engine_array = engine_array_ptr.get();
