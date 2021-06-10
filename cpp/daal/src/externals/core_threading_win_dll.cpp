d7901b1f75060e22108372ac1f5bbe6d23114400
Michael Smirnov <michael.smirnov@intel.com>
2021-06-10 09:52:27 +0300

    Update cpp/daal/src/externals/core_threading_win_dll.cpp
    Co-authored-by: Ruslan Israfilov <ruslan.is@outlook.com>

--- cpp/daal/src/externals/core_threading_win_dll.cpp
+++ cpp/daal/src/externals/core_threading_win_dll.cpp
@@ -779,7 +779,7 @@ DAAL_EXPORT void _daal_tbb_task_scheduler_free(void *& init)
     if (init == NULL)
     {
         // If threading library was not opened, there is nothing to free,
-        // so we do not need to open threading library just to do nothing.
+        // so we do not need to load threading library.
         // Moreover, loading threading library in the Environment destructor
         // results in a crush because of the use of Wintrust library after it was unloaded.
         // This happens because of undefined order to deinitialize global static objects
