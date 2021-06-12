ed01cfb5a58e7d6bb39f710af70425fb5c7ba020
SHVETS, KIRILL <kirill.shvets@intel.com>
2021-06-12 21:23:12 +0300

    clang

--- cpp/daal/include/data_management/data/data_archive.h
+++ cpp/daal/include/data_management/data/data_archive.h
@@ -511,7 +511,7 @@ public:
     CompressedDataArchive(daal::data_management::CompressorImpl * compressor) : minBlockSize(1024 * 64), _errors(new services::ErrorCollection())
     {
         compressionStream = new daal::data_management::CompressionStream(compressor, minBlockSize);
-        _errors = compressionStream->getErrors();
+        _errors           = compressionStream->getErrors();
         serializedBuffer  = 0;
     }
 
@@ -631,7 +631,7 @@ public:
         : minBlockSize(1024 * 64), _errors(new services::ErrorCollection())
     {
         decompressionStream = new daal::data_management::DecompressionStream(decompressor, minBlockSize);
-        _errors = decompressionStream->getErrors();
+        _errors             = decompressionStream->getErrors();
         serializedBuffer    = 0;
     }
 
@@ -747,8 +747,8 @@ public:
      */
     InputDataArchive() : _finalized(false), _errors(new services::ErrorCollection())
     {
-        _arch = new DataArchive;
-        _errors = static_cast<DataArchive*>(_arch)->getErrors();
+        _arch   = new DataArchive;
+        _errors = static_cast<DataArchive *>(_arch)->getErrors();
         archiveHeader();
     }
 
@@ -768,8 +768,8 @@ public:
      */
     InputDataArchive(daal::data_management::CompressorImpl * compressor) : _finalized(false), _errors(new services::ErrorCollection())
     {
-        _arch = new CompressedDataArchive(compressor);
-        _errors = static_cast<CompressedDataArchive*>(_arch)->getErrors();
+        _arch   = new CompressedDataArchive(compressor);
+        _errors = static_cast<CompressedDataArchive *>(_arch)->getErrors();
         archiveHeader();
     }
 
@@ -1009,8 +1009,8 @@ public:
      */
     OutputDataArchive(InputDataArchive & arch) : _errors(new services::ErrorCollection())
     {
-        _arch = new DataArchive(arch.getDataArchive());
-        _errors = static_cast<DataArchive*>(_arch)->getErrors();
+        _arch   = new DataArchive(arch.getDataArchive());
+        _errors = static_cast<DataArchive *>(_arch)->getErrors();
         archiveHeader();
     }
 
@@ -1030,8 +1030,8 @@ public:
      */
     OutputDataArchive(byte * ptr, size_t size) : _errors(new services::ErrorCollection())
     {
-        _arch = new DataArchive(ptr, size);
-        _errors = static_cast<DataArchive*>(_arch)->getErrors();
+        _arch   = new DataArchive(ptr, size);
+        _errors = static_cast<DataArchive *>(_arch)->getErrors();
         archiveHeader();
     }
 
@@ -1040,8 +1040,8 @@ public:
      */
     OutputDataArchive(daal::data_management::DecompressorImpl * decompressor, byte * ptr, size_t size) : _errors(new services::ErrorCollection())
     {
-        _arch = new DecompressedDataArchive(decompressor);
-        _errors = static_cast<DecompressedDataArchive*>(_arch)->getErrors();
+        _arch   = new DecompressedDataArchive(decompressor);
+        _errors = static_cast<DecompressedDataArchive *>(_arch)->getErrors();
         _arch->write(ptr, size);
         archiveHeader();
     }
