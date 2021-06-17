9ab27321dac91eeaef4990b33cb98c5097652151
YakhtinLeonid <leonid.yakhtin@intel.com>
2021-06-17 13:16:14 +0300

    Add correctness tests

--- cpp/oneapi/dal/algo/shortest_paths/test/badargs.cpp
+++ cpp/oneapi/dal/algo/shortest_paths/test/badargs.cpp
@@ -21,8 +21,6 @@
 
 #include "oneapi/dal/test/engine/common.hpp"
 
-#include <iostream>
-
 namespace oneapi::dal::algo::shortest_paths::test {
 
 namespace dal = oneapi::dal;
@@ -59,21 +57,6 @@ protected:
     int source;
 };
 
-// class example_graph_type : public graph_base_data {
-// public:
-//     example_graph_type() {
-//         vertex_count = 3;
-//         edge_count = 2;
-//         cols_count = 2;
-//         rows_count = 4;
-//         source = 0;
-//     }
-//     std::array<std::int64_t, 4> rows = { 0, 1, 2, 2 };
-//     std::array<std::int32_t, 2> cols = { 1, 2 };
-//     std::array<double, 2> edge_weights = { 3, 4 };
-//     std::array<float, 3> distances = { 0, 3, 4 };
-//     std::array<float, 0> predecessors = {};
-// };
 class example_graph_type : public graph_base_data {
 public:
     example_graph_type() {
@@ -81,58 +64,101 @@ public:
         edge_count = 9;
         cols_count = 9;
         rows_count = 7;
-        source = 0;
     }
     std::array<std::int64_t, 7> rows = { 0, 2, 4, 6, 8, 9, 9 };
     std::array<std::int32_t, 9> cols = { 1, 2, 3, 4, 3, 4, 4, 5, 5 };
     std::array<double, 9> edge_weights = { 10, 20, 50, 10, 20, 33, 20, 2, 1 };
-    std::array<float, 6> distances = { 0, 10, 20, 40, 20, 21 };
-    std::array<float, 0> predecessors = {};
+};
+
+class empty_graph_type : public graph_base_data {
+public:
+    empty_graph_type() {
+        vertex_count = 0;
+        edge_count = 0;
+        cols_count = 0;
+        rows_count = 1;
+    }
+    std::array<std::int64_t, 1> rows = { 0 };
+    std::array<std::int32_t, 0> cols = {};
+    std::array<double, 0> edge_weights = {};
+};
+
+class negative_weights_graph_type : public graph_base_data {
+public:
+    negative_weights_graph_type() {
+        vertex_count = 3;
+        edge_count = 2;
+        cols_count = 2;
+        rows_count = 4;
+    }
+    std::array<std::int64_t, 4> rows = { 0, 1, 2, 2 };
+    std::array<std::int32_t, 2> cols = { 1, 2 };
+    std::array<double, 2> edge_weights = { -10, -20 };
 };
 
 class shortest_paths_badargs_test {
 public:
-template<typename GraphType>
-    void check_shortest_paths(double delta){
-    
+    template <typename GraphType>
+    void check_shortest_paths(double delta, int source, bool nothing_to_calculate = false) {
         using namespace dal::preview::shortest_paths;
         GraphType graph_data;
-        std::cout<<"init"<<std::endl;
 
         const auto graph_builder = dal::preview::detail::directed_adjacency_vector_graph_builder<
-                                                                int,
-                                                                double,
-                                                                oneapi::dal::preview::empty_value,
-                                                                int,
-                                                                std::allocator<char>>(graph_data.get_vertex_count(),
-                                                                                      graph_data.get_edge_count(),
-                                                                                      graph_data.rows.data(),
-                                                                                      graph_data.cols.data(),
-                                                                                      graph_data.edge_weights.data());
-    
-        std::cout<<"builder is created"<<std::endl;
+            int,
+            double,
+            oneapi::dal::preview::empty_value,
+            int,
+            std::allocator<char>>(graph_data.get_vertex_count(),
+                                  graph_data.get_edge_count(),
+                                  graph_data.rows.data(),
+                                  graph_data.cols.data(),
+                                  graph_data.edge_weights.data());
+
         const auto& graph = graph_builder.get_graph();
-        std::cout<<"graph is created"<<std::endl;
+
+        auto result_type = nothing_to_calculate
+                               ? optional_results::distances & optional_results::predecessors
+                               : optional_results::distances | optional_results::predecessors;
         std::allocator<char> alloc;
 
         const auto shortest_paths_desc =
             descriptor<float, method::delta_stepping, task::one_to_all, std::allocator<char>>(
-                graph_data.get_source(),
+                source,
                 delta,
-                optional_results::distances | optional_results::predecessors,
+                result_type,
                 alloc);
-        std::cout<<"descriptor is created"<<std::endl;
 
         const auto result_shortest_paths = dal::preview::traverse(shortest_paths_desc, graph);
-        std::cout<<"fine"<<std::endl;
     }
 };
 
 #define SHORTEST_PATHS_BADARG_TEST(name) \
     TEST_M(shortest_paths_badargs_test, name, "[shortest_paths][badarg]")
 
-SHORTEST_PATHS_BADARG_TEST("Check delta is > 0"){
-    REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(100)), invalid_argument);
+SHORTEST_PATHS_BADARG_TEST("Check delta is > 0") {
+    // REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(0, 0)), invalid_argument);
+    REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(-0.001, 1)),
+                      invalid_argument);
+    REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(-10000, 2)),
+                      invalid_argument);
 }
 
+SHORTEST_PATHS_BADARG_TEST("Check source is in graph ") {
+    REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(5, -2)), invalid_argument);
+    REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(5, 100)), invalid_argument);
+}
+
+SHORTEST_PATHS_BADARG_TEST("Check empty graph") {
+    REQUIRE_THROWS_AS((this->check_shortest_paths<empty_graph_type>(5, 0)), invalid_argument);
+}
+
+// SHORTEST_PATHS_BADARG_TEST("Check edges are non-negative") {
+//     REQUIRE_THROWS_AS((this->check_shortest_paths<negative_weights_graph_type>(5, 0)),
+//                       invalid_argument);
+// }
+
+SHORTEST_PATHS_BADARG_TEST("Check nothing to calculate case") {
+    REQUIRE_THROWS_AS((this->check_shortest_paths<example_graph_type>(5, 0, true)),
+                      invalid_argument);
+}
 } // namespace oneapi::dal::algo::shortest_paths::test
