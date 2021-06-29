80d98e7c2dd3f594c653192ebe440925ed6e0b77
Kirill <kirill.petrov@intel.com>
2021-06-29 10:40:37 +0300

    Clean old interfaces (#1611)

--- cpp/daal/src/algorithms/brownboost/brownboost_model_fpt.cpp
+++ cpp/daal/src/algorithms/brownboost/brownboost_model_fpt.cpp
@@ -1,4 +1,4 @@
-/* file: brownboost_model_fpt_v1.cpp */
+/* file: brownboost_model_fpt.cpp */
 /*******************************************************************************
 * Copyright 2014-2021 Intel Corporation
 *
@@ -29,47 +29,6 @@ namespace algorithms
 {
 namespace brownboost
 {
-namespace interface1
-{
-/**
- *  Constructs the BrownBoost %Model
- * \tparam modelFPType  Data type to store BrownBoost model data, double or float
- * \param[in] dummy     Dummy variable for the templated constructor
- * \DAAL_DEPRECATED_USE{ Model::create }
- */
-template <typename modelFPType>
-DAAL_EXPORT Model::Model(size_t nFeatures, modelFPType dummy) : boosting::Model(nFeatures)
-{
-    _alpha = data_management::NumericTablePtr(new data_management::HomogenNumericTable<modelFPType>(NULL, 1, 0));
-}
-
-template <typename modelFPType>
-DAAL_EXPORT Model::Model(size_t nFeatures, modelFPType dummy, services::Status & st) : boosting::Model(nFeatures, st)
-{
-    if (!st)
-    {
-        return;
-    }
-    _alpha = data_management::HomogenNumericTable<modelFPType>::create(NULL, 1, 0, &st);
-}
-
-/**
- * Constructs the BrownBoost model
- * \tparam modelFPType  Data type to store BrownBoost model data, double or float
- * \param[in]  nFeatures Number of features in the dataset
- * \param[out] stat      Status of the model construction
- */
-template <typename modelFPType>
-DAAL_EXPORT ModelPtr Model::create(size_t nFeatures, services::Status * stat)
-{
-    DAAL_DEFAULT_CREATE_IMPL_EX(Model, nFeatures, (modelFPType)0);
-}
-
-template DAAL_EXPORT Model::Model(size_t, DAAL_FPTYPE);
-template DAAL_EXPORT Model::Model(size_t, DAAL_FPTYPE, services::Status &);
-template DAAL_EXPORT ModelPtr Model::create<DAAL_FPTYPE>(size_t, services::Status *);
-} //namespace interface1
-
 /**
  *  Constructs the BrownBoost %Model
  * \tparam modelFPType  Data type to store BrownBoost model data, double or float
