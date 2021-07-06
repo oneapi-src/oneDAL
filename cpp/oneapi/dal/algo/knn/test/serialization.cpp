05ca92ab2a0fc2706cef17a78c9e4ab75a163c16
Dmitriy Kalyanov <dmitriy.kalyanov@intel.com>
2021-07-06 16:03:23 +0300

    fix test

--- cpp/oneapi/dal/algo/knn/test/serialization.cpp
+++ cpp/oneapi/dal/algo/knn/test/serialization.cpp
@@ -129,8 +129,8 @@ public:
 
     void compare_infer_results(const infer_result<task_t>& actual,
                                const infer_result<task_t>& reference) {
-        INFO("compare labels") {
-            te::check_if_tables_equal<float_t>(actual.get_labels(), reference.get_labels());
+        INFO("compare responses") {
+            te::check_if_tables_equal<float_t>(actual.get_responses(), reference.get_responses());
         }
     }
 
