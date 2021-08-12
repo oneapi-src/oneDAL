41e03d402c85c9481cebcbceea31e7828ce57cda
Ruslan Israfilov <ruslan.israfilov@intel.com>
2021-08-12 09:06:15 -0700

    Inherit MPI communicator interface from spmd_communicator_via_host_impl

--- cpp/oneapi/dal/detail/mpi/communicator.hpp
+++ cpp/oneapi/dal/detail/mpi/communicator.hpp
@@ -26,7 +26,7 @@ class mpi_error : public communication_error {};
 
 inline void mpi_call(int mpi_status) {}
 
-inline MPI_Datatype mpi_data_type(const data_type& dtype) {
+inline MPI_Datatype make_mpi_data_type(const data_type& dtype) {
     switch (dtype) {
         case data_type::int8: return MPI_INT8_T;
         case data_type::int16: return MPI_INT16_T;
@@ -43,98 +43,6 @@ inline MPI_Datatype mpi_data_type(const data_type& dtype) {
     }
     return MPI_DATATYPE_NULL;
 }
-
-#ifdef ONEDAL_DATA_PARALLEL
-class host2usm_synchronizing_buffer {
-public:
-    explicit host2usm_synchronizing_buffer(const sycl::queue& q,
-                                           byte_t* usm_ptr,
-                                           const data_type& dtype,
-                                           std::int64_t count)
-            : queue_(q),
-              usm_ptr_(usm_ptr),
-              host_ptr_(nullptr),
-              dtype_(dtype),
-              count_(count),
-              is_readonly_(false) {
-        ONEDAL_ASSERT(usm_ptr);
-        ONEDAL_ASSERT(count > 0);
-    }
-
-    explicit host2usm_synchronizing_buffer(const sycl::queue& q,
-                                           const byte_t* usm_ptr,
-                                           const data_type& dtype,
-                                           std::int64_t count)
-            : queue_(q),
-              usm_ptr_(const_cast<byte_t*>(usm_ptr)),
-              host_ptr_(nullptr),
-              dtype_(dtype),
-              count_(count),
-              is_readonly_(true) {
-        ONEDAL_ASSERT(usm_ptr);
-        ONEDAL_ASSERT(count > 0);
-    }
-
-    host2usm_synchronizing_buffer(const host2usm_synchronizing_buffer&) = delete;
-    host2usm_synchronizing_buffer& operator=(const host2usm_synchronizing_buffer&) = delete;
-
-    ~host2usm_synchronizing_buffer() {
-        // If destructor called because of exception,
-        // we don't need to write back anything
-        if (!std::uncaught_exception()) {
-            try_write_back();
-        }
-
-        free(default_host_policy{}, host_ptr_);
-        host_ptr_ = nullptr;
-    }
-
-    const byte_t* get_host_data() {
-        ONEDAL_ASSERT(!is_readonly_);
-        return try_copy_to_host();
-    }
-
-    byte_t* get_mutable_host_data() {
-        ONEDAL_ASSERT(!is_readonly_);
-        need_write_back_ = true;
-        return try_copy_to_host();
-    }
-
-    void write_back() {
-        try_write_back();
-    }
-
-private:
-    byte_t* try_copy_to_host() {
-        if (!host_ptr_) {
-            const std::int64_t dtype_size = get_data_type_size(dtype_);
-            const std::int64_t size = check_mul_overflow(dtype_size, count_);
-
-            host_ptr_ = static_cast<byte_t*>(malloc(default_host_policy{}, size));
-            memcpy_usm2host(queue_, host_ptr_, usm_ptr_, size);
-        }
-        return host_ptr_;
-    }
-
-    void try_write_back() {
-        // Write-back happens only if mutable host data was requested
-        if (host_ptr_ && need_write_back_) {
-            const std::int64_t dtype_size = get_data_type_size(dtype_);
-            const std::int64_t size = check_mul_overflow(dtype_size, count_);
-            memcpy_host2usm(queue_, usm_ptr_, host_ptr_, size);
-            need_write_back_ = false;
-        }
-    }
-
-    sycl::queue queue_;
-    byte_t* usm_ptr_;
-    byte_t* host_ptr_;
-    data_type dtype_;
-    std::int64_t count_;
-    bool is_readonly_;
-    bool need_write_back_ = false;
-};
-#endif
 
 class mpi_request_impl : public dal::detail::spmd_request_iface {
 public:
@@ -157,7 +65,7 @@ private:
     MPI_Request mpi_request_;
 };
 
-class mpi_communicator_impl : public spmd_communicator_iface {
+class mpi_communicator_impl : public spmd_communicator_via_host_impl {
 public:
     explicit mpi_communicator_impl(const MPI_Comm& mpi_comm, std::int64_t default_root = 0)
             : mpi_comm_(mpi_comm),
@@ -205,7 +113,7 @@ public:
         MPI_Request mpi_request;
         mpi_call(MPI_Ibcast(send_buf,
                             integral_cast<int>(count),
-                            mpi_data_type(dtype),
+                            make_mpi_data_type(dtype),
                             integral_cast<int>(root),
                             mpi_comm_,
                             &mpi_request));
@@ -213,48 +121,12 @@ public:
         return new mpi_request_impl{ mpi_request };
     }
 
-#ifdef ONEDAL_DATA_PARALLEL
-    spmd_request_iface* bcast(sycl::queue& q,
-                              byte_t* send_buf,
-                              std::int64_t count,
-                              const data_type& dtype,
-                              const std::vector<sycl::event>& deps,
-                              std::int64_t root) override {
-        ONEDAL_ASSERT(root >= 0);
-
-        if (count == 0) {
-            return nullptr;
-        }
-
-        ONEDAL_ASSERT(send_buf);
-        ONEDAL_ASSERT(count > 0);
-
-        check_if_queue_matches_pointer(q, send_buf);
-        sycl::event::wait_and_throw(deps);
-
-        host2usm_synchronizing_buffer sync_send_buf(q, send_buf, dtype, count);
-        wait_request(bcast(sync_send_buf.get_mutable_host_data(), count, dtype, root));
-    }
-#endif
-
     spmd_request_iface* gather(const byte_t* send_buf,
                                std::int64_t send_count,
                                byte_t* recv_buf,
                                std::int64_t recv_count,
                                const data_type& dtype,
                                std::int64_t root) override {}
-
-#ifdef ONEDAL_DATA_PARALLEL
-    spmd_request_iface* gather(sycl::queue& q,
-                               const byte_t* send_buf,
-                               std::int64_t send_count,
-                               byte_t* recv_buf,
-                               std::int64_t recv_count,
-                               const data_type& dtype,
-                               const std::vector<sycl::event>& deps,
-                               std::int64_t root) override {}
-
-#endif
 
     spmd_request_iface* gatherv(const byte_t* send_buf,
                                 std::int64_t send_count,
@@ -264,33 +136,11 @@ public:
                                 const data_type& dtype,
                                 std::int64_t root) override {}
 
-#ifdef ONEDAL_DATA_PARALLEL
-    spmd_request_iface* gatherv(sycl::queue& q,
-                                const byte_t* send_buf,
-                                std::int64_t send_count,
-                                byte_t* recv_buf,
-                                const std::int64_t* recv_counts_host,
-                                const std::int64_t* displs_host,
-                                const data_type& dtype,
-                                const std::vector<sycl::event>& deps,
-                                std::int64_t root) override {}
-#endif
-
     spmd_request_iface* allgather(const byte_t* send_buf,
                                   std::int64_t send_count,
                                   byte_t* recv_buf,
                                   std::int64_t recv_count,
                                   const data_type& dtype) override {}
-
-#ifdef ONEDAL_DATA_PARALLEL
-    spmd_request_iface* allgather(sycl::queue& q,
-                                  const byte_t* send_buf,
-                                  std::int64_t send_count,
-                                  byte_t* recv_buf,
-                                  std::int64_t recv_count,
-                                  const data_type& dtype,
-                                  const std::vector<sycl::event>& deps) override {}
-#endif
 
     spmd_request_iface* allreduce(const byte_t* send_buf,
                                   byte_t* recv_buf,
@@ -298,26 +148,7 @@ public:
                                   const data_type& dtype,
                                   const spmd_reduce_op& op) override {}
 
-#ifdef ONEDAL_DATA_PARALLEL
-    spmd_request_iface* allreduce(sycl::queue& q,
-                                  const byte_t* send_buf,
-                                  byte_t* recv_buf,
-                                  std::int64_t count,
-                                  const data_type& dtype,
-                                  const spmd_reduce_op& op,
-                                  const std::vector<sycl::event>& deps) override {}
-#endif
-
 private:
-#ifdef ONEDAL_DATA_PARALLEL
-    void check_if_pointer_matches_queue(const sycl::queue& q, void* ptr) {
-        const auto alloc_kind = sycl::get_pointer_type(ptr, q.get_context());
-        if (alloc_kind == sycl::usm::alloc::unknown) {
-            throw invalid_argument{ error_messages::unknown_usm_pointer_type() };
-        }
-    }
-#endif
-
     MPI_Comm mpi_comm_;
     std::int64_t default_root_;
     std::int64_t rank_ = -1;
@@ -326,8 +157,8 @@ private:
 
 class mpi_communicator : public spmd_communicator {
 public:
-    explicit mpi_communicator(const MPI_Comm& mpi_comm)
-            : spmd_communicator(new mpi_communicator_impl{ mp_comm }) {}
+    explicit mpi_communicator(const MPI_Comm& mpi_comm, std::int64_t default_root = 0)
+            : spmd_communicator(new mpi_communicator_impl{ mpi_comm, default_root }) {}
 };
 
 } // namespace v1
