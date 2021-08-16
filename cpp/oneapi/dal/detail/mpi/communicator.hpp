d00cb1636dd8953b6e9594596950cf708a6a2796
Ruslan Israfilov <ruslan.israfilov@intel.com>
2021-08-16 01:06:48 -0700

    Add missing MPI functions

--- cpp/oneapi/dal/detail/mpi/communicator.hpp
+++ cpp/oneapi/dal/detail/mpi/communicator.hpp
@@ -16,6 +16,10 @@
 
 #pragma once
 
+// THIS FILE SHOULD NOT BE INCLUDED TO ANY OTHER NON-MPI HEADERS, OTHERWISE
+// IT CREATES DEPENDENCY ON MPI AT THE USER'S APPLICATION COMPILE TIME
+// TODO: In the future this can be solved via __has_include C++17 feature
+
 #include <mpi.h>
 #include "oneapi/dal/detail/communicator.hpp"
 
@@ -44,6 +48,14 @@ inline MPI_Datatype make_mpi_data_type(const data_type& dtype) {
     return MPI_DATATYPE_NULL;
 }
 
+inline MPI_Op make_mpi_reduce_op(const spmd_reduce_op& op) {
+    switch (op) {
+        case spmd_reduce_op::sum: return MPI_SUM;
+        default: ONEDAL_ASSERT(!"Unknown reduce operation");
+    }
+    return MPI_OP_NULL;
+}
+
 class mpi_request_impl : public dal::detail::spmd_request_iface {
 public:
     explicit mpi_request_impl(const MPI_Request& request) : mpi_request_(request) {}
@@ -65,8 +77,19 @@ private:
     MPI_Request mpi_request_;
 };
 
+/// Implementation of the low-level SPMD communicator interface via MPI
+/// TODO: Currently message sizes are limited via `int` type.
+///       Large message sizes should be handled on the communicator side in the future.
 class mpi_communicator_impl : public spmd_communicator_via_host_impl {
 public:
+    // Explicitly declare all virtual functions with overloads to workaround Clang warning
+    // https://stackoverflow.com/questions/18515183/c-overloaded-virtual-function-warning-by-clang
+    using spmd_communicator_iface::bcast;
+    using spmd_communicator_iface::gather;
+    using spmd_communicator_iface::gatherv;
+    using spmd_communicator_iface::allgather;
+    using spmd_communicator_iface::allreduce;
+
     explicit mpi_communicator_impl(const MPI_Comm& mpi_comm, std::int64_t default_root = 0)
             : mpi_comm_(mpi_comm),
               default_root_(default_root) {}
@@ -127,7 +150,29 @@ public:
                                std::int64_t recv_count,
                                const data_type& dtype,
                                std::int64_t root) override {
-        return nullptr;
+        ONEDAL_ASSERT(root >= 0);
+
+        if (send_count == 0) {
+            return nullptr;
+        }
+
+        ONEDAL_ASSERT(send_buf);
+        if (get_rank() == root) {
+            ONEDAL_ASSERT(recv_buf);
+        }
+
+        MPI_Request mpi_request;
+        mpi_call(MPI_Igather(send_buf,
+                             integral_cast<int>(send_count),
+                             make_mpi_data_type(dtype),
+                             recv_buf,
+                             integral_cast<int>(recv_count),
+                             make_mpi_data_type(dtype),
+                             integral_cast<int>(root),
+                             mpi_comm_,
+                             &mpi_request));
+
+        return new mpi_request_impl{ mpi_request };
     }
 
     spmd_request_iface* gatherv(const byte_t* send_buf,
@@ -137,7 +182,41 @@ public:
                                 const std::int64_t* displs,
                                 const data_type& dtype,
                                 std::int64_t root) override {
-        return nullptr;
+        ONEDAL_ASSERT(root >= 0);
+
+        if (send_count == 0) {
+            return nullptr;
+        }
+
+        ONEDAL_ASSERT(send_buf);
+        if (get_rank() == root) {
+            ONEDAL_ASSERT(recv_counts);
+            ONEDAL_ASSERT(displs);
+            ONEDAL_ASSERT(recv_buf);
+        }
+
+        array<int> recv_counts_int;
+        array<int> displs_int;
+
+        if (get_rank() == root) {
+            const std::int64_t rank_count = get_rank_count();
+            recv_counts_int.reset(rank_count);
+            displs_int.reset(rank_count);
+        }
+
+        MPI_Request mpi_request;
+        mpi_call(MPI_Igatherv(send_buf,
+                              integral_cast<int>(send_count),
+                              make_mpi_data_type(dtype),
+                              recv_buf,
+                              recv_counts_int.get_data(),
+                              displs_int.get_data(),
+                              make_mpi_data_type(dtype),
+                              integral_cast<int>(root),
+                              mpi_comm_,
+                              &mpi_request));
+
+        return new mpi_request_impl{ mpi_request };
     }
 
     spmd_request_iface* allgather(const byte_t* send_buf,
@@ -145,7 +224,24 @@ public:
                                   byte_t* recv_buf,
                                   std::int64_t recv_count,
                                   const data_type& dtype) override {
-        return nullptr;
+        if (send_count == 0) {
+            return nullptr;
+        }
+
+        ONEDAL_ASSERT(send_buf);
+        ONEDAL_ASSERT(recv_buf);
+
+        MPI_Request mpi_request;
+        mpi_call(MPI_Iallgather(send_buf,
+                                integral_cast<int>(send_count),
+                                make_mpi_data_type(dtype),
+                                recv_buf,
+                                integral_cast<int>(recv_count),
+                                make_mpi_data_type(dtype),
+                                mpi_comm_,
+                                &mpi_request));
+
+        return new mpi_request_impl{ mpi_request };
     }
 
     spmd_request_iface* allreduce(const byte_t* send_buf,
@@ -153,7 +249,23 @@ public:
                                   std::int64_t count,
                                   const data_type& dtype,
                                   const spmd_reduce_op& op) override {
-        return nullptr;
+        if (count == 0) {
+            return nullptr;
+        }
+
+        ONEDAL_ASSERT(send_buf);
+        ONEDAL_ASSERT(recv_buf);
+
+        MPI_Request mpi_request;
+        mpi_call(MPI_Iallreduce(send_buf,
+                                recv_buf,
+                                integral_cast<int>(count),
+                                make_mpi_data_type(dtype),
+                                make_mpi_reduce_op(op),
+                                mpi_comm_,
+                                &mpi_request));
+
+        return new mpi_request_impl{ mpi_request };
     }
 
 private:
