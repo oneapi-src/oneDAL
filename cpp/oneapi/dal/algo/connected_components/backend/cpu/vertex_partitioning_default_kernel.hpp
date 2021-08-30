1b9b1ec778c6fd331b87523fb91253e05d9c91a4
Plastova <polina.plastova@intel.com>
2021-08-30 21:36:57 +0300

    align with comments

--- cpp/oneapi/dal/algo/connected_components/backend/cpu/vertex_partitioning_default_kernel.hpp
+++ cpp/oneapi/dal/algo/connected_components/backend/cpu/vertex_partitioning_default_kernel.hpp
@@ -27,11 +27,13 @@ namespace oneapi::dal::preview::connected_components::backend {
 using namespace oneapi::dal::preview::detail;
 using namespace oneapi::dal::preview::backend;
 
-inline void link(const std::int32_t &u, const std::int32_t &v, std::int32_t *components) {
+//Given an edge (u, v), ensures that u and v are within the same component tree, or connects them otherwise
+template <typename Cpu>
+void link(const std::int32_t &u, const std::int32_t &v, std::int32_t *components) {
     std::int32_t p1 = components[u];
     std::int32_t p2 = components[v];
-    std::int32_t h;
-    std::int32_t l;
+    std::int32_t h = 0;
+    std::int32_t l = 0;
     while (p1 != p2) {
         if (p1 > p2) {
             h = p1;
@@ -50,18 +52,21 @@ inline void link(const std::int32_t &u, const std::int32_t &v, std::int32_t *com
     }
 }
 
-inline void compress(const std::int32_t &u, std::int32_t *components) {
-    while (components[components[u]] != components[u]) {
+//Reduces component trees to single-level depth
+template <typename Cpu>
+void compress(const std::int32_t &u, std::int32_t *components) {
+    if (components[components[u]] != components[u]) {
         components[u] = components[components[u]];
     }
 }
 
-inline void order_component_ids(const std::int64_t &vertex_count,
+template <typename Cpu>
+void order_component_ids(const std::int64_t &vertex_count,
                                 std::int64_t &component_count,
                                 std::int32_t *components) {
     std::int32_t ordered_comp_id = 0;
-    const auto max_vertex_id = dal::detail::integral_cast<std::int32_t>(vertex_count - 1);
-    for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
+
+    for (std::int32_t u = 0; u < vertex_count; ++u) {
         if (components[u] == u) {
             components[u] = ordered_comp_id;
             component_count++;
@@ -74,40 +79,35 @@ inline void order_component_ids(const std::int64_t &vertex_count,
 }
 
 template <typename Cpu>
-inline std::int32_t most_frequent_element(const std::int32_t *components,
+std::int32_t most_frequent_element(const std::int32_t *components,
                                           const std::int64_t &vertex_count,
-                                          byte_alloc_iface *alloc_ptr,
-                                          const std::int32_t &samples_num = 1024) {
-    using vertex_type = std::int32_t;
-    using vertex_allocator_type = inner_alloc<vertex_type>;
-
-    vertex_allocator_type vertex_allocator(alloc_ptr);
-
-    vertex_type *rnd_vertex_ids = allocate(vertex_allocator, samples_num);
+                                          inner_alloc<std::int32_t> &vertex_allocator,
+                                          const std::int64_t &samples_count = 1024) {
+    std::int32_t *rnd_vertex_ids = allocate(vertex_allocator, samples_count);
 
     dal::backend::primitives::engine eng;
     dal::backend::primitives::rng<std::int32_t> rn_gen;
-    rn_gen.uniform(samples_num, rnd_vertex_ids, eng.get_state(), 0, vertex_count);
+    rn_gen.uniform(samples_count, rnd_vertex_ids, eng.get_state(), 0, vertex_count);
 
-    vertex_type *sample_counts = allocate(vertex_allocator, vertex_count);
-    const auto max_vertex_id = dal::detail::integral_cast<std::int32_t>(vertex_count - 1);
-    for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
-        sample_counts[u] = 0;
-    }
-    for (std::int32_t i = 0; i < samples_num; ++i) {
-        sample_counts[components[rnd_vertex_ids[i]]]++;
-    }
-    deallocate(vertex_allocator, rnd_vertex_ids, samples_num);
+    std::int32_t *root_sample_counts = allocate(vertex_allocator, vertex_count);
 
-    std::int32_t max_sample_count = 0;
+    for (std::int32_t i = 0; i < vertex_count; ++i) {
+        root_sample_counts[i] = 0;
+    }
+    for (std::int32_t i = 0; i < samples_count; ++i) {
+        root_sample_counts[components[rnd_vertex_ids[i]]]++;
+    }
+    deallocate(vertex_allocator, rnd_vertex_ids, samples_count);
+
+    std::int32_t max_root_sample_count = 0;
     std::int32_t most_frequent_root = 0;
-    for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
-        if (sample_counts[u] > max_sample_count) {
-            max_sample_count = sample_counts[u];
+    for (std::int32_t u = 0; u < vertex_count; ++u) {
+        if (root_sample_counts[u] > max_root_sample_count) {
+            max_root_sample_count = root_sample_counts[u];
             most_frequent_root = u;
         }
     }
-    deallocate(vertex_allocator, sample_counts, vertex_count);
+    deallocate(vertex_allocator, root_sample_counts, vertex_count);
 
     return most_frequent_root;
 }
@@ -118,54 +118,49 @@ struct afforest {
         const detail::descriptor_base<task::vertex_partitioning> &desc,
         const dal::preview::detail::topology<std::int32_t> &t,
         byte_alloc_iface *alloc_ptr) {
-        using vertex_type = std::int32_t;
-        using vertex_allocator_type = inner_alloc<vertex_type>;
-
+        using vertex_allocator_type = inner_alloc<std::int32_t>;
         vertex_allocator_type vertex_allocator(alloc_ptr);
-
         const auto vertex_count = t.get_vertex_count();
-        const auto max_vertex_id = dal::detail::integral_cast<std::int32_t>(vertex_count - 1);
 
-        vertex_type *components = allocate(vertex_allocator, vertex_count);
-
-        for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
+        std::int32_t *components = allocate(vertex_allocator, vertex_count);
+        for (std::int32_t u = 0; u < vertex_count; ++u) {
             components[u] = u;
         }
 
         const std::int32_t neighbors_round = 2;
 
-        for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
+        for (std::int32_t u = 0; u < vertex_count ; ++u) {
             std::int32_t neighbors_count = t.get_vertex_degree(u);
-            for (std::int32_t v = 0; (v < neighbors_count) && (v < neighbors_round); ++v) {
-                link(u, t.get_vertex_neighbors_begin(u)[v], components);
+            for (std::int32_t i = 0; (i < neighbors_count) && (i < neighbors_round); ++i) {
+                link<Cpu>(u, t.get_vertex_neighbors_begin(u)[i], components);
             }
         }
 
         const std::int32_t sample_comp =
-            most_frequent_element<Cpu>(components, vertex_count, alloc_ptr);
+            most_frequent_element<Cpu>(components, vertex_count, vertex_allocator);
 
-        for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
+        for (std::int32_t u = 0; u < vertex_count; ++u) {
             if (components[u] != sample_comp) {
                 if (t.get_vertex_degree(u) >= neighbors_round) {
                     for (auto v = t.get_vertex_neighbors_begin(u);
                          v != t.get_vertex_neighbors_end(u);
                          ++v) {
-                        link(u, *v, components);
+                        link<Cpu>(u, *v, components);
                     }
                 }
             }
         }
 
-        for (std::int32_t u = 0; u <= max_vertex_id; ++u) {
-            compress(u, components);
+        for (std::int32_t u = 0; u < vertex_count; ++u) {
+            compress<Cpu>(u, components);
         }
 
         std::int64_t component_count = 0;
-        order_component_ids(vertex_count, component_count, components);
+        order_component_ids<Cpu>(vertex_count, component_count, components);
 
-        auto labels_arr = array<vertex_type>::empty(vertex_count);
-        vertex_type *labels = labels_arr.get_mutable_data();
-        dal::backend::copy<vertex_type>(labels, components, vertex_count);
+        auto labels_arr = array<std::int32_t>::empty(vertex_count);
+        std::int32_t *labels = labels_arr.get_mutable_data();
+        dal::backend::copy<std::int32_t>(labels, components, vertex_count);
         deallocate(vertex_allocator, components, vertex_count);
 
         return vertex_partitioning_result<task::vertex_partitioning>()
