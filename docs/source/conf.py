a395d09b6d729841c9256ff2863bbe9115599336
amaklaev <artem.maklaev@intel.com>
2021-09-08 23:09:31 +0300

    format fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,46 +1,48 @@
-#===============================================================================
-# Copyright 2014-2021 Intel Corporation
+#== == == == == == == == == == == == == == == == == == == == == == == == == == \
+    == == == == == == == == == == == == == =
+#Copyright 2014 - 2021 Intel Corporation
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#Licensed under the Apache License, Version 2.0(the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#http: // www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#===============================================================================
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#== == == == == == == == == == == == == == == == == == == == == == == == == == \
+    == == == == == == == == == == == == == =
 
 ##  Content:
 ##     Intel(R) oneDAL configuration file for the Sphinx documentation builder
 ##******************************************************************************
 
-# Configuration file for the Sphinx documentation builder.
+#Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+#This file only contains a selection of the most common options.For a full
+#list see the documentation:
+#http: // www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
+#-- Path setup -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+#If extensions(or modules to document with autodoc) are in another directory,
+#add these directories to sys.path here.If the directory is relative to the
+#documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-# -- Project information -----------------------------------------------------
+#-- Project information -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 project = 'oneDAL'
 copyright = '2014 - 2021, Intel Corporation' # pylint: disable=redefined-builtin
 
-# The full version, including alpha/beta/rc tags
-# release = '2021'
+#The full version, including alpha / beta / rc tags
+#release = '2021'
 
 rst_prolog = """
 .. |short_name| replace:: oneDAL
@@ -54,34 +56,33 @@ rst_prolog = """
 """
 
 if tags.has('use_intelname'):
-    # use supplied =t use_intelname
+#use supplied = t use_intelname
     rst_prolog += ".. |full_name| replace:: Intel\ |reg|\  oneAPI Data Analytics Library"
 else:
     rst_prolog += ".. |full_name| replace:: oneAPI Data Analytics Library"
 
-
-# for substitutions in code blocks and sphinx-prompts:
+#for substitutions in code blocks and sphinx - prompts:
 substitutions = [
     ('|short_name|', 'oneDAL'),
     ('|daal_in_code|', 'daal')
     ]
 
-# -- General configuration ---------------------------------------------------
+#-- General configuration -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+#Add any Sphinx extension module names here, as strings.They can be
+#extensions coming with Sphinx(named 'sphinx.ext.*') or your custom
+#ones.
 
-# sys.path.insert(0, path_relative_to_repo_root('source/elements/oneDAL'))
+#sys.path.insert(0, path_relative_to_repo_root('source/elements/oneDAL'))
 
 extensions = ['sphinx-prompt', 'sphinx_substitution_extensions', 'sphinx.ext.extlinks', 'sphinx_tabs.tabs', 'dalapi', 'sphinx.ext.githubpages']
 
-# Add any paths that contain templates here, relative to this directory.
+#Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+#List of patterns, relative to source directory, that match files and
+#directories to ignore when looking for source files.
+#This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["opt-notice.rst", 'daal/data-management/numeric-tables/*.rst', 'onedal/get-started/*.rst',
                     'daal/algorithms/dbscan/distributed-steps/*',
                     'daal/algorithms/kmeans/includes/*',
@@ -96,31 +97,31 @@ extlinks = {
     'cpp_sample': ('https://github.com/oneapi-src/oneDAL/tree/master/samples/daal/cpp/%s', '')
 }
 
-# -- Options for HTML output -------------------------------------------------
+#-- Options for HTML output -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+#The theme to use for HTML and HTML Help pages.See the documentation for
+#a list of builtin themes.
 #
 ## html_theme = 'alabaster'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+#Add any paths that contain custom static files(such as style sheets) here,
+#relative to this directory.They are copied after the builtin static files,
+#so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# html_context = {
-#     'css_files': [
-#         '_static/style.css',  # override wide tables in RTD theme
-#         ],
-#     }
+#html_context = {
+#'css_files' : [
+#'_static/style.css', #override wide tables in RTD theme
+#],
+#}
 
 
 html_theme = 'sphinx_book_theme'
 html_logo = '_static/oneAPI-rgb-rev-100.png'
 html_favicon = '_static/favicons.png'
-# html_theme_path = ['_themes']
+#html_theme_path = ['_themes']
 
-# Theme options
+#Theme options
 html_theme_options = {
     'repository_url': 'https://github.com/oneapi-src/oneDAL',
     'path_to_docs': 'docs/source',
@@ -130,23 +131,23 @@ html_theme_options = {
     'extra_footer': '<p align="right"><a href="https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html">Cookies</a></p>'
 }
 
-# oneDAL project directory is needed for `dalapi` extension
+#oneDAL project directory is needed for `dalapi` extension
 onedal_enable_listing = False
 onedal_relative_doxyfile_dir = '../doxygen/oneapi'
 onedal_relative_sources_dir = '../../cpp/oneapi/dal'
 
-# ignore these missing references during a doc build
+#ignore these missing references during a doc build
 nitpick_ignore = [
-    # top level namespace
+#top level namespace
     ('cpp:identifier', 'dal'),
-    # method
+#method
     ('cpp:identifier', 'method'),
     ('cpp:identifier', 'Method'),
-    # task
+#task
     ('cpp:identifier', 'task'),
     ('cpp:identifier', 'task::by_default'),
     ('cpp:identifier', 'Task'),
-    # detail
+#detail
     ('cpp:identifier', 'detail'),
     ('cpp:identifier', 'detail::descriptor_base<>'),
     ('cpp:identifier', 'detail::descriptor_base<>::float_t'),
@@ -158,12 +159,12 @@ nitpick_ignore = [
     ('cpp:identifier', 'detail::enable_if_classification_t<T>'),
     ('cpp:identifier', 'detail::enable_if_regression_t<T>'),
     ('cpp:identifier', 'detail::descriptor_base<>::kernel_t'),
-    # data types
+#data types
     ('cpp:identifier', 'int64_t'),
     ('cpp:identifier', 'data_t'),
     ('cpp:identifier', 'kernel_t'),
     ('cpp:identifier', 'distance_t'),
-    # knn
+#knn
     ('cpp:identifier', 'knn'),
     ('cpp:identifier', 'knn::descriptor'),
     ('cpp:identifier', 'knn::train_result'),
@@ -172,15 +173,15 @@ nitpick_ignore = [
     ('cpp:identifier', 'knn::infer_input'),
     ('cpp:identifier', 'detail::enable_if_brute_force_t<M>'),
     ('cpp:identifier', 'detail::enable_if_search_t<T>'),
-    # minkowski_distance
+#minkowski_distance
     ('cpp:identifier', 'minkowski_distance'),
     ('cpp:identifier', 'minkowski_distance::descriptor'),
     ('cpp:identifier', 'oneapi::dal::minkowski_distance'),
     ('cpp:identifier', 'oneapi::dal::minkowski_distance::descriptor<Float>'),
-    # chebyshev_distance
+#chebyshev_distance
     ('cpp:identifier', 'chebyshev_distance'),
     ('cpp:identifier', 'chebyshev_distance::descriptor'),
-    # kmeans
+#kmeans
     ('cpp:identifier', 'kmeans'),
     ('cpp:identifier', 'kmeans::descriptor'),
     ('cpp:identifier', 'kmeans::train_result'),
@@ -193,20 +194,20 @@ nitpick_ignore = [
     ('cpp:identifier', 'infer_input::model'),
     ('cpp:identifier', 'infer_input::model::centroids'),
     ('cpp:identifier', ),
-    # kmeans_init
+#kmeans_init
     ('cpp:identifier', 'kmeans_init'),
     ('cpp:identifier', 'kmeans_init::descriptor'),
     ('cpp:identifier', 'kmeans_init::compute_input'),
     ('cpp:identifier', 'kmeans_init::compute_result'),
     ('cpp:identifier', 'compute'),
-    # pca
+#pca
     ('cpp:identifier', 'pca'),
     ('cpp:identifier', 'pca::descriptor'),
     ('cpp:identifier', 'pca::train_result'),
     ('cpp:identifier', 'pca::train_input'),
     ('cpp:identifier', 'pca::infer_result'),
     ('cpp:identifier', 'pca::infer_input'),
-    # svm
+#svm
     ('cpp:identifier', 'svm'),
     ('cpp:identifier', 'svm::descriptor'),
     ('cpp:identifier', 'svm::train_result'),
@@ -221,27 +222,27 @@ nitpick_ignore = [
     ('cpp:identifier', 'task::regression'),
     ('cpp:identifier', 'task::classification'),
     ('cpp:identifier', 'detail::enable_if_regression_t<T>'),
-    # linear kernel
+#linear kernel
     ('cpp:identifier', 'linear_kernel'),
     ('cpp:identifier', 'linear_kernel::descriptor'),
     ('cpp:identifier', 'linear_kernel::compute_result'),
     ('cpp:identifier', 'linear_kernel::compute_input'),
-    # polynomial kernel
+#polynomial kernel
     ('cpp:identifier', 'polynomial_kernel'),
     ('cpp:identifier', 'polynomial_kernel::descriptor'),
     ('cpp:identifier', 'polynomial_kernel::compute_result'),
     ('cpp:identifier', 'polynomial_kernel::compute_input'),
-    # rbf kernel
+#rbf kernel
     ('cpp:identifier', 'rbf_kernel'),
     ('cpp:identifier', 'rbf_kernel::descriptor'),
     ('cpp:identifier', 'rbf_kernel::compute_result'),
     ('cpp:identifier', 'rbf_kernel::compute_input'),
-    # sigmoid kernel
+#sigmoid kernel
     ('cpp:identifier', 'sigmoid_kernel'),
     ('cpp:identifier', 'sigmoid_kernel::descriptor'),
     ('cpp:identifier', 'sigmoid_kernel::compute_result'),
     ('cpp:identifier', 'sigmoid_kernel::compute_input'),
-    # decision forest
+#decision forest
     ('cpp:identifier', 'decision_forest'),
     ('cpp:identifier', 'decision_forest::infer_result'),
     ('cpp:identifier', 'decision_forest::infer_input'),
@@ -256,7 +257,7 @@ nitpick_ignore = [
     ('cpp:identifier', 'error_metric_mode::none'),
     ('cpp:identifier', 'error_metric_mode::out_of_bag_error'),
     ('cpp:identifier', 'error_metric_mode::out_of_bag_error_per_observation'),
-    # basic statistics 
+#basic statistics 
     ('cpp:identifier', 'basic_statistics'),
     ('cpp:identifier', 'basic_statistics::descriptor'),
     ('cpp:identifier', 'basic_statistics::compute_result'),
@@ -268,12 +269,12 @@ nitpick_ignore = [
     ('cpp:identifier', 'oneapi::dal::basic_statistics::method'),
     ('cpp:identifier', 'oneapi::dal::basic_statistics::method::v1'),
     ('cpp:identifier', 'oneapi::dal::basic_statistics::method::v1::dense'),
-    # common for algorithms
+#common for algorithms
     ('cpp:identifier', 'result'),
-    # common for result options
+#common for result options
     ('cpp:identifier', 'result_option_id_base'),
     ('cpp:identifier', 'result_option_id'),
-    # tables
+#tables
     ('cpp:identifier', 'table'),
     ('cpp:identifier', 'row_count'),
     ('cpp:identifier', 'column_count'),
@@ -289,23 +290,23 @@ nitpick_ignore = [
     ('cpp:identifier', 'mutable_data'),
     ('cpp:identifier', 'data'),
     ('cpp:identifier', 'count'),
-    # array
+#array
     ('cpp:identifier', 'array'),
     ('cpp:identifier', 'T'),
     ('cpp:identifier', 'array<T>'),
     ('cpp:identifier', 'array<Y>'),
     ('cpp:identifier', 'has_mutable_data'),
-    # csv
+#csv
     ('cpp:identifier', 'csv'),
     ('cpp:identifier', 'read'),
     ('cpp:identifier', 'read_options'),
     ('cpp:identifier', 'default_read_options'),
     ('cpp:identifier', 'default_delimiter'),
     ('cpp:identifier', 'Object'),
-    # oneapi
+#oneapi
     ('cpp:identifier', 'oneapi'),
     ('cpp:identifier', 'oneapi::dal'),
-    # oneapi - kmeans
+#oneapi - kmeans
     ('cpp:identifier', 'oneapi::dal::kmeans'),
     ('cpp:identifier', 'oneapi::dal::kmeans::task'),
     ('cpp:identifier', 'oneapi::dal::decision_forest'),
@@ -316,7 +317,7 @@ nitpick_ignore = [
     ('cpp:identifier', 'oneapi::dal::svm::method'),
     ('cpp:identifier', 'oneapi::dal::svm::task'),
 
-    # graphs
+#graphs
     ('cpp:identifier', 'vertex_size_type<Graph>'),
     ('cpp:identifier', 'edge_size_type<Graph>'),
     ('cpp:identifier', 'vertex_type<Graph>'),
