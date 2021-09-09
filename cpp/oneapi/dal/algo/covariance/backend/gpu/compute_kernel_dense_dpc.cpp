f170b0a1d7aae071d8cb58ab0622ba0933474f75
asolovev <aleksandr.solovev@intel.com>
2021-09-09 21:25:31 +0300

    first commit

--- cpp/oneapi/dal/algo/covariance/backend/gpu/compute_kernel_dense_dpc.cpp
+++ cpp/oneapi/dal/algo/covariance/backend/gpu/compute_kernel_dense_dpc.cpp
@@ -14,27 +14,96 @@
 * limitations under the License.
 *******************************************************************************/
 
-#include "daal/src/algorithms/covariance/oneapi/covariance_kernel_oneapi.h"
-
 #include "oneapi/dal/algo/covariance/backend/gpu/compute_kernel.hpp"
 #include "oneapi/dal/backend/interop/common_dpc.hpp"
 #include "oneapi/dal/backend/interop/error_converter.hpp"
 #include "oneapi/dal/backend/interop/table_conversion.hpp"
-
+#include "oneapi/dal/backend/primitives/lapack.hpp"
+#include "oneapi/dal/backend/primitives/reduction.hpp"
+#include "oneapi/dal/backend/primitives/stat.hpp"
+#include "oneapi/dal/backend/primitives/utils.hpp"
 #include "oneapi/dal/table/row_accessor.hpp"
 
 namespace oneapi::dal::covariance::backend {
 
+namespace pr = oneapi::dal::backend::primitives;
+
+using dal::backend::context_cpu;
 using dal::backend::context_gpu;
+using input_t = train_input<task::dim_reduction>;
+using result_t = train_result<task::dim_reduction>;
 using descriptor_t = detail::descriptor_base<task::compute>;
 
-namespace daal_covariance = daal::algorithms::covariance;
-namespace interop = dal::backend::interop;
 
 template <typename Float>
-using daal_covariance_kernel_t = daal_covariance::oneapi::internal::
-    CovarianceDenseBatchKernelOneAPI<Float, daal_covariance::Method::defaultDense>;
+inline auto compute_means(sycl::queue& queue,
+                          const pr::ndview<Float, 2>& data,
+                          const pr::ndview<Float, 1>& means,
+                          const dal::backend::event_vector& deps = {}) {
+    
+    const std::int64_t column_count = data.get_dimension(1);
+    const std::int64_t row_count = data.get_dimension(0);
 
+    auto sums = pr::ndarray<Float, 1>::empty(q, { column_count }, sycl::usm::alloc::device);
+    auto reduce_event =
+        pr::reduce_by_columns(q, data, sums, pr::sum<Float>{}, pr::identity<Float>{}, deps);
+    
+    const Float inv_n = Float(1.0 / double(row_count));
+
+    const Float* sums_ptr = sums.get_data();
+    Float* means_ptr = means.get_mutable_data();
+
+    const Float eps = std::numeric_limits<Float>::epsilon();
+
+    return q.submit([&](sycl::handler& cgh) {
+        const auto range = make_multiple_nd_range_1d(p, device_max_wg_size(q));
+
+        cgh.depends_on(deps);
+        cgh.parallel_for(range, [=](sycl::nd_item<1> id) {
+            const std::int64_t i = id.get_global_id();
+            if (i < p) {
+                const Float s = sums_ptr[i];
+                means_ptr[i] = inv_n * s;
+            }
+        });
+    });
+}
+
+template <typename Float>
+inline auto compute_covariance(sycl::queue& queue,
+                               const pr::ndview<Float, 2>& data,
+                               const pr::ndview<Float, 1>& means,
+                               const pr::ndview<Float, 2>& covariance,
+                               const dal::backend::event_vector& deps = {}) {
+    const std::int64_t column_count = data.get_dimension(1);
+    const std::int64_t row_count = data.get_dimension(0);
+
+    auto sums = pr::ndarray<Float, 1>::empty(q, { column_count }, sycl::usm::alloc::device);
+    auto reduce_event =
+        pr::reduce_by_columns(q, data, sums, pr::sum<Float>{}, pr::identity<Float>{}, deps);
+    
+    const Float inv_n = Float(1.0 / double(row_count));
+    const Float inv_n1 = (row_count > 1.0f) ? Float(1.0 / double(row_count - 1)) : 1.0f;
+
+    const Float* sums_ptr = sums.get_data();
+    const Float* corr_ptr = corr.get_mutable_data();
+    Float* means_ptr = means.get_mutable_data();
+
+    const Float eps = std::numeric_limits<Float>::epsilon();
+
+    return q.submit([&](sycl::handler& cgh) {
+        const auto range = make_multiple_nd_range_1d(p, device_max_wg_size(q));
+
+        cgh.depends_on(deps);
+        cgh.parallel_for(range, [=](sycl::nd_item<1> id) {
+            const std::int64_t i = id.get_global_id();
+            if (i < p) {
+                const Float s = sums_ptr[i];
+                means_ptr[i] = inv_n * s;
+            }
+        });
+    });
+}
 template <typename Float, typename Task>
 static compute_result<Task> call_daal_kernel(const context_gpu& ctx,
                                              const descriptor_t& desc,
@@ -113,19 +182,39 @@ static compute_result<Task> call_daal_kernel(const context_gpu& ctx,
     return result;
 }
 
-template <typename Float, typename Task>
-static compute_result<Task> compute(const context_gpu& ctx,
-                                    const descriptor_t& desc,
-                                    const compute_input<Task>& input) {
-    return call_daal_kernel<Float, Task>(ctx, desc, input.get_data());
+template <typename Float>
+static result_t compute(const context_gpu& ctx, const descriptor_t& desc, const input_t& input) {
+    const auto x = input.get_x();
+    const auto y = input.get_y();
+
+    auto& queue = ctx.get_queue();
+
+    const std::int64_t x_row_count = x.get_row_count();
+    const std::int64_t y_row_count = y.get_row_count();
+
+    ONEDAL_ASSERT(x.get_column_count() == y.get_column_count());
+    dal::detail::check_mul_overflow(x_row_count, y_row_count);
+
+    const auto x_nd = pr::table2ndarray<Float>(queue, x, sycl::usm::alloc::device);
+    const auto y_nd = pr::table2ndarray<Float>(queue, y, sycl::usm::alloc::device);
+
+    auto res_nd =
+        pr::ndarray<Float, 2>::empty(queue, { x_row_count, y_row_count }, sycl::usm::alloc::device);
+
+    auto compute_rbf_event = compute_rbf(queue, x_nd, y_nd, res_nd, desc.get_sigma());
+
+    const auto res_array = res_nd.flatten(queue, { compute_rbf_event });
+    auto res_table = homogen_table::wrap(res_array, x_row_count, y_row_count);
+
+    return result_t{}.set_values(res_table);
 }
 
 template <typename Float>
-struct compute_kernel_gpu<Float, method::by_default, task::compute> {
-    compute_result<task::compute> operator()(const context_gpu& ctx,
-                                             const descriptor_t& desc,
-                                             const compute_input<task::compute>& input) const {
-        return compute<Float, task::compute>(ctx, desc, input);
+struct compute_kernel_gpu<Float, method::dense, task::compute> {
+    result_t operator()(const context_gpu& ctx,
+                        const descriptor_t& desc,
+                        const input_t& input) const {
+        return compute<Float>(ctx, desc, input);
     }
 };
 
