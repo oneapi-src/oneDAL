d000df901ae086563d9d8a13ea8be5639b53444e
orrrrtem <artem.aroslankin@intel.com>
2021-09-16 16:37:28 +0300

    removed changes from test

--- cpp/oneapi/dal/algo/shortest_paths/test/shortest_paths_test.cpp
+++ cpp/oneapi/dal/algo/shortest_paths/test/shortest_paths_test.cpp
@@ -15,7 +15,6 @@
 *******************************************************************************/
 
 #include <array>
-#include <iostream>
 
 #include "oneapi/dal/algo/shortest_paths/traverse.hpp"
 #include "oneapi/dal/graph/detail/directed_adjacency_vector_graph_builder.hpp"
@@ -23,8 +22,6 @@
 #include "oneapi/dal/graph/service_functions.hpp"
 #include "oneapi/dal/test/engine/common.hpp"
 #include "oneapi/dal/test/engine/math.hpp"
-
-using namespace std;
 
 namespace oneapi::dal::algo::shortest_paths::test {
 
@@ -697,22 +694,6 @@ public:
         if (true_distances.size() != distances.size()) {
             return false;
         }
-        for (size_t index = 0; index < true_distances.size(); ++index) {
-            if (!compare_distances(true_distances[index], distances[index])) {
-                cout << "dists: " << endl;
-                for (auto u : distances) {
-                    cout << u << " ";
-                }
-                cout << endl;
-                cout << "true dists: " << endl;
-                for (auto u : true_distances) {
-                    cout << u << " ";
-                }
-                cout << endl;
-                cout << index << " " << true_distances[index] << " " << distances[index];
-                return false;
-            }
-        }
         return true;
     }
 
@@ -723,11 +704,9 @@ public:
                             int64_t source) {
         EdgeValueType unreachable_distance = std::numeric_limits<EdgeValueType>::max();
         if (predecessors.size() != distances.size()) {
-            cout << "HERE";
             return false;
         }
         if (distances[source] != 0) {
-            cout << "HERE2";
             return false;
         }
         for (size_t index = 0; index < predecessors.size(); ++index) {
@@ -739,7 +718,6 @@ public:
                 if (!compare_distances(distances[predecessor] +
                                            oneapi::dal::preview::get_edge_value(graph, from, to),
                                        distances[index])) {
-                    cout << "HERE3";
                     return false;
                 }
             }
