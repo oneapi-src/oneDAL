d23a9961e2560f2daf3c35b9678b4cc43dc1819f
vmeshche <vladislav.meshcheryakov@intel.com>
2021-10-20 16:05:02 +0300

    fix typo

--- cpp/daal/src/services/compiler/generic/env_detect_features.cpp
+++ cpp/daal/src/services/compiler/generic/env_detect_features.cpp
@@ -258,6 +258,11 @@ DAAL_EXPORT bool __daal_serv_cpu_extensions_available()
 
 DAAL_EXPORT int __daal_serv_cpu_detect(int enable)
 {
+    if ((enable & daal::services::Environment::avx512_mic_e1) == daal::services::Environment::avx512_mic_e1)
+    {
+        fpk_serv_enable_instructions(MKL_ENABLE_AVX512_MIC_E1);
+    }
+
 #if defined(__APPLE__)
     __daal_serv_CPUHasAVX512f_enable_it_mac();
 #endif
@@ -268,9 +273,9 @@ DAAL_EXPORT int __daal_serv_cpu_detect(int enable)
 
     if (check_avx512_mic_features() && daal_check_is_intel_cpu())
     {
-        return daal::avx512;
+        return daal::avx512_mic;
     }
-    
+
     if (check_avx2_features())
     {
         return daal::avx2;
