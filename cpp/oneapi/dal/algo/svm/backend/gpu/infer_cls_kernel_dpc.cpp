2049218ccb30f42ad4ae92710c02e819e26d3ff5
dmitrii-kriukov <dmitrii.kriukov@intel.com>
2021-10-28 15:40:46 +0300

    rewirte responces compute on dpcpp

--- cpp/oneapi/dal/algo/svm/backend/gpu/infer_cls_kernel_dpc.cpp
+++ cpp/oneapi/dal/algo/svm/backend/gpu/infer_cls_kernel_dpc.cpp
@@ -14,21 +14,17 @@
 * limitations under the License.
 *******************************************************************************/
 
-//#include <daal/src/algorithms/svm/oneapi/svm_predict_kernel_oneapi.h>
-
 #include "oneapi/dal/table/row_accessor.hpp"
 #include "oneapi/dal/algo/svm/backend/gpu/infer_kernel.hpp"
 #include "oneapi/dal/algo/svm/backend/gpu/svm_predict.hpp"
 #include "oneapi/dal/algo/svm/backend/model_conversion.hpp"
 #include "oneapi/dal/algo/svm/backend/kernel_function_impl.hpp"
-#include "oneapi/dal/backend/interop/common_dpc.hpp"
-#include "oneapi/dal/backend/interop/error_converter.hpp"
-#include "oneapi/dal/backend/interop/table_conversion.hpp"
 #include "oneapi/dal/backend/transfer.hpp"
 
 #include "oneapi/dal/backend/primitives/blas.hpp"
 #include "oneapi/dal/backend/primitives/utils.hpp"
 
+#include "oneapi/dal/detail/profiler.hpp"
 
 namespace oneapi::dal::svm::backend {
 
@@ -38,14 +34,36 @@ using input_t = infer_input<task::classification>;
 using result_t = infer_result<task::classification>;
 using descriptor_t = detail::descriptor_base<task::classification>;
 
-namespace daal_svm = daal::algorithms::svm;
-namespace daal_kernel_function = daal::algorithms::kernel_function;
 namespace interop = dal::backend::interop;
 namespace pr = dal::backend::primitives;
+namespace be = dal::backend;
+
+template <typename Float>
+pr::ndarray<Float, 1> get_responses(sycl::queue& queue,
+                                    const pr::ndarray<Float, 1>& distances,
+                                    const model_t& model,
+                                    const be::event_vector& deps = {}) {
+    const auto size = distances.get_count();
+    auto response = pr::ndarray<Float, 1>::empty(queue, size, sycl::usm::alloc::device);
+
+    auto response_data = response.get_mutable_data();
+    const auto distance_data = distances.get_data();
+
+    const auto second_class_response = model.get_second_class_response();
+    const auto first_class_response = model.get_first_class_response();
+
+    auto res_event = queue.submit([&](sycl::handler& h) {
+        h.depends_on(deps);
+        h.parallel_for(be::make_range_1d(size), [=](sycl::id<1> idx) {
+            response_data[idx] =
+                distance_data[idx] >= 0 ? second_class_response : first_class_response;
+        });
+    });
+    return response;
+}
 
 template <typename Float>
 static result_t infer(const context_gpu& ctx, const descriptor_t& desc, const input_t& input) {
-//    return call_daal_kernel<Float>(ctx, desc, input.get_model(), input.get_data());
     auto& queue = ctx.get_queue();
 
     const std::uint64_t class_count = desc.get_class_count();
@@ -62,117 +80,65 @@ static result_t infer(const context_gpu& ctx, const descriptor_t& desc, const in
         throw internal_error{ dal::detail::error_messages::unknown_kernel_function_type() };
     }
 
-    const std::int64_t row_count  = data.get_row_count();
+    const std::int64_t row_count = data.get_row_count();
 
-// DAAL_ASSERT(result.getNumberOfRows() == nVectors)
-// DAAL_ASSERT(result.getNumberOfColumns() == 1)
-
-//    BlockDescriptor<algorithmFPType> resultBlock;
-//    DAAL_CHECK_STATUS(status, result.getBlockOfRows(0, nVectors, ReadWriteMode::writeOnly, resultBlock));
-//    auto distanceBuff = resultBlock.getBuffer();
-
-     auto distance_nd = pr::ndarray<Float, 1>::empty(queue, { row_count }, sycl::usm::alloc::device);
+    auto distance_nd = pr::ndarray<Float, 1>::empty(queue, { row_count }, sycl::usm::alloc::device);
 
     auto sv_coeff_table = trained_model.get_coeffs();
-    const std::int64_t n_sv  = trained_model.get_support_vector_count();
+    const std::int64_t n_sv = trained_model.get_support_vector_count();
 
     if (n_sv == 0) {
-        auto fill_event = distance_nd.fill(queue, Float(0.0));
-        fill_event.wait_and_throw();
-    } else {
-        const auto sv_coeff_buff = pr::table2ndarray_1d<Float>(queue, sv_coeff_table, sycl::usm::alloc::device);
+        distance_nd.fill(queue, Float(0)).wait_and_throw();
+    }
+    else {
+        const auto sv_coeff_buff =
+            pr::table2ndarray_1d<Float>(queue, sv_coeff_table, sycl::usm::alloc::device);
 
         const auto bieses = pr::table2ndarray_1d<Float>(trained_model.get_biases());
         const auto bias = *(bieses.get_data());
-        std::cout << "bias=" << bias << "\n";
         auto fill_event = distance_nd.fill(queue, bias);
-        fill_event.wait_and_throw();
 
-        auto svTable = trained_model.get_support_vectors();
-
-        // const std::int64_t n_rows_per_block = xTable->getDataLayout() == NumericTableIface::csrArray ? nVectors : 1024;
-        // const std::int64_t nblocks       = nVectors / n_rows_per_block + !!(nVectors % n_rows_per_block);
+        auto sv_table = trained_model.get_support_vectors();
 
         const std::int64_t n_rows_per_block = 1024;
-        const std::int64_t nblocks       = row_count / n_rows_per_block + !!(row_count % n_rows_per_block);
-
-//        std::cout << "NBlocks" << nblocks << "\n";
+        const std::int64_t nblocks =
+            row_count / n_rows_per_block + !!(row_count % n_rows_per_block);
 
         std::shared_ptr<predict_task<Float>> predict_task =
-            std::make_shared<predict_task_dense<Float>>(queue, n_rows_per_block, data, svTable, kernel_ptr);
+            std::make_shared<predict_task_dense<Float>>(n_rows_per_block,
+                                                        data,
+                                                        sv_table,
+                                                        kernel_ptr);
 
-        for (std::int64_t iblock = 0; iblock < nblocks; ++iblock)
-        {
-//            std::cout << "IBlock:" << iblock << "\n nblocks: " << nblocks << "\n";
-            const std::int64_t start_row          = iblock * n_rows_per_block;
+        for (std::int64_t iblock = 0; iblock < nblocks; ++iblock) {
+            const std::int64_t start_row = iblock * n_rows_per_block;
+            const std::int64_t n_rows_per_block_real =
+                (iblock != nblocks - 1) ? n_rows_per_block : row_count - iblock * n_rows_per_block;
 
-            // std::cout << "startrow=" << start_row << "\n";
-            const std::int64_t n_rows_per_block_real = (iblock != nblocks - 1) ? n_rows_per_block : row_count - iblock * n_rows_per_block;
-                        // std::cout << "n_rows_per_block_real=" << n_rows_per_block_real << "\n";
-
-            auto distance_view = pr::ndarray<Float, 2>::wrap(distance_nd.get_mutable_data() + start_row, {n_rows_per_block_real, 1});
-            
-            auto kernelResBuff = predict_task->kernel_compute(queue, start_row, n_rows_per_block_real);
-
-            // std::cout << "****kernelResBuff****\n";
-
-            // auto print1 = kernelResBuff.to_host(queue).get_data();
-            // for(int i = 1079000; i < 1084000; ++i) {
-            //     std::cout << print1[i] << " ";
-            // }
-            
-            // std::cout << "\n****kernelResBuff****\n";
-
+            auto distance_view =
+                pr::ndarray<Float, 2>::wrap(distance_nd.get_mutable_data() + start_row,
+                                            { n_rows_per_block_real, 1 });
+            auto kernel_res = predict_task->kernel_compute(queue, start_row, n_rows_per_block_real);
             auto reshape_sv_coeff = sv_coeff_buff.reshape(pr::ndshape<2>{ n_sv, 1 });
-
-            // std::cout << "****reshape_sv_coeff****\n";
-
-            // auto print2 = sv_coeff_buff.to_host(queue).get_data();
-            // for(int i = 0; i <  sv_coeff_buff.to_host(queue).get_count() && i < 1000; ++i) {
-            //     std::cout << print2[i] << " ";
-            // }
-            
-            // std::cout << "\n****reshape_sv_coeff****\n";
-
-            auto gemm_event = pr::gemm(queue, kernelResBuff, reshape_sv_coeff, distance_view, Float(1), Float(1));
-            /// m = n_rows_per_block_real n = 1 k = n_sv alpha =  algorithmFPType(1.0) a_buffer = kernelResBuff lda = n_sv offsetA = 0 b_buffer = sv_coeff_buff ldb = 1 offsetB = 0 beta = 1.0 c_buffer = distaceBuff  ldc = 1 offsetC = start_row
-            // A = m x k ; B = k x n
-            gemm_event.wait_and_throw();
-            // std::cout << "****distance_nd****\n" ;
-
-            // auto print3 = distance_nd.to_host(queue).get_data();
-            // for(int i = 0; i < distance_nd.to_host(queue).get_count() && i < 1000; ++i) {
-            //     std::cout << print3[i] << " ";
-            // }
-
-            // std::cout << "\n****distance_nd****\n";
+            {
+                ONEDAL_PROFILER_TASK(gemm, queue);
+                pr::gemm(queue,
+                         kernel_res,
+                         reshape_sv_coeff,
+                         distance_view,
+                         Float(1),
+                         Float(1),
+                         { fill_event })
+                    .wait_and_throw();
+            }
         }
-        }
-///
-    // std::cout << "********\n";
-
-    // auto print = distance_nd.to_host(queue).get_data();
-    // for(int i = 0; i < distance_nd.get_count(); ++i) {
-    //     std::cout << print[i] << " ";
-    // }
-
-    // std::cout << "\n********\n";
-
-    const auto arr_decision_func_host = distance_nd.to_host(queue);
-    const auto arr_decision_data = arr_decision_func_host.get_data();
-
-    // TODO: rework with help dpcpp code
-    auto arr_response = pr::ndarray<Float, 1>::empty(row_count * 1);
-    auto response_data = arr_response.get_mutable_data();
-    for (std::int64_t i = 0; i < row_count; ++i) {
-        response_data[i] = arr_decision_data[i] >= 0
-                               ? trained_model.get_second_class_response()
-                               : trained_model.get_first_class_response();
     }
 
+    auto response_nd = get_responses(queue, distance_nd, trained_model, {});
+
     return result_t()
-        .set_decision_function(homogen_table::wrap(arr_decision_func_host.flatten(), row_count, 1))
-        .set_responses(homogen_table::wrap(arr_response.flatten(), row_count, 1));
+        .set_decision_function(homogen_table::wrap(distance_nd.flatten(queue), row_count, 1))
+        .set_responses(homogen_table::wrap(response_nd.flatten(queue), row_count, 1));
 }
 
 template <typename Float>
--- cpp/oneapi/dal/algo/svm/backend/gpu/svm_predict.hpp
+++ cpp/oneapi/dal/algo/svm/backend/gpu/svm_predict.hpp
@@ -19,8 +19,6 @@
 #include "oneapi/dal/backend/primitives/utils.hpp"
 #include "oneapi/dal/backend/primitives/ndarray.hpp"
 #include "oneapi/dal/table/common.hpp"
-//#include "oneapi/dal/algo/svm/backend/gpu/misc.hpp"
-//#include "oneapi/dal/algo/svm/backend/gpu/svm_predict.hpp"
 
 namespace oneapi::dal::svm::backend {
 
@@ -33,72 +31,51 @@ class predict_task {
 public:
     virtual ~predict_task() = default;
 
-    pr::ndarray<Float, 2> kernel_compute(sycl::queue& q, const std::int64_t start_row, const std::int64_t n_rows)
-    {
-
-        auto data_nd = pr::table2ndarray<Float>(this->queue_, this->x_table_);
-        auto data = data_nd.get_data() + data_nd.get_dimension(1) * start_row;
-
-        table x_block_nt = homogen_table::wrap(q, data, n_rows, data_nd.get_dimension(1));
-        //auto x_block_nt = get_block_nt_data(start_row, n_rows);
-
-        // auto byaka = pr::table2ndarray<Float>(q, x_block_nt, sycl::usm::alloc::device);
-        // auto byaka_host = byaka.to_host(q);
-        // auto byaka_data = byaka_host.get_data();
-
-        // for(int i = 0; i < byaka.get_count(); ++i) {
-        //     std::cout << byaka_data[i] << ' ';
-        // }
-
-        // std::cout << x_block_nt.get_row_count() << " ";
-        // std::cout << x_block_nt.get_column_count() << "\n";
-
-        const auto result =
-            kernel_->compute_kernel_function(dal::detail::data_parallel_policy(q),
-                                                x_block_nt,
-                                                sv_table_);
-
-        return pr::table2ndarray<Float>(q, result, sycl::usm::alloc::device);
-    }
+    virtual pr::ndarray<Float, 2> kernel_compute(sycl::queue& q,
+                                                 const std::int64_t start_row,
+                                                 const std::int64_t n_rows) = 0;
 
 protected:
-    predict_task(sycl::queue& q, const std::int64_t max_row_per_block, const table & x_table, const table & sv_table,
-                const detail::kernel_function_ptr & kernel)
-        : x_table_(x_table), sv_table_(sv_table), _n_size(sv_table.get_row_count()), queue_(q)
-    {
-        kernel_ = kernel;
-    }
-
-    virtual table get_block_nt_data(const std::int64_t start_row, const std::int64_t n_rows) = 0;
+    predict_task(const std::int64_t max_row_per_block,
+                 const table& x_table,
+                 const table& sv_table,
+                 const detail::kernel_function_ptr& kernel)
+            : x_table_(x_table),
+              sv_table_(sv_table),
+              kernel_(kernel) {}
 
 protected:
     const table& x_table_;
     const table& sv_table_;
-    std::int64_t _n_size;
     detail::kernel_function_ptr kernel_;
-    sycl::queue queue_;
 };
 
 template <typename Float>
-class predict_task_dense : public predict_task<Float>
-{
+class predict_task_dense : public predict_task<Float> {
 public:
     virtual ~predict_task_dense() = default;
 
-    predict_task_dense(sycl::queue& q, const std::int64_t max_row_per_block, const table& x_table, const table & sv_table,
-                     const detail::kernel_function_ptr & kernel)
-        : predict_task<Float>(q, max_row_per_block, x_table, sv_table, kernel)
-    {}
+    predict_task_dense(const std::int64_t max_row_per_block,
+                       const table& x_table,
+                       const table& sv_table,
+                       const detail::kernel_function_ptr& kernel)
+            : predict_task<Float>(max_row_per_block, x_table, sv_table, kernel) {}
 
-    table get_block_nt_data(const std::int64_t start_row, const std::int64_t n_rows) override
-    {
-        auto data_nd = pr::table2ndarray<Float>(this->queue_, this->x_table_);
+    pr::ndarray<Float, 2> kernel_compute(sycl::queue& q,
+                                         const std::int64_t start_row,
+                                         const std::int64_t n_rows) {
+        auto data_nd = pr::table2ndarray<Float>(q, this->x_table_);
         auto data = data_nd.get_data() + data_nd.get_dimension(1) * start_row;
 
-        table x_block_nt = homogen_table::wrap(this->queue_, data, n_rows, data_nd.get_dimension(1));
-        return x_block_nt;
-    }
+        table x_block_nt = homogen_table::wrap(q, data, n_rows, data_nd.get_dimension(1));
 
+        const auto result =
+            this->kernel_->compute_kernel_function(dal::detail::data_parallel_policy(q),
+                                                   x_block_nt,
+                                                   this->sv_table_);
+
+        return pr::table2ndarray<Float>(q, result, sycl::usm::alloc::device);
+    }
 };
 }
 #endif
