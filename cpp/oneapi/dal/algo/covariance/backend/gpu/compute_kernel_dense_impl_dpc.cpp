ff06c1ec4bf0acb6a0eca89fe874b8c349943a61
Alexandr-Solovev <aleksandr.solovev@intel.com>
2021-11-16 23:10:44 -0800

    add gemm tasks

--- cpp/oneapi/dal/algo/covariance/backend/gpu/compute_kernel_dense_impl_dpc.cpp
+++ cpp/oneapi/dal/algo/covariance/backend/gpu/compute_kernel_dense_impl_dpc.cpp
@@ -815,10 +815,11 @@ result_t compute_kernel_dense_impl<Float, List>::operator()(const descriptor_t& 
     last_event.wait_and_throw();
     auto xtx =
         pr::ndarray<Float, 2>::empty(q_, { column_count, column_count }, sycl::usm::alloc::device);
-
-    auto gemm_event = gemm(q_, data_nd.t(), data_nd, xtx, Float(1), Float(0));
-    gemm_event.wait_and_throw();
-
+    {
+        ONEDAL_PROFILER_TASK(gemm, q);
+        auto gemm_event = gemm(q_, data_nd.t(), data_nd, xtx, Float(1), Float(0));
+        gemm_event.wait_and_throw();
+    }
     comm_.allreduce(xtx.flatten(q_, { last_event }), spmd::reduce_op::sum).wait();
 
     comm_.allreduce(rows_count_global, spmd::reduce_op::sum).wait();
