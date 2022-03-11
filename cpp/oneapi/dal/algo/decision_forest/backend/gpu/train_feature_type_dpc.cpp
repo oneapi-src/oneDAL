b5fd204a1ef2c9519b39ff44839e527f8addf12c
Nikita Kulikov <nikita.kulikov@intel.com>
2022-03-10 23:53:23 -0800

    Initial

--- cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_feature_type_dpc.cpp
+++ cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_feature_type_dpc.cpp
@@ -130,16 +130,16 @@ sycl::event indexed_features<Float, Bin, Index>::fill_bin_map(
         cgh.parallel_for(nd_range, [=](sycl::nd_item<1> item) {
             auto sbg = item.get_sub_group();
             const std::uint32_t n_groups = item.get_group_range(0);
-            const std::uint32_t n_sub_groups = sbg.get_group_range()[0];
+            const std::uint32_t n_sub_groups = sbg.get_group_range(0);
             const std::uint32_t n_total_sub_groups = n_sub_groups * n_groups;
             const Index elems_for_sbg =
                 row_count / n_total_sub_groups + bool(row_count % n_total_sub_groups);
-            const std::uint32_t local_size = sbg.get_local_range()[0];
+            const std::uint32_t local_size = sbg.get_local_range(0);
 
             const std::uint32_t local_id = sbg.get_local_id();
             const std::uint32_t sub_group_id = sbg.get_group_id();
             const std::uint32_t group_id =
-                item.get_group().get_group_id(0) * n_sub_groups + sub_group_id;
+                item.get_group(0) * n_sub_groups + sub_group_id;
 
             Index ind_start = group_id * elems_for_sbg;
             Index ind_end =
