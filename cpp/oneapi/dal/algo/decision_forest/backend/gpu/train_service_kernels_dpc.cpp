5e906347e11047e374592ed1dd2483965bd7c628
KulikovNikita <nikita.kulikov@intel.com>
2022-03-11 12:30:02 +0300

    Update cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_service_kernels_dpc.cpp

--- cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_service_kernels_dpc.cpp
+++ cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_service_kernels_dpc.cpp
@@ -572,7 +572,7 @@ sycl::event train_service_kernels<Float, Bin, Index, Task>::count_absent_rows_fo
             const Index elems_for_sbg =
                 block_row_count / n_total_sub_groups + bool(block_row_count % n_total_sub_groups);
 
-            const Index local_size = sbg.get_local_range(0);
+            const Index local_size = sbg.get_local_range()[0];
 
             const Index local_id = sbg.get_local_id();
             const Index sub_group_id = sbg.get_group_id();
