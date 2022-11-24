810e9c6c26de9b53b8f8da634cb6746b34081844
Nikita Kulikov <nikita.kulikov@intel.com>
2022-11-24 09:11:32 -0800

    Clang-format samples in respect to the PR #2160

--- samples/oneapi/dpc/ccl/sources/linear_regression_distr_ccl.cpp
+++ samples/oneapi/dpc/ccl/sources/linear_regression_distr_ccl.cpp
@@ -32,26 +32,20 @@ namespace dal = oneapi::dal;
 namespace lr = dal::linear_regression;
 
 void run(sycl::queue &queue) {
-    const auto train_data_file_name =
-        get_data_path("linear_regression_train_data.csv");
-    const auto train_response_file_name =
-        get_data_path("linear_regression_train_responses.csv");
+    const auto train_data_file_name = get_data_path("linear_regression_train_data.csv");
+    const auto train_response_file_name = get_data_path("linear_regression_train_responses.csv");
     const auto test_data_file_name = get_data_path("linear_regression_test_data.csv");
-    const auto test_response_file_name =
-        get_data_path("linear_regression_test_responses.csv");
+    const auto test_response_file_name = get_data_path("linear_regression_test_responses.csv");
 
     const auto x_train =
-        dal::read<dal::table>(queue, dal::csv::data_source{train_data_file_name});
-    const auto y_train = dal::read<dal::table>(
-        queue, dal::csv::data_source{train_response_file_name});
-    const auto x_test =
-        dal::read<dal::table>(queue, dal::csv::data_source{test_data_file_name});
-    const auto y_test = dal::read<dal::table>(
-        queue, dal::csv::data_source{test_response_file_name});
+        dal::read<dal::table>(queue, dal::csv::data_source{ train_data_file_name });
+    const auto y_train =
+        dal::read<dal::table>(queue, dal::csv::data_source{ train_response_file_name });
+    const auto x_test = dal::read<dal::table>(queue, dal::csv::data_source{ test_data_file_name });
+    const auto y_test =
+        dal::read<dal::table>(queue, dal::csv::data_source{ test_response_file_name });
 
-    auto comm =
-        dal::preview::spmd::make_communicator<dal::preview::spmd::backend::ccl>(
-            queue);
+    auto comm = dal::preview::spmd::make_communicator<dal::preview::spmd::backend::ccl>(queue);
     auto rank_id = comm.get_rank();
     auto rank_count = comm.get_rank_count();
 
@@ -62,15 +56,14 @@ void run(sycl::queue &queue) {
 
     const auto lr_desc = lr::descriptor<float>{};
 
-    const auto result_train = dal::preview::train(
-        comm, lr_desc, x_train_vec.at(rank_id), y_train_vec.at(rank_id));
+    const auto result_train =
+        dal::preview::train(comm, lr_desc, x_train_vec.at(rank_id), y_train_vec.at(rank_id));
 
-    const auto result_infer = dal::preview::infer(
-        comm, lr_desc, x_test_vec.at(rank_id), result_train.get_model());
+    const auto result_infer =
+        dal::preview::infer(comm, lr_desc, x_test_vec.at(rank_id), result_train.get_model());
 
     if (comm.get_rank() == 0) {
-        std::cout << "Prediction results:\n"
-                  << result_infer.get_responses() << std::endl;
+        std::cout << "Prediction results:\n" << result_infer.get_responses() << std::endl;
 
         std::cout << "Ground truth:\n" << y_test_vec.at(rank_id) << std::endl;
     }
@@ -79,18 +72,18 @@ void run(sycl::queue &queue) {
 int main(int argc, char const *argv[]) {
     int status = MPI_Init(nullptr, nullptr);
     if (status != MPI_SUCCESS) {
-        throw std::runtime_error{"Problem occurred during MPI init"};
+        throw std::runtime_error{ "Problem occurred during MPI init" };
     }
 
     auto device = sycl::device(sycl::gpu_selector_v);
-    std::cout << "Running on " << device.get_platform().get_info<sycl::info::platform::name>() << ", " << device.get_info<sycl::info::device::name>()
-              << std::endl;
-    sycl::queue q{device};
+    std::cout << "Running on " << device.get_platform().get_info<sycl::info::platform::name>()
+              << ", " << device.get_info<sycl::info::device::name>() << std::endl;
+    sycl::queue q{ device };
     run(q);
 
     status = MPI_Finalize();
     if (status != MPI_SUCCESS) {
-        throw std::runtime_error{"Problem occurred during MPI finalize"};
+        throw std::runtime_error{ "Problem occurred during MPI finalize" };
     }
     return 0;
 }
