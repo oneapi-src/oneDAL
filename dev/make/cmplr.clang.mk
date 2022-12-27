e15d179adaf742cae0a15cae2fa0821f89290313
Nikolay Petrov <nikolay.a.petrov@intel.com>
2022-12-27 21:57:09 +0000

    fixing clang compielr build opts for macOS

--- dev/make/cmplr.clang.mk
+++ dev/make/cmplr.clang.mk
@@ -49,7 +49,7 @@ pedantic.opts.mac.clang = $(pedantic.opts.clang)
 pedantic.opts.lnx.clang = $(pedantic.opts.clang)
 
 
-p4_OPT.icc   = $(-Q)$(if $(OS_is_mac),march=haswell,march=nocona)
-mc3_OPT.icc  = $(-Q)$(if $(OS_is_mac),march=haswell,march=nehalem)
+p4_OPT.clang   = $(-Q)march=nocona
+mc3_OPT.clang  = $(-Q)march=nehalem
 avx2_OPT.clang = $(-Q)march=haswell
 skx_OPT.clang  = $(-Q)march=skx
