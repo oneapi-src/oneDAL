f5e62c1c895f80619758efa95e432526d7e077bc
Nikolay Petrov <nikolay.a.petrov@intel.com>
2022-12-30 18:03:36 +0000

    Setting TBB manually

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -149,7 +149,7 @@ for link_mode in ${link_modes}; do
         mkdir Build
     fi
 
-    cmake -B./Build -S. -G "Unix Makefiles" -DTARGET_LINK=${link_mode}
+    cmake -B./Build -S. -G "Unix Makefiles" -DTARGET_LINK=${link_mode} -DSET_TBB_MANUALLY=YES
     err=$?
     if [ ${err} -ne 0 ]; then
         echo -e "$(date +'%H:%M:%S') CMAKE GENERATE FAILED\t\t"
