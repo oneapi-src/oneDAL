953df411bad94f5df1c54c23eb6ba1aad5bb77b7
icfaust <icfaust@gmail.com>
2023-01-13 02:47:44 -0800

    Change namespace for DAAL internal consistency

--- cpp/daal/include/data_management/data/internal/numeric_table_sycl_homogen.h
+++ cpp/daal/include/data_management/data/internal/numeric_table_sycl_homogen.h
@@ -72,7 +72,7 @@ public:
 
 #ifdef DAAL_SYCL_INTERFACE_USM
     static services::SharedPtr<SyclHomogenNumericTable<DataType> > create(const services::SharedPtr<DataType> & usmData, size_t nColumns,
-                                                                          size_t nRows, const sycl::queue & queue, services::Status * stat = NULL)
+                                                                          size_t nRows, const ::sycl::queue & queue, services::Status * stat = NULL)
     {
         const size_t bufferSize = nColumns * nRows;
 
@@ -91,7 +91,7 @@ public:
 
 #ifdef DAAL_SYCL_INTERFACE_USM
     static services::SharedPtr<SyclHomogenNumericTable<DataType> > create(DataType * usmData, size_t nColumns, size_t nRows,
-                                                                          const sycl::queue & queue, services::Status * stat = NULL)
+                                                                          const ::sycl::queue & queue, services::Status * stat = NULL)
     {
         const auto overflow_status = checkSizeOverflow(nRows, nColumns);
         if (!overflow_status)
