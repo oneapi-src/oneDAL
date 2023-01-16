4a2e9c894596c427bcdfdb70be389e3826b22a0f
Anatoly Volkov <anatoly.volkov@intel.com>
2023-01-16 01:55:06 -0800

    Add 4 hessian computation implementations and perfomance tests

--- cpp/oneapi/dal/backend/primitives/objective_function/logloss.hpp
+++ cpp/oneapi/dal/backend/primitives/objective_function/logloss.hpp
@@ -21,7 +21,7 @@
 namespace oneapi::dal::backend::primitives {
 
 template <typename Float>
-sycl::event compute_predictions(sycl::queue& q,
+sycl::event compute_probabilities(sycl::queue& q,
                                 const ndview<Float, 1>& parameters,
                                 const ndview<Float, 2>& data,
                                 ndview<Float, 1>& predictions,
@@ -48,6 +48,17 @@ sycl::event compute_logloss_with_der(sycl::queue& q,
                                      Float L1 = Float(0),
                                      Float L2 = Float(0),
                                      const event_vector& deps = {});
+
+template <typename Float>
+sycl::event compute_derivative(sycl::queue& q,
+                                const ndview<Float, 1>& parameters,
+                                const ndview<Float, 2>& data,
+                                const ndview<std::int32_t, 1>& labels,
+                                const ndview<Float, 1>& probabilities,
+                                ndview<Float, 1>& out_derivative,
+                                Float L1 = Float(0),
+                                Float L2 = Float(0),
+                                const event_vector& deps = {});
 
 template <typename Float>
 sycl::event compute_hessian(sycl::queue& q,
