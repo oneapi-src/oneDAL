d6aedbcaabc233f84e3b4262deba165d77292d2b
Kruglov, Oleg <oleg.kruglov@intel.com>
2023-03-01 06:13:02 -0800

    Add wait_and_throw() to gemm_event to prevent invalid memory access

--- cpp/oneapi/dal/algo/rbf_kernel/backend/gpu/compute_kernel_dense_dpc.cpp
+++ cpp/oneapi/dal/algo/rbf_kernel/backend/gpu/compute_kernel_dense_dpc.cpp
@@ -110,6 +110,7 @@ void compute_rbf(sycl::queue& queue,
     {
         ONEDAL_PROFILER_TASK(rbf_kernel.gemm, queue);
         gemm_event = pr::gemm(queue, x_nd, y_nd.t(), res_nd, alpha, beta, { reduce_y_event });
+        gemm_event.wait_and_throw();
     }
 
     compute_exponents(queue, sqr_x_nd, sqr_y_nd, res_nd, sigma, { gemm_event });
