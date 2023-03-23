70d3b8c76c6c1cb87720eccca3a20335ba7d99cd
maria-Petrova <maria.petrova@intel.com>
2023-03-23 06:51:38 -0700

    Change add delimiter line

--- deploy/local/vars_lnx.sh
+++ deploy/local/vars_lnx.sh
@@ -44,10 +44,13 @@
 #   /script/absolute/pathname
 
 #check if ONEAPI_ROOT is defined
-if ![ -z "${ONEAPI_ROOT:-}" ] ; then
+if [ -n "${ONEAPI_ROOT:-}" ] ; then
   export DAALROOT=$ONEAPI_ROOT
   export CLASSPATH=$ONEAPI_ROOT/share/java/onedal.jar${CLASSPATH+:${CLASSPATH}}
+  exit 0
 fi
+
+echo "Test of the first line"
 
 # executing function in a *subshell* to localize vars and effects on `cd`
 get_script_path() (
