657c8784e052dcb0ec2ad9d390cef3060333b399
maria-Petrova <maria.petrova@intel.com>
2023-03-23 06:53:24 -0700

    Change if logic

--- deploy/local/vars_lnx.sh
+++ deploy/local/vars_lnx.sh
@@ -44,13 +44,11 @@
 #   /script/absolute/pathname
 
 #check if ONEAPI_ROOT is defined
-if [ -n "${ONEAPI_ROOT:-}" ] ; then
+if ![ -z "${ONEAPI_ROOT:-}" ] ; then
   export DAALROOT=$ONEAPI_ROOT
   export CLASSPATH=$ONEAPI_ROOT/share/java/onedal.jar${CLASSPATH+:${CLASSPATH}}
   exit 0
 fi
-
-echo "Test of the first line"
 
 # executing function in a *subshell* to localize vars and effects on `cd`
 get_script_path() (
