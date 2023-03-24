cc2620376ebc2a8df5721ffac84867a0dfca8d21
maria-Petrova <maria.petrova@intel.com>
2023-03-24 05:16:50 -0700

    Win fix

--- deploy/local/vars_win.bat
+++ deploy/local/vars_win.bat
@@ -17,7 +17,7 @@ rem ============================================================================
 
 setlocal
 rem check if ONEAPI_ROOT is defined
-if not "%ONEAPI_ROOT%"=="" (
+if not "!ONEAPI_ROOT!"=="" (
     set "DAALROOT=%ONEAPI_ROOT%"
     set "CLASSPATH=%ONEAPI_ROOT%\share\java\onedal.jar;%CLASSPATH%"
     exit /b 0
