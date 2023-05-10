c06ae3dd85b76828b925e699372c7ccb50582183
KulikovNikita <nikita.kulikov@intel.com>
2023-05-10 15:45:30 +0100

    Update cpp/oneapi/dal/backend/primitives/placement/cumulative_sum.hpp
    Co-authored-by: Alexandra <alexandra.epanchinzeva@intel.com>

--- cpp/oneapi/dal/backend/primitives/placement/cumulative_sum.hpp
+++ cpp/oneapi/dal/backend/primitives/placement/cumulative_sum.hpp
@@ -65,7 +65,7 @@ namespace detail {
 /// @brief Computes partial cumulative sums inplace
 ///        on the `data` array assuming elements have
 ///        `curr_stride` stribe between them and size
-///        of each block equal to `base_stride`.
+///        of each block is equal to `base_stride`.
 /// @note  Usually, `base_stride== wg`.
 /// @note  Always `base_stride <= curr_stride`
 template <typename Type>
