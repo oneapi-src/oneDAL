5779c002ad6471895553bf24fa1eb4d0ef9f1ade
Ian Faust <icfaust@gmail.com>
2023-05-31 00:27:16 +0200

    Switch from seed to engine in train_kernel_hist_impl_dpc.cpp

--- cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_kernel_hist_impl_dpc.cpp
+++ cpp/oneapi/dal/algo/decision_forest/backend/gpu/train_kernel_hist_impl_dpc.cpp
@@ -2889,7 +2889,7 @@ train_result<Task> train_kernel_hist_impl<Float, Bin, Index, Task>::operator()(
 
     de::check_mul_overflow<std::size_t>((ctx.tree_count_ - 1), skip_num);
 
-    pr::engine_collection collection(ctx.tree_count_, desc.get_seed());
+    pr::engine_collection collection(ctx.tree_count_, desc.get_engine());
     rng_engine_list_t engine_arr = collection([&](std::size_t i, std::size_t& skip) {
         skip = i * skip_num;
     });
