3569055ff6bbf10adbc568f088ddc1d04ad6f915
Kruglov, Oleg <oleg.kruglov@intel.com>
2023-06-15 04:29:58 -0700

    Add create_task_arena method

---
+++ cpp/oneapi/dal/backend/threading.hpp
@@ -0,0 +1,51 @@
+/*******************************************************************************
+* Copyright 2023 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#pragma once
+
+#include "tbb/tbb.h"
+#include <tbb/task_arena.h>
+#include <tbb/task_scheduler_observer.h>
+
+struct threading_policy {
+    const int max_threads_per_core;
+    const bool thread_pinning;
+
+public:
+    threading_policy():
+        max_threads_per_core(0),
+        thread_pinning(false) {}
+    
+    threading_policy(const int &max_threads_per_core_):
+        max_threads_per_core(max_threads_per_core_),
+        thread_pinning(false) {}
+
+    threading_policy(const int &max_threads_per_core_, const bool &thread_pinning_):
+        max_threads_per_core(max_threads_per_core_),
+        thread_pinning(thread_pinning_) {}
+};
+
+inline tbb::task_arena create_task_arena(const threading_policy& policy) {
+
+    if (policy.max_threads_per_core) {
+        tbb::task_arena task_arena{tbb::task_arena::constraints{}.set_max_threads_per_core(policy.max_threads_per_core)};
+        return task_arena;
+    }
+    else {
+        tbb::task_arena task_arena{};
+        return task_arena;
+    }
+}
