f77edf25af7db6b75b2409b598d3156c5cc80afc
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2023-06-26 14:10:03 +0200

    fix typo in cpp/oneapi/dal/table/test/csr_accessor.cpp
    Co-authored-by: Alexandra <alexandra.epanchinzeva@intel.com>

--- cpp/oneapi/dal/table/test/csr_accessor.cpp
+++ cpp/oneapi/dal/table/test/csr_accessor.cpp
@@ -78,7 +78,7 @@ public:
     ///                         to be pulled from the table.
     /// @param[in] end_idx      Either zero-based index of the row that goes after the last row
     ///                         of the block of data to be pulled from the table;
-    ///                         or -1, if the last row to be pulled is equal to the last row
+    ///                         or -1 if the last row to be pulled is equal to the last row
     ///                         of the whole table.
     void pull_checks(std::int64_t start_idx, std::int64_t end_idx) {
         ONEDAL_ASSERT(0 <= start_idx && start_idx <= row_count);
