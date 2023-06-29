7689988a563d179983bf0fe56b1769a705a4ae5b
Alexandr-Solovev <aleksandr.solovev@intel.com>
2023-06-29 04:04:17 -0700

    move eigen on dpcpp

--- cpp/oneapi/dal/backend/primitives/lapack/eigen_cpu.cpp
+++ cpp/oneapi/dal/backend/primitives/lapack/eigen_cpu.cpp
@@ -1,94 +1,88 @@
-/*******************************************************************************
-* Copyright 2021 Intel Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+// /*******************************************************************************
+// * Copyright 2021 Intel Corporation
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// *******************************************************************************/
 
-#include "oneapi/dal/backend/dispatcher.hpp"
-#include "oneapi/dal/backend/primitives/lapack/eigen.hpp"
+// #include "oneapi/dal/backend/dispatcher.hpp"
+// #include "oneapi/dal/backend/primitives/lapack/eigen.hpp"
 
-namespace oneapi::dal::backend::primitives {
+// namespace oneapi::dal::backend::primitives {
 
-template <typename Cpu, typename Float>
-void flip_eigvals_impl_cpu(Float* a,
-                           Float* w,
-                           std::int64_t n,
-                           std::int64_t lda,
-                           std::int64_t w_count,
-                           Float* a_flipped,
-                           std::int64_t lda_flipped,
-                           Float* w_flipped) {
-    ONEDAL_ASSERT(a);
-    ONEDAL_ASSERT(w);
-    ONEDAL_ASSERT(a_flipped);
-    ONEDAL_ASSERT(w_flipped);
-    ONEDAL_ASSERT(n > 0);
-    ONEDAL_ASSERT(lda >= n);
-    ONEDAL_ASSERT(w_count > 0);
-    ONEDAL_ASSERT(w_count <= n);
+// sycl::event flip_eigvals_impl_gpu(sycl::queue& q,
+//                                       float* a,
+//                                       float* w,
+//                                       std::int64_t n,
+//                                       std::int64_t lda,
+//                                       std::int64_t w_count,
+//                                       float* a_flipped,
+//                                       std::int64_t lda_flipped,
+//                                       float* w_flipped,
+//                                       const event_vector& dep) {
+//     ONEDAL_ASSERT(a);
+//     ONEDAL_ASSERT(w);
+//     ONEDAL_ASSERT(a_flipped);
+//     ONEDAL_ASSERT(w_flipped);
+//     ONEDAL_ASSERT(n > 0);
+//     ONEDAL_ASSERT(lda >= n);
+//     ONEDAL_ASSERT(w_count > 0);
+//     ONEDAL_ASSERT(w_count <= n);
 
-    if (a == a_flipped) {
-        ONEDAL_ASSERT(lda == lda_flipped);
+//     return q.submit([&](cl::sycl::handler& cgh) {
+//         auto a_accessor = a;
+//         auto w_accessor = w;
+//         auto a_flipped_accessor = a_flipped;
+//         auto w_flipped_accessor = w_flipped;
 
-        for (std::int64_t i = 0; i < n / 2; i++) {
-            const std::int64_t src_i = i;
-            const std::int64_t dst_i = n - i - 1;
-            for (std::int64_t j = 0; j < n; j++) {
-                std::swap(a[src_i * lda + j], a[dst_i * lda + j]);
-            }
-        }
-    }
-    else {
-        PRAGMA_IVDEP
-        for (std::int64_t i = 0; i < w_count; i++) {
-            const std::int64_t src_i = n - i - 1;
-            const std::int64_t dst_i = i;
-            for (std::int64_t j = 0; j < n; j++) {
-                a_flipped[dst_i * lda_flipped + j] = a[src_i * lda + j];
-            }
-        }
-    }
+//         cgh.depends_on(deps);
 
-    if (w == w_flipped) {
-        ONEDAL_ASSERT(n == w_count);
+//         cgh.parallel_for(cl::sycl::range<1>(n / 2), [=](cl::sycl::id<1> idx) {
+//             const std::int64_t i = idx[0];
+//             const std::int64_t src_i = n - i - 1;
+//             const std::int64_t dst_i = i;
 
-        for (std::int64_t i = 0; i < n / 2; i++) {
-            const std::int64_t src_i = i;
-            const std::int64_t dst_i = n - i - 1;
-            std::swap(w[src_i], w[dst_i]);
-        }
-    }
-    else {
-        PRAGMA_IVDEP
-        for (std::int64_t i = 0; i < w_count; i++) {
-            const std::int64_t src_i = n - i - 1;
-            const std::int64_t dst_i = i;
-            w_flipped[dst_i] = w[src_i];
-        }
-    }
-}
+//             if (a == a_flipped) {
+//                 for (std::int64_t j = 0; j < n; j++) {
+//                     std::swap(a_accessor[src_i * lda + j], a_accessor[dst_i * lda + j]);
+//                 }
+//             } else {
+//                 for (std::int64_t j = 0; j < n; j++) {
+//                     a_flipped_accessor[dst_i * lda_flipped + j] = a_accessor[src_i * lda + j];
+//                 }
+//             }
 
-#define INSTANTIATE(Cpu, Float)                                   \
-    template void flip_eigvals_impl_cpu<Cpu, Float>(Float*,       \
-                                                    Float*,       \
-                                                    std::int64_t, \
-                                                    std::int64_t, \
-                                                    std::int64_t, \
-                                                    Float*,       \
-                                                    std::int64_t, \
-                                                    Float*);
+//             if (w == w_flipped) {
+//                 std::swap(w_accessor[src_i], w_accessor[dst_i]);
+//             } else {
+//                 w_flipped_accessor[dst_i] = w_accessor[src_i];
+//             }
+//         });
+//     });
+// }
 
-INSTANTIATE(__CPU_TAG__, float)
-INSTANTIATE(__CPU_TAG__, double)
+// #define INSTANTIATE(Float)                                   \
+//     template sycl::event flip_eigvals_impl_gpu<Float>(
+//         sycl::queue&, \
+//         Float*,       \
+//                                                     Float*,       \
+//                                                     std::int64_t, \
+//                                                     std::int64_t, \
+//                                                     std::int64_t, \
+//                                                     Float*,       \
+//                                                     std::int64_t, \
+//                                                     Float*);
 
-} // namespace oneapi::dal::backend::primitives
+// INSTANTIATE(float)
+// INSTANTIATE(double)
+
+// } // namespace oneapi::dal::backend::primitives
