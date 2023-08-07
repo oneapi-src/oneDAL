4a5917aff67e8cdda770aa328f30f1d8cbfc20eb
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2023-08-07 17:55:29 +0200

    Add USM support into dal::csr_accessor class (#2434)
    The list of changes:
    * dal::csr_accessor's API extended to accept and return data allocated in USM
    * DPC++ kernels updated in dal::csr_accessor to handle USM-allocated data properly
    * csr_accessor_dpc test extended to test the cases when csr_table's and/or pulled arrays data is allocated in USM. The case when the table's data is allocated in USM (device or shared) memory and the accessor wants to pull it into host memory (USM or C++-allocated) is intentionally left uncovered. Because this functionality is not implemented yet. A separate PR will be created to cover that case.

--- cpp/oneapi/dal/table/backend/empty_table_impl.hpp
+++ cpp/oneapi/dal/table/backend/empty_table_impl.hpp
@@ -96,6 +96,17 @@ public:
                                  const sparse_indexing& indexing,
                                  const range& row_range) const {}
 
+#ifdef ONEDAL_DATA_PARALLEL
+    template <typename T>
+    void pull_csr_block_template(const detail::data_parallel_policy& policy,
+                                 dal::array<T>& data,
+                                 dal::array<std::int64_t>& column_indices,
+                                 dal::array<std::int64_t>& row_indices,
+                                 const sparse_indexing& indexing,
+                                 const range& row_range,
+                                 sycl::usm::alloc alloc) const {}
+#endif
+
     void serialize(detail::output_archive& ar) const override {
         // Nothing to serialize
     }
--- cpp/oneapi/dal/table/detail/csr_access_iface.hpp
+++ cpp/oneapi/dal/table/detail/csr_access_iface.hpp
@@ -38,7 +38,17 @@ namespace v1 {
                         const dal::sparse_indexing& indexing,     \
                         const range& row_range)
 
+#define PULL_CSR_BLOCK_SIGNATURE_DPC(T)                           \
+    void pull_csr_block(const data_parallel_policy& policy,       \
+                        dal::array<T>& data,                      \
+                        dal::array<std::int64_t>& column_indices, \
+                        dal::array<std::int64_t>& row_offsets,    \
+                        const dal::sparse_indexing& indexing,     \
+                        const range& row_range,                   \
+                        sycl::usm::alloc alloc)
+
 #define DECLARE_PULL_CSR_BLOCK_HOST(T) virtual PULL_CSR_BLOCK_SIGNATURE_HOST(T) = 0;
+#define DECLARE_PULL_CSR_BLOCK_DPC(T)  virtual PULL_CSR_BLOCK_SIGNATURE_DPC(T) = 0;
 
 #define DEFINE_TEMPLATE_PULL_CSR_BLOCK_HOST(Derived, T)                      \
     PULL_CSR_BLOCK_SIGNATURE_HOST(T) override {                              \
@@ -50,6 +60,17 @@ namespace v1 {
                                                              row_range);     \
     }
 
+#define DEFINE_TEMPLATE_PULL_CSR_BLOCK_DPC(Derived, T)                       \
+    PULL_CSR_BLOCK_SIGNATURE_DPC(T) override {                               \
+        static_cast<Derived*>(this)->pull_csr_block_template(policy,         \
+                                                             data,           \
+                                                             column_indices, \
+                                                             row_offsets,    \
+                                                             indexing,       \
+                                                             row_range,      \
+                                                             alloc);         \
+    }
+
 class pull_csr_block_iface {
 public:
     virtual ~pull_csr_block_iface() = default;
@@ -57,6 +78,12 @@ public:
     DECLARE_PULL_CSR_BLOCK_HOST(float)
     DECLARE_PULL_CSR_BLOCK_HOST(double)
     DECLARE_PULL_CSR_BLOCK_HOST(std::int32_t)
+
+#ifdef ONEDAL_DATA_PARALLEL
+    DECLARE_PULL_CSR_BLOCK_DPC(float)
+    DECLARE_PULL_CSR_BLOCK_DPC(double)
+    DECLARE_PULL_CSR_BLOCK_DPC(std::int32_t)
+#endif
 };
 
 template <typename Derived>
@@ -65,11 +92,20 @@ public:
     DEFINE_TEMPLATE_PULL_CSR_BLOCK_HOST(Derived, float)
     DEFINE_TEMPLATE_PULL_CSR_BLOCK_HOST(Derived, double)
     DEFINE_TEMPLATE_PULL_CSR_BLOCK_HOST(Derived, std::int32_t)
+
+#ifdef ONEDAL_DATA_PARALLEL
+    DEFINE_TEMPLATE_PULL_CSR_BLOCK_DPC(Derived, float)
+    DEFINE_TEMPLATE_PULL_CSR_BLOCK_DPC(Derived, double)
+    DEFINE_TEMPLATE_PULL_CSR_BLOCK_DPC(Derived, std::int32_t)
+#endif
 };
 
 #undef PULL_CSR_BLOCK_SIGNATURE_HOST
 #undef DECLARE_PULL_CSR_BLOCK_HOST
 #undef DEFINE_TEMPLATE_PULL_CSR_BLOCK_HOST
+#undef PULL_CSR_BLOCK_SIGNATURE_DPC
+#undef DECLARE_PULL_CSR_BLOCK_DPC
+#undef DEFINE_TEMPLATE_PULL_CSR_BLOCK_DPC
 
 } // namespace v1
 
