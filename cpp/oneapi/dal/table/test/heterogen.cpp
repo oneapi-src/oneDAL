f00f3139a7cfd1b21271285902552e2a50a3a7d2
Nikita Kulikov <nikita.kulikov@intel.com>
2023-08-17 07:24:52 -0700

    Minor

--- cpp/oneapi/dal/table/test/heterogen.cpp
+++ cpp/oneapi/dal/table/test/heterogen.cpp
@@ -33,7 +33,7 @@ TEST("can construct empty table") {
 TEST("can construct rowmajor table 3x2") {
     using oneapi::dal::detail::empty_delete;
 
-    float data[] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
+    constexpr float data[] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
 
     homogen_table t{ data, 3, 2, empty_delete<const float>() };
 
