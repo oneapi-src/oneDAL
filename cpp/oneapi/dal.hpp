55b40e4300dba32008b4f4c6296faf74b1d3303c
Aleksandr Solovev <aleksandr.solovev@intel.com>
2023-08-23 13:42:48 +0200

    Init addition of online interfaces in Covariance (#2455)

--- cpp/oneapi/dal.hpp
+++ cpp/oneapi/dal.hpp
@@ -24,6 +24,8 @@
 #include "oneapi/dal/infer.hpp"
 #include "oneapi/dal/read.hpp"
 #include "oneapi/dal/train.hpp"
+#include "oneapi/dal/partial_compute.hpp"
+#include "oneapi/dal/finalize_compute.hpp"
 
 /* Tables */
 #include "oneapi/dal/table/common.hpp"
