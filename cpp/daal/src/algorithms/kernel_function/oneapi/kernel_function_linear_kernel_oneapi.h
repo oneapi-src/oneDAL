764e206e2456431543a401112d95e09a984d7f52
Md Shafiul Alam <md.shafiul.alam@intel.com>
2023-09-12 10:35:04 -0700

    remove unreachable return statement

--- cpp/daal/src/algorithms/kernel_function/oneapi/kernel_function_linear_kernel_oneapi.h
+++ cpp/daal/src/algorithms/kernel_function/oneapi/kernel_function_linear_kernel_oneapi.h
@@ -87,7 +87,6 @@ public:
         case matrixMatrix: return computeInternalMatrixMatrix(ntLeft, ntRight, result, par);
         default: return services::ErrorIncorrectParameter;
         }
-        return services::Status();
     }
 
 protected:
