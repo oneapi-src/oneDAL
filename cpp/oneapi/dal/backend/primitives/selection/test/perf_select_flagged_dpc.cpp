d60120966bb019b1094ebf813879a8ca2571a233
Alexandr-Solovev <aleksandr.solovev@intel.com>
2023-09-24 00:54:30 -0700

    minor upd

--- cpp/oneapi/dal/backend/primitives/selection/test/perf_select_flagged_dpc.cpp
+++ cpp/oneapi/dal/backend/primitives/selection/test/perf_select_flagged_dpc.cpp
@@ -28,7 +28,7 @@ namespace la = te::linalg;
 namespace de = dal::detail;
 
 template <typename TestType>
-class select_flagged_test : public te::policy_fixture {
+class select_flagged_test : public te::float_algo_fixture<TestType> {
 public:
     using Float = std::tuple_element_t<0, TestType>;
     using Flag = std::tuple_element_t<1, TestType>;
