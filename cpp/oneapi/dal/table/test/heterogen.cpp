0a18249cc10b77504a50c666788fb3ffd7a7e885
Nikita Kulikov <nikita.kulikov@intel.com>
2023-10-05 02:07:57 -0700

    Debug deletion

--- cpp/oneapi/dal/table/test/heterogen.cpp
+++ cpp/oneapi/dal/table/test/heterogen.cpp
@@ -14,8 +14,6 @@
 * limitations under the License.
 *******************************************************************************/
 
-#include "oneapi/dal/detail/debug.hpp"
-
 #include "oneapi/dal/array.hpp"
 #include "oneapi/dal/chunked_array.hpp"
 
