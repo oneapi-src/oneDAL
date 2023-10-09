4b8214ba792bc5a04beb9996219f85670df233d1
Ovchinnikov Dmitriy <dmitriy.ovchinnikov@intel.com>
2023-10-09 06:01:45 -0700

    Add new files

---
+++ cpp/oneapi/dal/algo/basic_statistics/backend/gpu/compute_kernel_csr_impl.hpp
@@ -0,0 +1,47 @@
+/*******************************************************************************
+* Copyright 2023 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#pragma once
+
+#include "oneapi/dal/algo/basic_statistics/backend/gpu/compute_kernel.hpp"
+#include "oneapi/dal/backend/primitives/utils.hpp"
+#include "oneapi/dal/util/common.hpp"
+#include "oneapi/dal/detail/policy.hpp"
+#include "oneapi/dal/backend/communicator.hpp"
+
+#ifdef ONEDAL_DATA_PARALLEL
+
+namespace oneapi::dal::basic_statistics::backend {
+
+namespace de = dal::detail;
+namespace bk = dal::backend;
+namespace pr = dal::backend::primitives;
+
+template <typename Float>
+class compute_kernel_csr_impl {
+    using method_t = method::sparse;
+    using task_t = task::compute;
+    using comm_t = bk::communicator<spmd::device_memory_access::usm>;
+    using input_t = compute_input<task_t>;
+    using result_t = compute_result<task_t>;
+    using descriptor_t = detail::descriptor_base<task_t>;
+
+public:
+    result_t operator()(const bk::context_gpu& ctx, const descriptor_t& desc, const input_t& input);
+};
+
+} // namespace oneapi::dal::basic_statistics::backend
+#endif // ONEDAL_DATA_PARALLEL
