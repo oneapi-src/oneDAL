1a0d7fffec47e5332ce6585cb04e91cf97b032ce
Md Shafiul Alam <md.shafiul.alam@intel.com>
2023-10-12 21:39:43 -0700

    update pca infer

--- cpp/oneapi/dal/algo/pca/backend/gpu/infer_kernel_dpc.cpp
+++ cpp/oneapi/dal/algo/pca/backend/gpu/infer_kernel_dpc.cpp
@@ -22,19 +22,105 @@
 #include "oneapi/dal/backend/primitives/stat.hpp"
 #include "oneapi/dal/backend/primitives/utils.hpp"
 #include "oneapi/dal/backend/primitives/blas.hpp"
-#include "oneapi/dal/backend/primitives/element_wise.hpp"
-#include "oneapi/dal/backend/primitives/reduction.hpp"
-#include <iostream>
 
 namespace oneapi::dal::pca::backend {
 
 namespace pr = oneapi::dal::backend::primitives;
+namespace bk = oneapi::dal::backend;
 
 using dal::backend::context_gpu;
 using model_t = model<task::dim_reduction>;
 using input_t = infer_input<task::dim_reduction>;
 using result_t = infer_result<task::dim_reduction>;
 using descriptor_t = detail::descriptor_base<task::dim_reduction>;
+
+template <typename Float>
+inline auto make_mutable(sycl::queue& q, pr::ndarray<Float, 2> data)
+    -> std::tuple<sycl::event, pr::ndarray<Float, 2>> {
+    constexpr auto alloc = sycl::usm::alloc::device;
+
+    if (data.has_mutable_data()) {
+        sycl::event event{};
+        return std::make_tuple(std::move(event), std::move(data));
+    }
+    else {
+        auto place = pr::ndarray<Float, 2>::empty(q,
+                                                  { data.get_dimension(0), data.get_dimension(1) },
+                                                  alloc);
+        auto event = pr::copy(q, place, data, {});
+        return std::make_tuple(std::move(event), std::move(place));
+    }
+}
+
+template <typename Float>
+inline auto get_centered(sycl::queue& q, const table& data, const table& means)
+    -> std::tuple<sycl::event, pr::ndarray<Float, 2>> {
+    constexpr auto alloc = sycl::usm::alloc::device;
+
+    auto raw_data = pr::table2ndarray<Float>(q, data, alloc);
+
+    if (means.has_data()) {
+        auto [mut_event, mut_data] = make_mutable(q, std::move(raw_data));
+        auto raw_means = pr::table2ndarray_1d<Float>(q, means, alloc);
+        sycl::event new_mut_event = std::move(mut_event);
+        pr::ndarray<Float, 2> new_mut_data = std::move(mut_data);
+        auto event = q.submit([&](sycl::handler& h) {
+            h.depends_on({ new_mut_event });
+
+            const auto row_count = new_mut_data.get_dimension(0);
+            const auto col_count = new_mut_data.get_dimension(1);
+            const auto stride = new_mut_data.get_leading_stride();
+
+            const auto* const mean_ptr = raw_means.get_data();
+            auto* const data_ptr = new_mut_data.get_mutable_data();
+
+            auto range = bk::make_range_2d(row_count, col_count);
+            h.parallel_for(range, [=](sycl::id<2> id) -> void {
+                auto* const ptr = data_ptr + id[0] * stride + id[1];
+                (*ptr) -= *(mean_ptr + id[1]);
+            });
+        });
+
+        return std::make_tuple(std::move(event), std::move(new_mut_data));
+    }
+    else {
+        sycl::event event{};
+        return std::make_tuple(std::move(event), std::move(raw_data));
+    }
+}
+
+template <typename Float>
+inline auto get_whitened(sycl::queue& q,
+                         pr::ndarray<Float, 2>& mut_data,
+                         const table& eigenvalues,
+                         sycl::event deps) -> std::tuple<sycl::event, pr::ndarray<Float, 2>> {
+    constexpr auto alloc = sycl::usm::alloc::device;
+
+    if (eigenvalues.has_data()) {
+        auto raw_ev = pr::table2ndarray_1d<Float>(q, eigenvalues, alloc);
+        auto event = q.submit([&](sycl::handler& h) {
+            h.depends_on({ deps });
+            const auto row_count = mut_data.get_dimension(0);
+            const auto col_count = mut_data.get_dimension(1);
+            const auto stride = mut_data.get_leading_stride();
+
+            const auto* const ev_ptr = raw_ev.get_data();
+            auto* const data_ptr = mut_data.get_mutable_data();
+
+            auto range = bk::make_range_2d(row_count, col_count);
+            h.parallel_for(range, [=](sycl::id<2> id) -> void {
+                auto* const ptr = data_ptr + id[0] * stride + id[1];
+                (*ptr) /= sycl::sqrt(*(ev_ptr + id[1]));
+            });
+        });
+
+        return std::make_tuple(std::move(event), std::move(mut_data));
+    }
+    else {
+        sycl::event event{};
+        return std::make_tuple(std::move(event), std::move(mut_data));
+    }
+}
 
 template <typename Float>
 static result_t infer(const context_gpu& ctx, const descriptor_t& desc, const input_t& input) {
@@ -53,26 +139,10 @@ static result_t infer(const context_gpu& ctx, const descriptor_t& desc, const in
     ONEDAL_ASSERT(component_count > 0);
     dal::detail::check_mul_overflow(row_count, component_count);
 
-    const auto data_nd = pr::table2ndarray<Float>(queue, data, sycl::usm::alloc::device);
-    auto means_nd = pr::table2ndarray_1d<Float>(queue, means, sycl::usm::alloc::device);
-    if (!means.has_data()){
-        const auto [means_nd, cores_event] =
-        pr::ndarray<Float, 1>::full(queue, col_count, 0, sycl::usm::alloc::device);
-    }
-    auto mean_centered_data_nd =
-        pr::ndarray<Float, 2>::empty(queue, { row_count, col_count }, sycl::usm::alloc::device);
+    auto [mean_center_event, mean_centered_data_nd] = get_centered<Float>(queue, data, means);
+
     const auto eigenvectors_nd =
         pr::table2ndarray<Float>(queue, eigenvectors, sycl::usm::alloc::device);
-
-    const auto kernel_minus = [=](const Float a, const Float b) -> Float {
-        return a - b;
-    };
-    sycl::event mean_center_event;
-    {
-        auto mean_center_event =
-            pr::element_wise(queue, kernel_minus, data_nd, means_nd, mean_centered_data_nd, {});
-    }
-
     auto res_nd = pr::ndarray<Float, 2>::empty(queue,
                                                { row_count, component_count },
                                                sycl::usm::alloc::device);
@@ -85,41 +155,13 @@ static result_t infer(const context_gpu& ctx, const descriptor_t& desc, const in
                               res_nd,
                               Float(1.0),
                               Float(0.0),
-                              { });
+                              { mean_center_event });
         gemm_event.wait_and_throw();
     }
 
-    if (eigenvalues.has_data()) {
-        const auto eigenvalues_nd =
-            pr::table2ndarray_1d<Float>(queue, eigenvalues, sycl::usm::alloc::device);
-        auto sqrt_eigenvalues_nd =
-            pr::ndarray<Float, 1>::empty(queue, { component_count }, sycl::usm::alloc::device);
-        const auto kernel_sqrt = [=](const Float a) -> Float {
-            return sycl::sqrt(a);
-        };
-        sycl::event sqrt_event;
-        {
-            auto sqrt_event = pr::element_wise(queue, kernel_sqrt, eigenvalues_nd, sqrt_eigenvalues_nd, {gemm_event});
-        }
-
-        auto result_whitened_nd = pr::ndarray<Float, 2>::empty(queue,
-                                                               { row_count, component_count },
-                                                               sycl::usm::alloc::device);
-        const auto kernel_division = [=](const Float a, const Float b) -> Float {
-            return a/b;
-        };
-        sycl::event whiten_event;
-        {
-            auto whiten_event = pr::element_wise(queue, kernel_division, res_nd, sqrt_eigenvalues_nd, result_whitened_nd, {gemm_event});
-        }
-
-        const auto res_array_whitened = result_whitened_nd.flatten(queue, { whiten_event });
-        const auto res_table = homogen_table::wrap(res_array_whitened, row_count, component_count);
-
-        return result_t{}.set_transformed_data(res_table);
-    }
-
-    const auto res_array = res_nd.flatten(queue, { gemm_event });
+    auto [whiten_event, whitened_data_nd] =
+        get_whitened<Float>(queue, res_nd, eigenvalues, { gemm_event });
+    const auto res_array = whitened_data_nd.flatten(queue, { whiten_event });
     const auto res_table = homogen_table::wrap(res_array, row_count, component_count);
     return result_t{}.set_transformed_data(res_table);
 }
