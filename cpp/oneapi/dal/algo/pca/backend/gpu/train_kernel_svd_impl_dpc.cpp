b611c93d29b094817c7c436ab75ecf3f2d569eb8
Alexandr-Solovev <aleksandr.solovev@intel.com>
2023-10-16 02:24:04 -0700

    minor fix

--- cpp/oneapi/dal/algo/pca/backend/gpu/train_kernel_svd_impl_dpc.cpp
+++ cpp/oneapi/dal/algo/pca/backend/gpu/train_kernel_svd_impl_dpc.cpp
@@ -26,7 +26,6 @@
 #include "oneapi/dal/backend/primitives/blas.hpp"
 #include "oneapi/dal/algo/pca/backend/common.hpp"
 #include "oneapi/dal/algo/pca/backend/sign_flip.hpp"
-#include "oneapi/dal/backend/primitives/element_wise.hpp"
 
 namespace oneapi::dal::pca::backend {
 
