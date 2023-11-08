d80b91502df4eecb9edb1ed49213bd55e1a09b23
Alexandr-Solovev <aleksandr.solovev@intel.com>
2023-11-08 05:14:52 -0800

    minor fix

--- cpp/oneapi/dal/algo/dbscan/backend/gpu/kernel_fp_impl.hpp
+++ cpp/oneapi/dal/algo/dbscan/backend/gpu/kernel_fp_impl.hpp
@@ -29,8 +29,20 @@ namespace pr = dal::backend::primitives;
 inline std::int64_t get_recommended_sg_size(const sycl::queue& queue,
                                             std::int64_t column_count = 0) {
     // TODO optimization/dispatching
+    return column_count > 32 ? 32 : 16;
+}
+
+
+inline std::int64_t get_recommended_sg_size_cores(const sycl::queue& queue,
+                                            std::int64_t column_count = 0) {
+    if(column_count > 28)
+    {
     auto max_sg_size = bk::device_max_wg_size(queue);
     return bk::down_pow2(std::min(column_count, max_sg_size));
+    }
+    else {
+        return 16;
+    }
 }
 
 template <typename Float, bool use_weights>
@@ -61,7 +73,7 @@ struct get_core_wide_kernel {
         std::int32_t* cores_ptr = cores.get_mutable_data();
         auto event = queue.submit([&](sycl::handler& cgh) {
             cgh.depends_on(deps);
-            std::int64_t wg_size = get_recommended_sg_size(queue, column_count);
+            std::int64_t wg_size = get_recommended_sg_size_cores(queue, column_count);
             cgh.parallel_for(
                 bk::make_multiple_nd_range_2d({ wg_size, block_size }, { wg_size, 1 }),
                 [=](sycl::nd_item<2> item) {
@@ -73,22 +85,32 @@ struct get_core_wide_kernel {
                     if (wg_id >= block_size)
                         return;
                     const std::uint32_t local_id = sg.get_local_id();
+                    const std::uint32_t local_size = sg.get_local_range()[0];
 
                     count_type count = 0;
                     for (std::int64_t j = 0; j < row_count; j++) {
                         Float sum = Float(0);
-                        Float distance = Float(0);
-                        for (std::int64_t i = local_id; i < column_count; i += wg_size) {
+                        for (std::int64_t i = local_id; i < column_count / 2; i += local_size) {
                             Float val = data_ptr[(block_start + wg_id) * column_count + i] -
                                         data_ptr[j * column_count + i];
                             sum += val * val;
-                            distance =
-                                sycl::reduce_over_group(sg, sum, sycl::ext::oneapi::plus<Float>());
-                            if (distance > epsilon) {
-                                continue;
-                            }
                         }
-
+                        Float distance_check =
+                            sycl::reduce_over_group(sg, sum, sycl::ext::oneapi::plus<Float>());
+                        if (distance_check > epsilon) {
+                            continue;
+                        }
+                        for (std::int64_t i = column_count / 2 + local_id; i < column_count;
+                             i += local_size) {
+                            Float val = data_ptr[(block_start + wg_id) * column_count + i] -
+                                        data_ptr[j * column_count + i];
+                            sum += val * val;
+                        }
+                        Float distance =
+                            sycl::reduce_over_group(sg, sum, sycl::ext::oneapi::plus<Float>());
+                        if (distance > epsilon) {
+                            continue;
+                        }
                         if constexpr (use_weights) {
                             count += distance <= epsilon ? weights_ptr[j] : count_type(0);
                         }
@@ -307,6 +329,22 @@ sycl::event set_queue_ptr(sycl::queue& queue,
     });
 }
 
+sycl::event set_arr_value(sycl::queue& queue,
+                          pr::ndview<std::int32_t, 1>& arr,
+                          std::int32_t offset,
+                          std::int32_t value,
+                          const bk::event_vector& deps) {
+    auto arr_ptr = arr.get_mutable_data();
+
+    return queue.submit([&](sycl::handler& cgh) {
+        cgh.depends_on(deps);
+        cgh.parallel_for(bk::make_multiple_nd_range_2d({ 1, 1 }, { 1, 1 }),
+                         [=](sycl::nd_item<2> item) {
+                             arr_ptr[offset] = value;
+                         });
+    });
+}
+
 template <typename Float>
 sycl::event kernels_fp<Float>::update_queue(sycl::queue& queue,
                                             const pr::ndview<Float, 2>& data,
@@ -367,7 +405,7 @@ sycl::event kernels_fp<Float>::update_queue(sycl::queue& queue,
                 for (std::int32_t j = 0; j < algo_queue_size; j++) {
                     const std::int32_t index = queue_ptr[j + queue_begin];
                     Float sum = Float(0);
-                    for (std::int64_t i = local_id; i < column_count; i += local_size) {
+                    for (std::int64_t i = local_id; i < column_count / 2; i += local_size) {
                         Float val =
                             data_ptr[probe * column_count + i] - data_ptr[index * column_count + i];
                         sum += val * val;
@@ -375,6 +413,16 @@ sycl::event kernels_fp<Float>::update_queue(sycl::queue& queue,
                     Float distance =
                         sycl::reduce_over_group(sg, sum, sycl::ext::oneapi::plus<Float>());
                     if (distance > epsilon)
+                        continue;
+                    for (std::int64_t i = column_count / 2 + local_id; i < column_count;
+                         i += local_size) {
+                        Float val =
+                            data_ptr[probe * column_count + i] - data_ptr[index * column_count + i];
+                        sum += val * val;
+                    }
+                    Float distance_check =
+                        sycl::reduce_over_group(sg, sum, sycl::ext::oneapi::plus<Float>());
+                    if (distance_check > epsilon)
                         continue;
                     if (local_id == 0) {
                         responses_ptr[wg_id] = cluster_id;
@@ -396,22 +444,6 @@ sycl::event kernels_fp<Float>::update_queue(sycl::queue& queue,
             });
     });
     return event;
-}
-
-sycl::event set_arr_value(sycl::queue& queue,
-                          pr::ndview<std::int32_t, 1>& arr,
-                          std::int32_t offset,
-                          std::int32_t value,
-                          const bk::event_vector& deps) {
-    auto arr_ptr = arr.get_mutable_data();
-
-    return queue.submit([&](sycl::handler& cgh) {
-        cgh.depends_on(deps);
-        cgh.parallel_for(bk::make_multiple_nd_range_2d({ 1, 1 }, { 1, 1 }),
-                         [=](sycl::nd_item<2> item) {
-                             arr_ptr[offset] = value;
-                         });
-    });
 }
 
 template <typename Float>
