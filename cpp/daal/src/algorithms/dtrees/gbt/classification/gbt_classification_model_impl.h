4476b79d6017aca51795a0753a62a18463f07e0f
Ian Faust <icfaust@gmail.com>
2023-11-14 09:27:54 +0100

    Update cpp/daal/src/algorithms/dtrees/gbt/classification/gbt_classification_model_impl.h
    Co-authored-by: Andreas Huber <9201869+ahuber21@users.noreply.github.com>

--- cpp/daal/src/algorithms/dtrees/gbt/classification/gbt_classification_model_impl.h
+++ cpp/daal/src/algorithms/dtrees/gbt/classification/gbt_classification_model_impl.h
@@ -48,7 +48,7 @@ public:
     typedef gbt::internal::ModelImpl ImplType;
     typedef algorithms::classifier::internal::ModelInternal ClassificationImplType;
 
-    ModelImpl(size_t nFeatures = 0) : ClassificationImplType(nFeatures), _predictionBias(0.) {}
+    ModelImpl(size_t nFeatures = 0) : ClassificationImplType(nFeatures), _predictionBias(0.0f) {}
     ~ModelImpl() DAAL_C11_OVERRIDE {}
 
     virtual size_t getNumberOfFeatures() const DAAL_C11_OVERRIDE { return ClassificationImplType::getNumberOfFeatures(); }
