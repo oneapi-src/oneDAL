737c08321b2f32949b9780d93e3404e31c6c2771
Ajay Kumar Patel <ajaykumar.patel@fujitsu.com>
2023-12-14 11:57:07 +0530

    [aarch64] fix - resuse of var cause path value conflict

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -57,10 +57,10 @@ OS=${PLATFORM::3}
 ARCH=${PLATFORM:3:3}
 if [ "$ARCH" == arm ]; then
     full_arch=arm
-    cmake_results_dir=arm_aarch64
+    output_dir=arm_aarch64
 else
     full_arch=intel64
-    cmake_results_dir=intel_intel64
+    output_dir=intel_intel64
 fi
 build_system=${build_system:-cmake}
 backend=${backend:-mkl}
@@ -168,7 +168,7 @@ for link_mode in ${link_modes}; do
         fi
         output_result=
         err=
-        cmake_results_dir="_cmake_results/${cmake_results_dir}_${lib_ext}"
+        cmake_results_dir="_cmake_results/${output_dir}_${lib_ext}"
         for p in ${cmake_results_dir}/*; do
             e=$(basename "$p")
             ${p} 2>&1 > ${e}.res
