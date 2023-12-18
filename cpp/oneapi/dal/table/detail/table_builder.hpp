f8f6bd51b0fd9b9f75fbd0a2abfdced660ed1c17
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2023-12-18 05:15:19 -0800

    Improve comments in table builder classes

--- cpp/oneapi/dal/table/detail/table_builder.hpp
+++ cpp/oneapi/dal/table/detail/table_builder.hpp
@@ -25,10 +25,12 @@
 namespace oneapi::dal::detail {
 namespace v1 {
 
+/// Builds data table from the provided data.
 class ONEDAL_EXPORT table_builder {
     friend pimpl_accessor;
 
 public:
+    /// Build data table.
     table build() const {
         return detail::make_private<table>(impl_->build());
     }
@@ -40,17 +42,21 @@ private:
     pimpl<table_builder_iface> impl_;
 };
 
-/// Builds homogeneous table from the provided data
+/// Builds homogeneous table from the provided data.
 class ONEDAL_EXPORT homogen_table_builder : public table_builder {
 public:
     homogen_table_builder();
 
-    /// Builds homogeneous table
+    /// Build homogeneous table.
     homogen_table build() {
         return detail::make_private<homogen_table>(get_impl().build_homogen());
     }
 
-    /// Reset the
+    /// Reset the internal data of the builder with the data from the input table.
+    ///
+    /// @param t    R-value reference to input homogeneous table.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     auto& reset(homogen_table&& t) {
         const homogen_table local_table = std::move(t);
 
@@ -64,6 +70,17 @@ public:
         return *this;
     }
 
+    /// Reset the internal data of the builder with the provided inputs.
+    ///
+    /// @tparam Data    The type of elements in the input data block.
+    ///                 The :literal:`Data` type should be at least :expr:`float`, :expr:`double`
+    ///                 or :expr:`std::int32_t`.
+    ///
+    /// @param data         The array that stores a homogeneous data block.
+    /// @param row_count    The number of rows in the table to be built.
+    /// @param column_count The number of columns in the table to be built.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     template <typename Data>
     auto& reset(const dal::array<Data>& data, std::int64_t row_count, std::int64_t column_count) {
         const auto byte_data = detail::reinterpret_array_cast<byte_t>(data);
@@ -72,32 +89,78 @@ public:
         return *this;
     }
 
+    /// Reset the internal data of the builder with the provided inputs.
+    ///
+    /// @param data         The array that stores a homogeneous data block.
+    /// @param row_count    The number of rows in the table to be built.
+    /// @param column_count The number of columns in the table to be built.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     auto& set_data_type(data_type dt) {
         get_impl().set_data_type(dt);
         return *this;
     }
 
+    /// Set the type of the features in the data table
+    ///
+    /// @param ft The type of the features. Should be :literal:`feature_type::nominal`,
+    ///           :literal:`feature_type::ordinal`, :literal:`feature_type::interval` or
+    ///           :literal:`feature_type::ratio`.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     auto& set_feature_type(feature_type ft) {
         get_impl().set_feature_type(ft);
         return *this;
     }
 
+    /// Set the type of the layout of the data in the data table
+    ///
+    /// @param layout   The layout of the data. Should be :literal:`data_layout::row_major` or
+    ///                 :literal:`data_layout::column_major`.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     auto& set_layout(data_layout layout) {
         get_impl().set_layout(layout);
         return *this;
     }
 
+    /// Allocate the storage for the data table.
+    ///
+    /// @param row_count    The number of rows in the data block to allocate.
+    /// @param column_count The number of columns in the data block to allocate.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     auto& allocate(std::int64_t row_count, std::int64_t column_count) {
         get_impl().allocate(row_count, column_count);
         return *this;
     }
 
+    /// Copy data from the data block into the table builder.
+    ///
+    /// @tparam Data    The type of elements in the input data block.
+    ///                 The :literal:`Data` type should be at least :expr:`float`, :expr:`double`
+    ///                 or :expr:`std::int32_t`.
+    ///
+    /// @param data         The pointer to a homogeneous data block.
+    /// @param row_count    The number of rows in the data block.
+    /// @param column_count The number of columns in the data blockt.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     template <typename Data>
     auto& copy_data(const Data* data, std::int64_t row_count, std::int64_t column_count) {
         get_impl().copy_data(data, row_count, column_count);
         return *this;
     }
 
+    /// Copy data from the array.
+    ///
+    /// @tparam Data         The type of elements in the input array.
+    ///                      The :literal:`Data` type should be at least :expr:`float`, :expr:`double`
+    ///                      or :expr:`std::int32_t`.
+    ///
+    /// @param data         The array that stores a homogeneous data block.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     template <typename Data>
     auto& copy_data(const array<Data>& data) {
         get_impl().copy_data(reinterpret_array_cast<dal::byte_t>(data));
@@ -105,6 +168,14 @@ public:
     }
 
 #ifdef ONEDAL_DATA_PARALLEL
+    /// Allocate the USM storage for the data table.
+    ///
+    /// @param queue        The SYCL* queue object.
+    /// @param row_count    The number of rows in the data block to allocate.
+    /// @param column_count The number of columns in the data block to allocate.
+    /// @param alloc        The requested kind of USM in the allocated block.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     auto& allocate(const sycl::queue& queue,
                    std::int64_t row_count,
                    std::int64_t column_count,
@@ -113,6 +184,19 @@ public:
         return *this;
     }
 
+    /// Copy data from the USM data block into the table builder.
+    ///
+    /// @tparam Data    The type of elements in the input data block.
+    ///                 The :literal:`Data` type should be at least :expr:`float`, :expr:`double`
+    ///                 or :expr:`std::int32_t`.
+    ///
+    /// @param queue        The SYCL* queue object.
+    /// @param data         The pointer to a homogeneous data block.
+    /// @param row_count    The number of rows in the data block.
+    /// @param column_count The number of columns in the data block.
+    /// @param dependencies Events indicating availability of the :literal:`data` for reading or writing.
+    ///
+    /// @result Reference to the updated homogeneous table builder.
     template <typename Data>
     auto& copy_data(sycl::queue& queue,
                     const Data* data,
@@ -136,10 +220,27 @@ class ONEDAL_EXPORT csr_table_builder : public table_builder {
 public:
     csr_table_builder();
 
+    /// Build CSR table.
     csr_table build() {
         return detail::make_private<csr_table>(get_impl().build_csr());
     }
 
+    /// Reset the internal data of the builder with the provided inputs.
+    ///
+    /// @tparam Data    The type of elements in the input values block.
+    ///                 The :literal:`Data` type should be at least :expr:`float`, :expr:`double`
+    ///                 or :expr:`std::int32_t`.
+    ///
+    /// @param data           The array of values in the CSR layout.
+    /// @param column_indices The array of column indices in the CSR layout.
+    /// @param row_offsets    The array of row offsets in the CSR layout.
+    /// @param row_count      The number of rows in the table to be built.
+    /// @param column_count   The number of columns in the table to be built.
+    /// @param indexing       The indexing scheme used to access data in the CSR layout.
+    ///                       Should be :literal:`sparse_indexing::zero_based` or
+    ///                       :literal:`sparse_indexing::one_based`.
+    ///
+    /// @result Reference to the updated CSR table builder.
     template <typename Data>
     auto& reset(const dal::array<Data>& data,
                 const dal::array<std::int64_t>& column_indices,
