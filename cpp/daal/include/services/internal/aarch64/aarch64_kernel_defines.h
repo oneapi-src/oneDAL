a765d97bbae90a6d7a7ba973cc5e14e9b2d94187
rakshithgb-fujitsu <rakshith.gb@fujitsu.com>
2024-02-07 11:53:15 +0530

    cla changes

--- cpp/daal/include/services/internal/aarch64/aarch64_kernel_defines.h
+++ cpp/daal/include/services/internal/aarch64/aarch64_kernel_defines.h
@@ -1,3 +1,20 @@
+/* file: aarch64_kernel_defines.h */
+/*******************************************************************************
+* Copyright contributors to the oneDAL project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
 #ifndef __aarch64_KERNEL_DEFINES_H__
 #define __aarch64_KERNEL_DEFINES_H__
 
