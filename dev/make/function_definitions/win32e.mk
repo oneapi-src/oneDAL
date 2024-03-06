239b615f8744feb8ce94cd2a3ad70d1da6bf7b4c
Keeran Rothenfusser <keeranr@rivosinc.com>
2024-03-06 09:29:51 +0100

    Don't use year in copyright

--- dev/make/function_definitions/win32e.mk
+++ dev/make/function_definitions/win32e.mk
@@ -1,5 +1,4 @@
 #===============================================================================
-# Copyright 2024 UXL Foundation
 # Copyright contributors to the oneDAL project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
