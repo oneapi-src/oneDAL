804eb09f24ebebe6e4a9470493bd878453e3128a
ethanglaser <ethan.glaser@intel.com>
2024-03-08 15:52:34 -0800

    consolidate copy_convert to resolve "No tests ran"

--- cpp/oneapi/dal/table/backend/test/copy_convert_perf.cpp
+++ cpp/oneapi/dal/table/backend/test/copy_convert_perf.cpp
@@ -35,6 +35,63 @@
 
 namespace oneapi::dal::backend::primitives::test {
 
+template <typename Param>
+class copy_convert_cpu_test : public copy_convert_fixture<Param> {
+public:
+    using result_t = std::tuple_element_t<0, Param>;
+    using sources_t = std::tuple_element_t<1, Param>;
+
+    constexpr static inline const sources_t* dummy_ptr = nullptr;
+    constexpr static inline auto data_types = make_types_array(dummy_ptr);
+    constexpr static inline std::int64_t col_size = get_col_size(dummy_ptr);
+    constexpr static inline std::int64_t row_count = std::tuple_size_v<sources_t>;
+    constexpr static inline auto result_type = detail::make_data_type<result_t>();
+
+    void generate() {
+        this->col_count = GENERATE(100'000'001);
+        CAPTURE(this->col_count, row_count);
+        this->generate_input();
+    }
+
+    void test_copy_convert_rm() {
+        auto policy = this->get_host_policy();
+
+        const auto res_count = this->col_count * row_count;
+        const auto res_size = res_count * sizeof(result_t);
+        auto result = dal::array<dal::byte_t>::empty(res_size);
+        dal::array<data_type> types = this->get_types_array();
+
+        BENCHMARK(__PRETTY_FUNCTION__) {
+            copy_convert(policy,
+                         types,
+                         this->inp,
+                         { row_count, this->col_count },
+                         result_type,
+                         result,
+                         { this->col_count, 1l });
+        };
+    }
+
+    void test_copy_convert_cm() {
+        auto policy = this->get_host_policy();
+
+        const auto res_count = this->col_count * row_count;
+        const auto res_size = res_count * sizeof(result_t);
+        auto result = dal::array<dal::byte_t>::empty(res_size);
+        dal::array<data_type> types = this->get_types_array();
+
+        BENCHMARK(__PRETTY_FUNCTION__) {
+            copy_convert(policy,
+                         types,
+                         this->inp,
+                         { row_count, this->col_count },
+                         result_type,
+                         result,
+                         { 1l, row_count });
+        };
+    }
+};
+
 #ifdef ONEDAL_DATA_PARALLEL
 
 template <typename Param>
@@ -97,9 +154,20 @@ public:
         };
     }
 };
+#endif
 
+TEMPLATE_LIST_TEST_M(copy_convert_cpu_test,
+                     "Deterministic random array",
+                     "[convert][2d][small]",
+                     convert_types) {
+    this->generate();
+    this->test_copy_convert_rm();
+    this->test_copy_convert_cm();
+}
+
+#ifdef ONEDAL_DATA_PARALLEL
 TEMPLATE_LIST_TEST_M(copy_convert_dpc_test,
-                     "Determenistic random array",
+                     "Deterministic random array",
                      "[convert][2d][small]",
                      convert_types) {
     this->generate();
