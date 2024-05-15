f1daa213cedecfa3e75754eaca92c8656c840ba2
Alexandr-Solovev <aleksandr.solovev@intel.com>
2024-05-15 05:12:27 -0700

    init prints

--- cpp/oneapi/dal/algo/covariance/backend/gpu/compute_kernel_dense_impl_dpc.cpp
+++ cpp/oneapi/dal/algo/covariance/backend/gpu/compute_kernel_dense_impl_dpc.cpp
@@ -21,7 +21,7 @@
 #include "oneapi/dal/detail/common.hpp"
 #include "oneapi/dal/detail/policy.hpp"
 #include "oneapi/dal/detail/profiler.hpp"
-
+#include <iostream>
 #include "oneapi/dal/backend/primitives/ndarray.hpp"
 #include "oneapi/dal/backend/memory.hpp"
 #include "oneapi/dal/backend/primitives/reduction.hpp"
@@ -49,47 +49,51 @@ result_t compute_kernel_dense_impl<Float>::operator()(const descriptor_t& desc,
                                                       const parameters_t& params,
                                                       const input_t& input) {
     ONEDAL_ASSERT(input.get_data().has_data());
-
+    std::cout<<"step 1"<<std::endl;
     const auto data = input.get_data();
-
+    std::cout<<"step 2"<<std::endl;
     const std::int64_t row_count = data.get_row_count();
     ONEDAL_ASSERT(row_count > 0);
+    std::cout<<"step 3"<<std::endl;
     auto rows_count_global = row_count;
     const std::int64_t column_count = data.get_column_count();
+    std::cout<<"step 4"<<std::endl;
     ONEDAL_ASSERT(column_count > 0);
-
+    std::cout<<"step 5"<<std::endl;
     auto bias = desc.get_bias();
+    std::cout<<"step 6"<<std::endl;
     auto assume_centered = desc.get_assume_centered();
+    std::cout<<"step 7"<<std::endl;
 
     auto result = compute_result<task_t>{}.set_result_options(desc.get_result_options());
-
+    std::cout<<"step 8"<<std::endl;
     const auto data_nd = pr::table2ndarray<Float>(q_, data, alloc::device);
-
+    std::cout<<"step 9"<<std::endl;
     auto [sums, sums_event] = compute_sums(q_, data_nd, assume_centered, {});
-
+    std::cout<<"step 10"<<std::endl;
     {
         ONEDAL_PROFILER_TASK(allreduce_sums, q_);
         comm_.allreduce(sums.flatten(q_, { sums_event }), spmd::reduce_op::sum).wait();
     }
-
+    std::cout<<"step 11"<<std::endl;
     auto xtx = pr::ndarray<Float, 2>::empty(q_, { column_count, column_count }, alloc::device);
-
+    std::cout<<"step 12"<<std::endl;
     sycl::event gemm_event;
     {
         ONEDAL_PROFILER_TASK(gemm, q_);
         gemm_event = gemm(q_, data_nd.t(), data_nd, xtx, Float(1.0), Float(0.0));
     }
-
+    std::cout<<"step 13"<<std::endl;
     {
         ONEDAL_PROFILER_TASK(allreduce_xtx, q_);
         comm_.allreduce(xtx.flatten(q_, { gemm_event }), spmd::reduce_op::sum).wait();
     }
-
+    std::cout<<"step 14"<<std::endl;
     {
         ONEDAL_PROFILER_TASK(allreduce_rows_count_global);
         comm_.allreduce(rows_count_global, spmd::reduce_op::sum).wait();
     }
-
+    std::cout<<"step 15"<<std::endl;
     if (desc.get_result_options().test(result_options::cov_matrix)) {
         auto [cov, cov_event] = compute_covariance(q_,
                                                    rows_count_global,
@@ -101,12 +105,14 @@ result_t compute_kernel_dense_impl<Float>::operator()(const descriptor_t& desc,
         result.set_cov_matrix(
             (homogen_table::wrap(cov.flatten(q_, { cov_event }), column_count, column_count)));
     }
+    std::cout<<"step 16"<<std::endl;
     if (desc.get_result_options().test(result_options::cor_matrix)) {
         auto [corr, corr_event] =
             compute_correlation(q_, rows_count_global, xtx, sums, { gemm_event });
         result.set_cor_matrix(
             (homogen_table::wrap(corr.flatten(q_, { corr_event }), column_count, column_count)));
     }
+    std::cout<<"step 17"<<std::endl;
     if (desc.get_result_options().test(result_options::means)) {
         if (!assume_centered) {
             auto [means, means_event] = compute_means(q_, sums, rows_count_global, { gemm_event });
@@ -120,6 +126,7 @@ result_t compute_kernel_dense_impl<Float>::operator()(const descriptor_t& desc,
                 homogen_table::wrap(zero_means.flatten(q_, { zeros_event }), 1, column_count));
         }
     }
+    std::cout<<"step 18"<<std::endl;
     return result;
 }
 
