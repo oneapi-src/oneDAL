e46c7e2d6ea4b7e9f5a88e73b8cd7d9eb39465a2
Alexandr-Solovev <aleksandr.solovev@intel.com>
2024-05-21 05:11:06 -0700

    fix for arm

--- cpp/daal/src/threading/threading.cpp
+++ cpp/daal/src/threading/threading.cpp
@@ -77,12 +77,14 @@ DAAL_EXPORT void _daal_tbb_task_scheduler_free(void *& globalControl)
 
 DAAL_EXPORT void _daal_tbb_task_scheduler_handle_free(void *& schedulerHandle)
 {
-#if defined(__DO_TBB_LAYER__)
+#if defined(TARGET_X86_64)
+    #if defined(__DO_TBB_LAYER__)
     if (schedulerHandle)
     {
         delete reinterpret_cast<tbb::task_scheduler_handle *>(schedulerHandle);
         schedulerHandle = nullptr;
     }
+    #endif
 #endif
 }
 
@@ -105,10 +107,12 @@ DAAL_EXPORT size_t _setNumberOfThreads(const size_t numThreads, void ** globalCo
 
 DAAL_EXPORT size_t _setSchedulerHandle(void ** schedulerHandle)
 {
-#if defined(__DO_TBB_LAYER__)
+#if defined(TARGET_X86_64)
+    #if defined(__DO_TBB_LAYER__)
     // It is necessary for initializing tbb in cases where DAAL does not use it.
     tbb::task_arena {}.initialize();
     *schedulerHandle = reinterpret_cast<void *>(new tbb::task_scheduler_handle(tbb::attach {}));
+    #endif
 #endif
     return 0;
 }
