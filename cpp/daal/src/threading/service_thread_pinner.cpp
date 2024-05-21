5fcc819ff7ffa762cb784113cf229168d0ee19a9
Alexandr-Solovev <aleksandr.solovev@intel.com>
2024-05-21 07:29:05 -0700

    fixes

--- cpp/daal/src/threading/service_thread_pinner.cpp
+++ cpp/daal/src/threading/service_thread_pinner.cpp
@@ -236,7 +236,9 @@ public:
 thread_pinner_impl_t::thread_pinner_impl_t(void (*read_topo)(int &, int &, int &, int **), void (*deleter)(void *))
     : pinner_arena(nthreads = daal::threader_get_threads_number()), tbb::task_scheduler_observer(pinner_arena), topo_deleter(deleter)
 {
+        #if defined(TARGET_X86_64)
     pinner_arena.initialize();
+        #endif
     do_pinning = (nthreads > 0) ? true : false;
     is_pinning.set(0);
 
