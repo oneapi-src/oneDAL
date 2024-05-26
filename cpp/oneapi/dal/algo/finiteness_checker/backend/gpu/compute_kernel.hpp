ec73cfae47a3cc72f563129002d4c04c1af571e8
Ian Faust <icfaust@gmail.com>
2024-05-26 22:49:26 +0200

    Update compute_kernel.hpp

--- cpp/oneapi/dal/algo/finiteness_checker/backend/gpu/compute_kernel.hpp
+++ cpp/oneapi/dal/algo/finiteness_checker/backend/gpu/compute_kernel.hpp
@@ -24,9 +24,9 @@ namespace oneapi::dal::finiteness_checker::backend {
 
 template <typename Float, typename Method, typename Task>
 struct compute_kernel_gpu {
-    compute_result<Task> operator()(const dal::backend::context_gpu& ctx,
-                                    const detail::descriptor_base<Task>& params,
-                                    const compute_input<Task>& input) const;
+    bool operator()(const dal::backend::context_gpu& ctx,
+                    const detail::descriptor_base<Task>& params,
+                    const compute_input<Task>& input) const;
 
 #ifdef ONEDAL_DATA_PARALLEL
     void operator()(const dal::backend::context_gpu& ctx,
