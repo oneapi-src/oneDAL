d88bcbad8e889a5fa51fb20ef5721011ad5ad62f
Alexandr-Solovev <aleksandr.solovev@intel.com>
2024-05-29 01:38:00 -0700

    fix

--- cpp/daal/src/services/env_detect.cpp
+++ cpp/daal/src/services/env_detect.cpp
@@ -151,7 +151,7 @@ DAAL_EXPORT daal::services::Environment::~Environment()
 {
     daal::services::daal_free_buffers();
     _daal_tbb_task_scheduler_free(_globalControl);
-    _daal_tbb_task_scheduler_handle_finalize(_schedulerHandle);
+    // _daal_tbb_task_scheduler_handle_finalize(_schedulerHandle);
 }
 
 void daal::services::Environment::_cpu_detect(int enable)
