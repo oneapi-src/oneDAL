4c38fe2111b6af1b4c1ec0ba50d470d7b7b61b3d
Anatoly Volkov <anatoly.volkov@intel.com>
2024-07-31 03:12:24 -0700

    Add ONEDAL_EXPORT for sparse method

--- cpp/oneapi/dal/algo/logistic_regression/detail/train_ops.cpp
+++ cpp/oneapi/dal/algo/logistic_regression/detail/train_ops.cpp
@@ -64,6 +64,8 @@ private:
 
 INSTANTIATE(float, method::dense_batch, task::classification)
 INSTANTIATE(double, method::dense_batch, task::classification)
+INSTANTIATE(float, method::sparse, task::classification)
+INSTANTIATE(double, method::sparse, task::classification)
 
 } // namespace v1
 } // namespace oneapi::dal::logistic_regression::detail
