81df28f0423d1815de3add2dc35d61a8faecd3ab
Md Shafiul Alam <md.shafiul.alam@intel.com>
2024-07-31 09:36:42 -0700

    codefactor

--- .ci/env/apt.sh
+++ .ci/env/apt.sh
@@ -81,7 +81,7 @@ function install_qemu_emulation_deb {
     set -eo pipefail
     if [[ -z "${found_version}" ]] ; then
         # If nothing is found, error out and fail
-        echo "No qemu found with version numbers ${versions[@]}"
+        echo "None of requested qemu versions ${versions[*]} are available."
         false
     fi
 
