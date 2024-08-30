4aaee8db243c5b58338ee712cf434548bf46e1b4
Alexandr-Solovev <aleksandr.solovev@intel.com>
2024-08-30 07:30:30 -0700

    minor fix

--- cpp/daal/src/externals/core_threading_win_dll.cpp
+++ cpp/daal/src/externals/core_threading_win_dll.cpp
@@ -14,7 +14,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
-c
+
 /*
 //++c
 //  Implementation of "stubs" for threading layer functions for win dll case.
