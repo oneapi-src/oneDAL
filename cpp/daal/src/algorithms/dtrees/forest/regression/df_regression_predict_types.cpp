d6c3b103bf389fc7e543321fa36211979182576e
Victoriya Fedotova <victoriya.s.fedotova@intel.com>
2024-12-06 04:51:06 -0800

    Fix a typo

--- cpp/daal/src/algorithms/dtrees/forest/regression/df_regression_predict_types.cpp
+++ cpp/daal/src/algorithms/dtrees/forest/regression/df_regression_predict_types.cpp
@@ -43,7 +43,7 @@ namespace interface1
 __DAAL_REGISTER_SERIALIZATION_CLASS(Result, SERIALIZATION_DECISION_FOREST_REGRESSION_PREDICTION_RESULT_ID);
 
 /** Default constructor */
-Input::Input()                                = default;
+Input::Input() : algorithms::regression::prediction::Input(lastModelInputId + 1) {}
 Input::Input(const Input & other)             = default;
 Input & Input::operator=(const Input & other) = default;
 
