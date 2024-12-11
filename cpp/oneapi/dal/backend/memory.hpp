2ad6948d79a59477fed76f00925cfad778a497d1
Aleksandr Solovev <aleksandr.solovev@intel.com>
2024-12-11 13:18:24 +0100

    fix: avoid additional array allocation in host to device transfer (#2966)

--- cpp/oneapi/dal/backend/memory.hpp
+++ cpp/oneapi/dal/backend/memory.hpp
@@ -221,12 +221,9 @@ inline sycl::event memcpy_host2usm(sycl::queue& queue,
                                    const event_vector& deps = {}) {
     ONEDAL_ASSERT(is_known_usm(queue, dest_usm));
 
-    // TODO: Remove additional copy to host usm memory once
-    //       bug in `copy` with the host memory is fixed
-    auto tmp_usm_host = make_unique_usm_host(queue, size);
-    memcpy(tmp_usm_host.get(), src_host, size);
-    memcpy(queue, dest_usm, tmp_usm_host.get(), size, deps).wait_and_throw();
-    return {};
+    sycl::event memcpy_event = memcpy(queue, dest_usm, src_host, size, deps);
+    memcpy_event.wait_and_throw();
+    return memcpy_event;
 }
 
 inline sycl::event memcpy_usm2host(sycl::queue& queue,
@@ -236,12 +233,9 @@ inline sycl::event memcpy_usm2host(sycl::queue& queue,
                                    const event_vector& deps = {}) {
     ONEDAL_ASSERT(is_known_usm(queue, src_usm));
 
-    // TODO: Remove additional copy to host usm memory once
-    //       bug in `copy` with the host memory is fixed
-    auto tmp_usm_host = make_unique_usm_host(queue, size);
-    memcpy(queue, tmp_usm_host.get(), src_usm, size, deps).wait_and_throw();
-    memcpy(dest_host, tmp_usm_host.get(), size);
-    return {};
+    sycl::event memcpy_event = memcpy(queue, dest_host, src_usm, size, deps);
+    memcpy_event.wait_and_throw();
+    return memcpy_event;
 }
 
 /// Copies `count` number of elements from the USM-allocated array pointed to by `src`
